b3997ba1eb19fc50023df88b58b7f239f03db786
Alexander Kolesov <alexander.kolesov@evernym.com>
2021-04-21 18:27:42 +0300

    It's working

--- genlocalnetconfig.sh
+++ genlocalnetconfig.sh
@@ -1,110 +1,94 @@
 #!/bin/bash
 
-# TODO: Utilize home env
+set -euox pipefail
 
-set -euo pipefail
-
-rm -rf ~/.verimcosmos
+CHAIN_ID="verim-cosmos-chain"
 
 rm -rf localnet
-mkdir localnet localnet/client localnet/node0 localnet/node1 localnet/node2 localnet/node3
+mkdir localnet
 
 # client
 
-# verim-cosmosd config chain-id verim-cosmoschain
-# verim-cosmosd config output json
-# verim-cosmosd config indent true
-# verim-cosmosd config trust-node false
-
-echo 'test1234' | verim-cosmosd keys add jack
-echo 'test1234' | verim-cosmosd keys add alice
-echo 'test1234' | verim-cosmosd keys add bob
-echo 'test1234' | verim-cosmosd keys add anna
-
-cp -r ~/.verimcosmos/* localnet/client
+verim-cosmosd keys add jack --home localnet/client
+verim-cosmosd keys add alice --home localnet/client
+verim-cosmosd keys add bob --home localnet/client
+verim-cosmosd keys add anna --home localnet/client
 
 # node 0
 
-verim-cosmosd init node0 --chain-id verim-cosmoschain
+verim-cosmosd init node0 --chain-id $CHAIN_ID --home localnet/node0
+cp -r localnet/client/* localnet/node0
 
-jack_address=$(verim-cosmosd keys show jack -a)
-jack_pubkey=$(verim-cosmosd keys show jack -p)
+verim-cosmosd add-genesis-account jack 1000token,100000000stake --home localnet/node0
+verim-cosmosd add-genesis-account alice 1000token,100000000stake --home localnet/node0
+verim-cosmosd add-genesis-account bob 1000token,100000000stake --home localnet/node0
+verim-cosmosd add-genesis-account anna 1000token,100000000stake --home localnet/node0
 
-alice_address=$(verim-cosmosd keys show alice -a)
-alice_pubkey=$(verim-cosmosd keys show alice -p)
-
-bob_address=$(verim-cosmosd keys show bob -a)
-bob_pubkey=$(verim-cosmosd keys show bob -p)
-
-anna_address=$(verim-cosmosd keys show anna -a)
-anna_pubkey=$(verim-cosmosd keys show anna -p)
-
-verim-cosmosd add-genesis-account --address=$jack_address --pubkey=$jack_pubkey --roles="Trustee,NodeAdmin"
-verim-cosmosd add-genesis-account --address=$alice_address --pubkey=$alice_pubkey --roles="Trustee,NodeAdmin"
-verim-cosmosd add-genesis-account --address=$bob_address --pubkey=$bob_pubkey --roles="Trustee,NodeAdmin"
-verim-cosmosd add-genesis-account --address=$anna_address --pubkey=$anna_pubkey --roles="NodeAdmin"
-
-echo 'test1234' | verim-cosmosd gentx --from jack
-
-mv ~/.verimcosmos/* localnet/node0
+verim-cosmosd gentx jack 1000000stake --chain-id $CHAIN_ID --home localnet/node0
 
 # node 1
 
-verim-cosmosd init node1 --chain-id verim-cosmoschain
+verim-cosmosd init node1 --chain-id $CHAIN_ID --home localnet/node1
+cp -r localnet/client/* localnet/node1
 
-verim-cosmosd add-genesis-account --address=$jack_address --pubkey=$jack_pubkey --roles="Trustee,NodeAdmin"
-verim-cosmosd add-genesis-account --address=$alice_address --pubkey=$alice_pubkey --roles="Trustee,NodeAdmin"
-verim-cosmosd add-genesis-account --address=$bob_address --pubkey=$bob_pubkey --roles="Trustee,NodeAdmin"
-verim-cosmosd add-genesis-account --address=$anna_address --pubkey=$anna_pubkey --roles="NodeAdmin"
+verim-cosmosd add-genesis-account jack 1000token,100000000stake --home localnet/node1
+verim-cosmosd add-genesis-account alice 1000token,100000000stake --home localnet/node1
+verim-cosmosd add-genesis-account bob 1000token,100000000stake --home localnet/node1
+verim-cosmosd add-genesis-account anna 1000token,100000000stake --home localnet/node1
 
-echo 'test1234' | verim-cosmosd gentx --from alice
-
-mv ~/.verimcosmos/* localnet/node1
+verim-cosmosd gentx alice 1000000stake --chain-id $CHAIN_ID --home localnet/node1
 
 # node 2
 
-verim-cosmosd init node2 --chain-id verim-cosmoschain
+verim-cosmosd init node2 --chain-id $CHAIN_ID --home localnet/node2
+cp -r localnet/client/* localnet/node2
 
-verim-cosmosd add-genesis-account --address=$jack_address --pubkey=$jack_pubkey --roles="Trustee,NodeAdmin"
-verim-cosmosd add-genesis-account --address=$alice_address --pubkey=$alice_pubkey --roles="Trustee,NodeAdmin"
-verim-cosmosd add-genesis-account --address=$bob_address --pubkey=$bob_pubkey --roles="Trustee,NodeAdmin"
-verim-cosmosd add-genesis-account --address=$anna_address --pubkey=$anna_pubkey --roles="NodeAdmin"
+verim-cosmosd add-genesis-account jack 1000token,100000000stake --home localnet/node2
+verim-cosmosd add-genesis-account alice 1000token,100000000stake --home localnet/node2
+verim-cosmosd add-genesis-account bob 1000token,100000000stake --home localnet/node2
+verim-cosmosd add-genesis-account anna 1000token,100000000stake --home localnet/node2
 
-echo 'test1234' | verim-cosmosd gentx --from bob
-
-mv ~/.verimcosmos/* localnet/node2
+verim-cosmosd gentx bob 1000000stake --chain-id $CHAIN_ID --home localnet/node2
 
 # node 3
 
-verim-cosmosd init node3 --chain-id verim-cosmoschain
+verim-cosmosd init node3 --chain-id $CHAIN_ID --home localnet/node3
+cp -r localnet/client/* localnet/node3
 
-verim-cosmosd add-genesis-account --address=$jack_address --pubkey=$jack_pubkey --roles="Trustee,NodeAdmin"
-verim-cosmosd add-genesis-account --address=$alice_address --pubkey=$alice_pubkey --roles="Trustee,NodeAdmin"
-verim-cosmosd add-genesis-account --address=$bob_address --pubkey=$bob_pubkey --roles="Trustee,NodeAdmin"
-verim-cosmosd add-genesis-account --address=$anna_address --pubkey=$anna_pubkey --roles="NodeAdmin"
+verim-cosmosd add-genesis-account jack 1000token,100000000stake --home localnet/node3
+verim-cosmosd add-genesis-account alice 1000token,100000000stake --home localnet/node3
+verim-cosmosd add-genesis-account bob 1000token,100000000stake --home localnet/node3
+verim-cosmosd add-genesis-account anna 1000token,100000000stake --home localnet/node3
 
-echo 'test1234' | verim-cosmosd gentx --from anna
-
-cp -r ~/.verimcosmos/* localnet/node3
+verim-cosmosd gentx anna 1000000stake --chain-id $CHAIN_ID --home localnet/node3
 
 # Collect all validator creation transactions
 
-cp localnet/node0/config/gentx/* ~/.verim-cosmosd/config/gentx
-cp localnet/node1/config/gentx/* ~/.verim-cosmosd/config/gentx
-cp localnet/node2/config/gentx/* ~/.verim-cosmosd/config/gentx
-cp localnet/node3/config/gentx/* ~/.verim-cosmosd/config/gentx
+mkdir localnet/client/config/gentx
+
+cp localnet/node0/config/gentx/* localnet/client/config/gentx
+cp localnet/node1/config/gentx/* localnet/client/config/gentx
+cp localnet/node2/config/gentx/* localnet/client/config/gentx
+cp localnet/node3/config/gentx/* localnet/client/config/gentx
 
 # Embed them into genesis
 
-verim-cosmosd collect-gentxs
-verim-cosmosd validate-genesis
+verim-cosmosd init dummy-node --chain-id $CHAIN_ID --home localnet/client
+
+verim-cosmosd add-genesis-account jack 1000token,100000000stake --home localnet/client
+verim-cosmosd add-genesis-account alice 1000token,100000000stake --home localnet/client
+verim-cosmosd add-genesis-account bob 1000token,100000000stake --home localnet/client
+verim-cosmosd add-genesis-account anna 1000token,100000000stake --home localnet/client
+
+verim-cosmosd collect-gentxs --home localnet/client
+verim-cosmosd validate-genesis --home localnet/client
 
 # Update genesis for all nodes
 
-cp ~/.verimcosmos/config/genesis.json localnet/node0/config/
-cp ~/.verimcosmos/config/genesis.json localnet/node1/config/
-cp ~/.verimcosmos/config/genesis.json localnet/node2/config/
-cp ~/.verimcosmos/config/genesis.json localnet/node3/config/
+cp localnet/client/config/genesis.json localnet/node0/config/
+cp localnet/client/config/genesis.json localnet/node1/config/
+cp localnet/client/config/genesis.json localnet/node2/config/
+cp localnet/client/config/genesis.json localnet/node3/config/
 
 # Find out node ids
 
@@ -114,12 +98,12 @@ id2=$(ls localnet/node2/config/gentx | sed 's/gentx-\(.*\).json/\1/')
 id3=$(ls localnet/node3/config/gentx | sed 's/gentx-\(.*\).json/\1/')
 
 # Update address book of the first node
-peers="$id0@192.167.10.2:26656,$id1@192.167.10.3:26656,$id2@192.167.10.4:26656,$id3@192.167.10.5:26656"
-sed -i "s/persistent_peers = \"\"/persistent_peers = \"$peers\"/g" localnet/node0/config/config.toml
+peers="$id0@node0:26656,$id1@node1:26656,$id2@node2:26656,$id3@node3:26656"
+sed -i .orig "s/persistent_peers = \"\"/persistent_peers = \"$peers\"/g" localnet/node0/config/config.toml
 
 # Make RPC enpoint available externally
 
-sed -i 's/laddr = "tcp:\/\/127.0.0.1:26657"/laddr = "tcp:\/\/0.0.0.0:26657"/g' localnet/node0/config/config.toml
-sed -i 's/laddr = "tcp:\/\/127.0.0.1:26657"/laddr = "tcp:\/\/0.0.0.0:26657"/g' localnet/node1/config/config.toml
-sed -i 's/laddr = "tcp:\/\/127.0.0.1:26657"/laddr = "tcp:\/\/0.0.0.0:26657"/g' localnet/node2/config/config.toml
-sed -i 's/laddr = "tcp:\/\/127.0.0.1:26657"/laddr = "tcp:\/\/0.0.0.0:26657"/g' localnet/node3/config/config.toml
+sed -i .orig 's/laddr = "tcp:\/\/127.0.0.1:26657"/laddr = "tcp:\/\/0.0.0.0:26657"/g' localnet/node0/config/config.toml
+sed -i .orig 's/laddr = "tcp:\/\/127.0.0.1:26657"/laddr = "tcp:\/\/0.0.0.0:26657"/g' localnet/node1/config/config.toml
+sed -i .orig 's/laddr = "tcp:\/\/127.0.0.1:26657"/laddr = "tcp:\/\/0.0.0.0:26657"/g' localnet/node2/config/config.toml
+sed -i .orig 's/laddr = "tcp:\/\/127.0.0.1:26657"/laddr = "tcp:\/\/0.0.0.0:26657"/g' localnet/node3/config/config.toml
