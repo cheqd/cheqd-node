cc5d6731f41b94ada2762c0062090cdde4243f75
Alexander Kolesov <alexander.kolesov@evernym.com>
2021-06-14 18:29:59 +0300

    Multisig test is finally working

--- ci/integration_tests/multisig.sh
+++ ci/integration_tests/multisig.sh
@@ -2,10 +2,13 @@
 
 set -euox pipefail
 
-JACK_HOME="../local_net/node_configs/node0"
-ALICE_HOME="localnet/node1"
-BOB_HOME="localnet/node2"
-ANNA_HOME="localnet/node3"
+NODE_CONFIGS="../local_net/node_configs"
+CHAIN_ID="verim"
+
+JACK_HOME="$NODE_CONFIGS/node0"
+ALICE_HOME="$NODE_CONFIGS/node1"
+BOB_HOME="$NODE_CONFIGS/node2"
+ANNA_HOME="$NODE_CONFIGS/node3"
 
 JACK_PUBKEY=$(verim-noded keys show jack -p --home $JACK_HOME)
 ALICE_PUBKEY=$(verim-noded keys show alice -p --home $ALICE_HOME)
@@ -20,64 +23,92 @@ echo "bob: $BOB_PUBKEY"
 echo "anna: $ANNA_PUBKEY"
 
 
-############ Jack imports other's public keys and creates multisig key: ffour
+echo "############ Jack imports other's public keys and creates multisig key: ffour"
 
 verim-noded keys add alice_pub --pubkey $ALICE_PUBKEY --home $JACK_HOME
 verim-noded keys add bob_pub --pubkey $BOB_PUBKEY --home $JACK_HOME
 verim-noded keys add anna_pub --pubkey $ANNA_PUBKEY --home $JACK_HOME
 
 verim-noded keys add ffour --multisig=jack,alice_pub,bob_pub,anna_pub --multisig-threshold=3 --home $JACK_HOME
-
 verim-noded keys show ffour --home $JACK_HOME
+
+echo "############ Someone (Jack) transfers money to the ffour account"
+
 FFOUR_ADDRESS=$(verim-noded keys show ffour -a --home $JACK_HOME)
+FFOUR_PUBKEY=$(verim-noded keys show ffour -p --home $JACK_HOME)
 
-############ Someone (Jack) transfers money to the ffour account
+verim-noded tx bank send jack $FFOUR_ADDRESS 1000000token \
+  --fees 200000token \
+  --chain-id=$CHAIN_ID \
+  --home $JACK_HOME \
+  --yes
 
-verim-noded tx bank send jack $FFOUR_ADDRESS 1token --home $JACK_HOME
-
-
-############ Alice imports other's public keys and creates multisig key: ffour (she doesn't trust shared ffour key)
+echo "############ Alice imports other's public keys and creates multisig key: ffour"
 
 verim-noded keys add jack_pub --pubkey $JACK_PUBKEY --home $ALICE_HOME
 verim-noded keys add bob_pub --pubkey $BOB_PUBKEY --home $ALICE_HOME
 verim-noded keys add anna_pub --pubkey $ANNA_PUBKEY --home $ALICE_HOME
 
-# is 3 imoprtant?
+# Key order and trashhold must be the same. Multisig key import is not supported.
 verim-noded keys add ffour --multisig=jack_pub,alice,bob_pub,anna_pub --multisig-threshold=3 --home $ALICE_HOME
-
 verim-noded keys show ffour --home $ALICE_HOME
-FFOUR_ADDRESS_2=$(verim-noded keys show ffour -a --home $ALICE_HOME)
-FFOUR_PUBKEY=$(verim-noded keys show ffour -p --home $ALICE_HOME)
 
+echo "############ Bob imports other's public keys and creates multisig key: ffour"
 
-############ Bob imports ffour key directly (he trusts shared multisig key)
+verim-noded keys add jack_pub --pubkey $JACK_PUBKEY --home $BOB_HOME
+verim-noded keys add alice_pub --pubkey $ALICE_PUBKEY --home $BOB_HOME
+verim-noded keys add anna_pub --pubkey $ANNA_PUBKEY --home $BOB_HOME
 
-# verim-noded keys add jack_pub --pubkey $JACK_PUBKEY --home $BOB_HOME
-# verim-noded keys add alice_pub --pubkey $ALICE_PUBKEY --home $BOB_HOME
-# verim-noded keys add anna_pub --pubkey $ANNA_PUBKEY --home $BOB_HOME
+# Key order and trashhold must be the same. Multisig key import is not supported.
+verim-noded keys add ffour --multisig=jack_pub,alice_pub,bob,anna_pub --multisig-threshold=3 --home $BOB_HOME
+verim-noded keys show ffour --home $BOB_HOME
 
-# # is 3 imoprtant?
-# verim-noded keys add ffour --multisig=jack_pub,alice_pub,bob,anna_pub --multisig-threshold=3 --home $BOB_HOME
+echo "############ Jack generates a transaction"
 
-verim-noded keys add ffour_pub --pubkey $FFOUR_PUBKEY --home $BOB_HOME
+verim-noded tx bank send $FFOUR_ADDRESS $JACK_ADDRESS 1000token \
+  --fees 200000token \
+  --generate-only \
+  --home $JACK_HOME \
+  > $NODE_CONFIGS/unsignedTx.json
 
-verim-noded keys show ffour_pub --home $BOB_HOME
-FFOUR_ADDRESS_3=$(verim-noded keys show ffour_pub -a --home $BOB_HOME)
+echo "############ Jack signs the transaction"
 
+verim-noded tx sign $NODE_CONFIGS/unsignedTx.json \
+  --multisig=$FFOUR_ADDRESS \
+  --from=jack \
+  --output-document=$NODE_CONFIGS/jack_signature.json \
+  --home $JACK_HOME \
+  --chain-id=$CHAIN_ID
 
-############ Ffour
+echo "############ Alice signs the transaction"
 
-verim-noded tx bank send $FFOUR_ADDRESS $JACK_ADDRESS 1token --generate-only > localnet/unsignedTx.json --home $JACK_HOME
+verim-noded tx sign $NODE_CONFIGS/unsignedTx.json \
+  --multisig=$FFOUR_ADDRESS \
+  --from=alice \
+  --output-document=$NODE_CONFIGS/alice_signature.json \
+  --home $ALICE_HOME \
+  --chain-id=$CHAIN_ID
 
-verim-noded tx sign localnet/unsignedTx.json --multisig=$FFOUR_ADDRESS --from=jack --output-document=localnet/jack_signature.json --home $JACK_HOME --chain-id=verim
+echo "############ Bob signs the transaction. 3 out of 4 signatures is enough."
 
-verim-noded tx sign localnet/unsignedTx.json --multisig=$FFOUR_ADDRESS --from=alice --output-document=localnet/alice_signature.json --home $ALICE_HOME --chain-id=verim
+verim-noded tx sign $NODE_CONFIGS/unsignedTx.json \
+  --multisig=$FFOUR_ADDRESS \
+  --from=bob \
+  --output-document=$NODE_CONFIGS/bob_signature.json \
+  --home $BOB_HOME \
+  --chain-id=$CHAIN_ID
 
-verim-noded tx sign localnet/unsignedTx.json --multisig=$FFOUR_ADDRESS --from=bob --output-document=localnet/bob_signature.json --home $BOB_HOME --chain-id=verim # --account-number=11
+echo "############ Jack (can be anyone) composes signatures"
 
+# chain-id is important
 verim-noded tx multisign \
-  localnet/unsignedTx.json \
+  $NODE_CONFIGS/unsignedTx.json \
   ffour \
-  localnet/alice_signature.json localnet/jack_signature.json localnet/bob_signature.json > localnet/signedTx.json --home $JACK_HOME
+  $NODE_CONFIGS/jack_signature.json $NODE_CONFIGS/alice_signature.json $NODE_CONFIGS/bob_signature.json \
+  --chain-id=$CHAIN_ID \
+  --home $JACK_HOME \
+  > $NODE_CONFIGS/signedTx.json
 
-verim-noded tx broadcast localnet/signedTx.json --home $JACK_HOME
+echo "############ Jack (can be anyone) broadcasts the transaction"
+
+verim-noded tx broadcast $NODE_CONFIGS/signedTx.json --home $JACK_HOME --yes
