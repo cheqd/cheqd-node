b212f212a9447c155089b3e51fb1d5ef4d3ce683
Andrew Nikitin <andrew.nikitin@evernym.com>
2021-07-22 21:39:57 +0300

    Improve .deb creation
    Signed-off-by: Andrew Nikitin <andrew.nikitin@evernym.com>

---
+++ build_tools/common.sh
@@ -0,0 +1,7 @@
+#PATH_TO_BIN=/home/runner/go/bin/$PKG_NAME
+PATH_TO_BIN=/home/anikitin/go/bin/$PKG_NAME
+TMP_DIR=usr/bin
+OUTPUT_DIR=output
+TAR_ARCHIVE=${PKG_NAME}_${VERSION}.tar.gz
+PATH_TAR=$OUTPUT_DIR/$TAR_ARCHIVE
+
