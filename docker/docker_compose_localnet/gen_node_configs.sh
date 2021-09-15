057f807c36467a6841fc01250c194e4900cad443
Alexander Kolesov <alexander.kolesov@evernym.com>
2021-09-15 14:02:55 +0300

    Add observers to docker compose localnet

--- docker/docker_compose_localnet/gen_node_configs.sh
+++ docker/docker_compose_localnet/gen_node_configs.sh
@@ -4,9 +4,11 @@
 
 set -euox pipefail
 
-NODES_COUNT="4"
 CHAIN_ID="cheqd"
 NODE_CONFIGS_DIR="node_configs"
+
+VALIDATORS_COUNT="4"
+OBSERVERS_COUNT="2"
 
 # sed in macos requires extra argument
 sed_extension=''
@@ -20,9 +22,11 @@ rm -rf $NODE_CONFIGS_DIR
 mkdir $NODE_CONFIGS_DIR
 
 
-for ((i=0 ; i<$NODES_COUNT ; i++))
+echo "##### Setting up validators" 
+
+for ((i=0 ; i<$VALIDATORS_COUNT ; i++))
 do
-    echo "##### [Node $i] Generates key" 
+    echo "##### [Validator $i] Generates key" 
 
     NODE_HOME="$NODE_CONFIGS_DIR/node$i"
     cheqd-noded init "node$i" --chain-id $CHAIN_ID --home $NODE_HOME
@@ -38,7 +42,7 @@ echo "##### [Validator operators] Generate keys"
 
 OPERATORS_HOME=$NODE_CONFIGS_DIR/client
 
-for ((i=0 ; i<$NODES_COUNT ; i++))
+for ((i=0 ; i<$VALIDATORS_COUNT ; i++))
 do
     cheqd-noded keys add "operator$i" --home $OPERATORS_HOME
 done
@@ -51,7 +55,7 @@ sed -i $sed_extension 's/"stake"/"cheq"/' $OPERATORS_HOME/config/genesis.json
 
 echo "##### [Validator operators] Add them to the genesis" 
 
-for ((i=0 ; i<$NODES_COUNT ; i++))
+for ((i=0 ; i<$VALIDATORS_COUNT ; i++))
 do
     cheqd-noded add-genesis-account "operator$i" 20000000cheq --home $OPERATORS_HOME
 done
@@ -59,7 +63,7 @@ done
 
 echo "##### [Validator operators] Generate stake transactions" 
 
-for ((i=0 ; i<$NODES_COUNT ; i++))
+for ((i=0 ; i<$VALIDATORS_COUNT ; i++))
 do
     NODE_HOME="$NODE_CONFIGS_DIR/node$i"
     NODE_ID=$(cheqd-noded tendermint show-node-id --home $NODE_HOME)
@@ -77,7 +81,7 @@ cheqd-noded validate-genesis --home $OPERATORS_HOME
 
 echo "##### [Validator operators] Propagate genesis to nodes"
 
-for ((i=0 ; i<$NODES_COUNT ; i++))
+for ((i=0 ; i<$VALIDATORS_COUNT ; i++))
 do
     NODE_HOME="$NODE_CONFIGS_DIR/node$i"
 
@@ -87,9 +91,32 @@ done
 
 echo "##### [Validator operators] Set minimum fee price"
 
-for ((i=0 ; i<$NODES_COUNT ; i++))
+for ((i=0 ; i<$VALIDATORS_COUNT ; i++))
 do
     NODE_HOME="$NODE_CONFIGS_DIR/node$i"
 
     sed -i $sed_extension 's/minimum-gas-prices = ""/minimum-gas-prices = "0.00'$i'cheq"/g' $NODE_HOME/config/app.toml
+done
+
+
+echo "##### Setting up observers"
+
+for ((i=0 ; i<$OBSERVERS_COUNT ; i++))
+do
+    NODE_HOME="$NODE_CONFIGS_DIR/observer$i"
+
+    echo "##### [Observer $i] Generating keys" 
+    cheqd-noded init "node$i" --chain-id $CHAIN_ID --home $NODE_HOME
+    
+    echo "##### [Observer $i] Exporting public keys" 
+    NODE_ID=$(cheqd-noded tendermint show-node-id --home $NODE_HOME)
+    NODE_VAL_PUBKEY=$(cheqd-noded tendermint show-validator --home $NODE_HOME)
+    echo "$NODE_ID" > $NODE_HOME/node_id.txt
+    echo "$NODE_VAL_PUBKEY" > $NODE_HOME/node_val_pubkey.txt
+
+    echo "##### [Observer $i] Loading genesis" 
+    cp $OPERATORS_HOME/config/genesis.json $NODE_HOME/config/
+
+    echo "##### [Observer $i] Setting min gas prices" 
+    sed -i $sed_extension 's/minimum-gas-prices = ""/minimum-gas-prices = "'$i'cheq"/g' $NODE_HOME/config/app.toml
 done
