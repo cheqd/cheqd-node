003e67cbeb764e4498505cb006e3c4ce6d578df0
vladimir.shishkin <vladimir.shishkin@evernym.com>
2021-10-11 17:13:42 +0300

    debug
    Signed-off-by: vladimir.shishkin <vladimir.shishkin@evernym.com>

--- tests/networks/docker_compose/add_observer.sh
+++ tests/networks/docker_compose/add_observer.sh
@@ -2,10 +2,10 @@
 
 cd
 cheqd-noded init node5
-cp ${{ env.NODE_CONFIGS_BASE }}/node0/.cheqdnode/config/genesis.json ~/.cheqdnode/config/
+cp ${{NODE_CONFIGS_BASE}/node0/.cheqdnode/config/genesis.json ~/.cheqdnode/config/
 NODE0_IP=$(docker inspect -f {{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}} docker_compose_node0_1)
 echo $NODE0_IP
-PEER0=$(cat ${{ env.NODE_CONFIGS_BASE }}/node0/node_id.txt)@$NODE0_IP:26656
+PEER0=$(cat ${NODE_CONFIGS_BASE}/node0/node_id.txt)@$NODE0_IP:26656
 echo $PEER0
 sed -ri "s|persistent_peers = \".*\"|persistent_peers = \"${PEER0}\"|" ~/.cheqdnode/config/config.toml
 sed -ri "s|laddr = \"tcp://127.0.0.1:26657\"|laddr = \"tcp://127.0.0.1:26677\"|" ~/.cheqdnode/config/config.toml
