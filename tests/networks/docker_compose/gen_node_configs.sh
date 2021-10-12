cfb85a8876dd3b16e9b3278d2d6c886e0c57de57
vladimir.shishkin <vladimir.shishkin@evernym.com>
2021-10-12 14:38:42 +0300

    remove all 777
    Signed-off-by: vladimir.shishkin <vladimir.shishkin@evernym.com>

--- tests/networks/docker_compose/gen_node_configs.sh
+++ tests/networks/docker_compose/gen_node_configs.sh
@@ -15,7 +15,7 @@ source "../common.sh"
 NODE_CONFIGS_DIR="node_configs"
 rm -rf $NODE_CONFIGS_DIR
 mkdir $NODE_CONFIGS_DIR
-chmod -R 777 $NODE_CONFIGS_DIR # FIXME
+# chmod -R 777 $NODE_CONFIGS_DIR # FIXME
 pushd $NODE_CONFIGS_DIR
 
 echo "Generating validator keys..."
@@ -24,7 +24,7 @@ for ((i=0 ; i<$VALIDATORS_COUNT ; i++))
 do
     NODE_HOME="node$i"
     mkdir $NODE_HOME
-    chmod -R 777 $NODE_HOME # FIXME
+    # chmod -R 777 $NODE_HOME # FIXME
     pushd $NODE_HOME
 
     echo "[Validator $i] Generating key..."
@@ -35,8 +35,8 @@ do
 
     echo "Setting minimum fee price..."
 
-    sudo chmod -R 777 /home/runner/work/cheqd-node/cheqd-node/tests/networks/docker_compose/node_configs || echo "I'm not in pipeline" # FIXME
-    sudo chmod -R 777 ~/cheqd-node/tests/networks/docker_compose/node_configs || echo "I'm not in local machine" # FIXME
+    # sudo chmod -R 777 /home/runner/work/cheqd-node/cheqd-node/tests/networks/docker_compose/node_configs || echo "I'm not in pipeline" # FIXME
+    # sudo chmod -R 777 ~/cheqd-node/tests/networks/docker_compose/node_configs || echo "I'm not in local machine" # FIXME
     sed -i $sed_extension 's/minimum-gas-prices = ""/minimum-gas-prices = "25ncheq"/g' .cheqdnode/config/app.toml
 
     popd
@@ -45,13 +45,13 @@ done
 
 OPERATORS_HOME="client"
 mkdir $OPERATORS_HOME
-chmod -R 777 $OPERATORS_HOME # FIXME
+# chmod -R 777 $OPERATORS_HOME # FIXME
 pushd $OPERATORS_HOME
 
 echo "Initializing genesis..."
 cheqd_noded_docker init dummy_node --chain-id $CHAIN_ID
-sudo chmod -R 777 /home/runner/work/cheqd-node/cheqd-node/tests/networks/docker_compose/node_configs || echo "I'm not in pipeline" # FIXME
-sudo chmod -R 777 ~/cheqd-node/tests/networks/docker_compose/node_configs || echo "I'm not in local machine" # FIXME
+# sudo chmod -R 777 /home/runner/work/cheqd-node/cheqd-node/tests/networks/docker_compose/node_configs || echo "I'm not in pipeline" # FIXME
+# sudo chmod -R 777 ~/cheqd-node/tests/networks/docker_compose/node_configs || echo "I'm not in local machine" # FIXME
 sed -i $sed_extension 's/"stake"/"ncheq"/' .cheqdnode/config/genesis.json
 
 echo "Generating operator keys..."
@@ -108,7 +108,7 @@ do
     NODE_HOME="observer$i"
 
     mkdir $NODE_HOME
-    chmod -R 777 $NODE_HOME # FIXME
+    # chmod -R 777 $NODE_HOME # FIXME
     pushd $NODE_HOME
 
     echo "##### [Observer $i] Generating keys..."
@@ -120,8 +120,8 @@ do
 
     echo "##### [Observer $i] Loading genesis..."
     OPERATORS_HOME="../client"
-    sudo chmod -R 777 /home/runner/work/cheqd-node/cheqd-node/tests/networks/docker_compose/node_configs || echo "I'm not in pipeline" # FIXME
-    sudo chmod -R 777 ~/cheqd-node/tests/networks/docker_compose/node_configs || echo "I'm not in local machine" # FIXME
+    # sudo chmod -R 777 /home/runner/work/cheqd-node/cheqd-node/tests/networks/docker_compose/node_configs || echo "I'm not in pipeline" # FIXME
+    # sudo chmod -R 777 ~/cheqd-node/tests/networks/docker_compose/node_configs || echo "I'm not in local machine" # FIXME
     cp "$OPERATORS_HOME/.cheqdnode/config/genesis.json" ".cheqdnode/config/"
 
     echo "##### [Observer $i] Setting min gas prices..."
