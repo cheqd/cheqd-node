04c5392243e1c4461e24f73fa55e2eeb8b28f5ee
vladimir.shishkin <vladimir.shishkin@evernym.com>
2021-10-13 16:17:32 +0300

    debug
    Signed-off-by: vladimir.shishkin <vladimir.shishkin@evernym.com>

--- tests/networks/docker_compose/wait.sh
+++ tests/networks/docker_compose/wait.sh
@@ -5,12 +5,12 @@ set -euox pipefail
 cmd="$1"
 
 for i in 1 2 3 4 5 6; do
+    sleep 30
     if eval $cmd; then
         echo "Waiter returned success!"
         exit 0
     else
         echo "Waiter returned failure. Retrying..."
-        sleep 60
     fi
 done
 
