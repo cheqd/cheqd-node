e8d0d212157d1d53ba1e9a6f470e35c4348549d6
vladimir.shishkin <vladimir.shishkin@evernym.com>
2021-10-13 15:24:18 +0300

    debug all assertions
    Signed-off-by: vladimir.shishkin <vladimir.shishkin@evernym.com>

--- tests/networks/docker_compose/wait.sh
+++ tests/networks/docker_compose/wait.sh
@@ -2,13 +2,13 @@
 
 cmd="$1"
 
-for i in 1 2 3 4; do
+for i in 1 2 3 4 5 6; do
     if eval $cmd; then
         echo "Waiter returned success!"
         exit 0
     else
         echo "Waiter returned fail. Retrying..."
-        sleep 30
+        sleep 60
     fi
 done
 
