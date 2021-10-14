11cdf4dd47fcfeca05358462c3f0135c62296cad
Alexander Kolesov <alexander.kolesov@evernym.com>
2021-10-14 14:36:40 +0300

    Update add_observer.sh

--- tests/networks/docker_compose/add_observer.sh
+++ tests/networks/docker_compose/add_observer.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
+
 sudo chmod -R 777 /etc/cheqd-node
-cd
-cheqd-noded init node5
-# cp ${NODE_CONFIGS_BASE}/node0/.cheqdnode/config/genesis.json ~/.cheqdnode/config/
-cp ${NODE_CONFIGS_BASE}/node0/.cheqdnode/config/genesis.json /etc/cheqd-node/
-NODE0_IP=$(docker inspect -f {{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}} docker_compose_node0_1)
+
+NODE0_IP="127.0.0.1"
 PEER0=$(cat ${NODE_CONFIGS_BASE}/node0/node_id.txt)@$NODE0_IP:26656
-sed -ri "s|persistent_peers = \".*\"|persistent_peers = \"${PEER0}\"|" ~/.cheqdnode/config/config.toml
-sed -ri "s|laddr = \"tcp://127.0.0.1:26657\"|laddr = \"tcp://127.0.0.1:26677\"|" ~/.cheqdnode/config/config.toml
-sed -ri "s|laddr = \"tcp://0.0.0.0:26656\"|laddr = \"tcp://0.0.0.0:26676\"|" ~/.cheqdnode/config/config.toml
-cp ~/.cheqdnode/config/config.toml /etc/cheqd-node/config.toml
-# cheqd-noded start
+
+sudo su cheqd
+
+# Init
+cheqd-noded init node5
+
+# Genesis
+cp ${NODE_CONFIGS_BASE}/node0/.cheqdnode/config/genesis.json /etc/cheqd-node/
+
+# Config
+sed -ri "s|persistent_peers = \".*\"|persistent_peers = \"${PEER0}\"|" /etc/cheqd-node/config.toml
+sed -ri "s|laddr = \"tcp://127.0.0.1:26657\"|laddr = \"tcp://127.0.0.1:26677\"|" /etc/cheqd-node/config.toml
+sed -ri "s|laddr = \"tcp://0.0.0.0:26656\"|laddr = \"tcp://0.0.0.0:26676\"|" /etc/cheqd-node/config.toml
+
+exit
+
 sudo systemctl start cheqd-noded
 systemctl status cheqd-noded
