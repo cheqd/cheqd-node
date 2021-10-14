1e54a1a0032a5c2d9529bbf67f2d905a63a10363
Alexander Kolesov <alexander.kolesov@evernym.com>
2021-10-14 14:42:23 +0300

    Update add_observer.sh

--- tests/networks/docker_compose/add_observer.sh
+++ tests/networks/docker_compose/add_observer.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+# Init node5 keys and configs
+sudo su cheqd
+cheqd-noded init node5
+exit
+
 sudo chmod -R 777 /etc/cheqd-node
 
 NODE0_IP="127.0.0.1"
 PEER0=$(cat ${NODE_CONFIGS_BASE}/node0/node_id.txt)@$NODE0_IP:26656
-
-sudo su cheqd
-
-# Init
-cheqd-noded init node5
 
 # Genesis
 cp ${NODE_CONFIGS_BASE}/node0/.cheqdnode/config/genesis.json /etc/cheqd-node/
@@ -17,8 +17,6 @@ cp ${NODE_CONFIGS_BASE}/node0/.cheqdnode/config/genesis.json /etc/cheqd-node/
 sed -ri "s|persistent_peers = \".*\"|persistent_peers = \"${PEER0}\"|" /etc/cheqd-node/config.toml
 sed -ri "s|laddr = \"tcp://127.0.0.1:26657\"|laddr = \"tcp://127.0.0.1:26677\"|" /etc/cheqd-node/config.toml
 sed -ri "s|laddr = \"tcp://0.0.0.0:26656\"|laddr = \"tcp://0.0.0.0:26676\"|" /etc/cheqd-node/config.toml
-
-exit
 
 sudo systemctl start cheqd-noded
 systemctl status cheqd-noded
