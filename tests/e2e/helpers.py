c04f4a015fbf97444a734ed571d91512677b96fc
Vladimir Shishkin <vladimir.shishkin@dsr-corporation.com>
2021-10-18 16:53:55 +0300

    add helpes and fixtures
    Signed-off-by: Vladimir Shishkin <vladimir.shishkin@dsr-corporation.com>

---
+++ tests/e2e/helpers.py
@@ -0,0 +1,31 @@
+import sys
+import pexpect
+
+
+IMPLICIT_TIMEOUT = 30
+ENCODING = "utf-8"
+READ_BUFFER = 6000
+
+TEST_NET_DESTINATION = "--node 'tcp://seed1.us.testnet.cheqd.network:26657' --chain-id 'cheqd-testnet-2'"
+TEST_NET_FEES = "--fees 5000000ncheq"
+TEST_NET_GAS_X_GAS_PRICES = "--gas 70000 --gas-prices 25ncheq"
+YES_FLAG = "-y"
+
+SENDER_ADDRESS = "cheqd1ece09txhq6nm9fkft9jh3mce6e48ftescs5jsw"
+SENDER_MNEMONIC = "oil long siege student rent jar awkward park entry ripple enable company sort people little damp arrange wise slender push brief solve tattoo cycle"
+RECEIVER_ADDRESS= "cheqd16d72a6kusmzml5mjhzjv63c9j5xnpsyqs8f3sk"
+RECEIVER_MNEMONIC = "strike impact earth indoor man illness virus genuine rib control antenna loop neck rotate bargain original nasty size either try snap quiz stairs huge"
+
+
+def run(command_base, command, params, expected_output):
+    cli = pexpect.spawn(f"{command_base} {command} {params}", encoding=ENCODING, timeout=IMPLICIT_TIMEOUT, maxread=READ_BUFFER)
+    cli.logfile = sys.stdout
+    cli.expect(expected_output)
+    print(f"BEFORE >>> {cli.before}") # FIXME DEBUG
+    print(f"AFTER >>> {cli.after}") # FIXME DEBUG
+    return cli
+
+
+def run_interaction(cli, input_string, expected_output):
+    cli.sendline(input_string)
+    cli.expect(expected_output)
--- tests/e2e/test_cli.py
+++ tests/e2e/test_cli.py
@@ -1,38 +1,16 @@
-import sys
-import os
 import re
-import pexpect
 import pytest
+from helpers import run, run_interaction, \
+    TEST_NET_DESTINATION, TEST_NET_FEES, TEST_NET_GAS_X_GAS_PRICES, YES_FLAG, \
+    SENDER_ADDRESS, SENDER_MNEMONIC, RECEIVER_ADDRESS, RECEIVER_MNEMONIC
 
 
-IMPLICIT_TIMEOUT = 30
-ENCODING = "utf-8"
-READ_BUFFER = 6000
-TEST_NET_DESTINATION = "--node 'tcp://seed1.us.testnet.cheqd.network:26657' --chain-id 'cheqd-testnet-2'"
-TEST_NET_FEES = "--fees 5000000ncheq"
-TEST_NET_GAS_X_GAS_PRICES = "--gas 70000 --gas-prices 25ncheq"
-YES_FLAG = "-y"
-
-sender = "cheqd1ece09txhq6nm9fkft9jh3mce6e48ftescs5jsw"
-receiver = "cheqd16d72a6kusmzml5mjhzjv63c9j5xnpsyqs8f3sk"
-
-
-def run(command_base, command, params, expected_output):
-    cli = pexpect.spawn(f"{command_base} {command} {params}", encoding=ENCODING, timeout=IMPLICIT_TIMEOUT, maxread=READ_BUFFER)
-    cli.logfile = sys.stdout
-    cli.expect(expected_output)
-    print(f"BEFORE >>> {cli.before}")
-    print(f"AFTER >>> {cli.after}")
-    return cli
-
-
-@pytest.mark.skip
+# @pytest.mark.skip
 @pytest.mark.parametrize(
         "command, params, expected_output",
         [
             ("help", "",r"cheqd App(.*?)Usage:(.*?)Available Commands:(.*?)Flags:"),
-            # ("version", "",os.environ["RELEASE_NUMBER"]), # this works against deb package but not against starport build
-            ("status", "",r"\"NodeInfo\"(.*?)\"network\":\"cheqd\"(.*?)\"moniker\":\"node0\""),
+            ("status", "--node 'tcp://seed1.us.testnet.cheqd.network:26657'",r"\"NodeInfo\"(.*?)\"network\":\"cheqd-testnet-2\"(.*?)\"moniker\":\"seed1-us-testnet-cheqd\""),
         ]
     )
 def test_basic(command, params, expected_output):
@@ -40,7 +18,7 @@ def test_basic(command, params, expected_output):
     run(command_base, command, params, expected_output)
 
 
-@pytest.mark.skip
+# @pytest.mark.skip
 @pytest.mark.parametrize(
         "command, params, expected_output",
         [
@@ -58,14 +36,12 @@ def test_keys(command, params, expected_output):
     run(command_base, command, params, expected_output)
 
 
-# use fixture to send with note and then check memo
-@pytest.mark.skip
+# @pytest.mark.skip
 @pytest.mark.parametrize(
         "command, params, expected_output",
         [
             ("staking validators", f"{TEST_NET_DESTINATION}", r"pagination:(.*?)validators:"),
-            ("bank balances", f"{sender} {TEST_NET_DESTINATION}", r"balances:(.*?)amount:(.*?)denom: ncheq(.*?)pagination:"),
-            # ("tx", f"{tx_hash} {TEST_NET_DESTINATION}", f"{tx_memo}"),
+            ("bank balances", f"{SENDER_ADDRESS} {TEST_NET_DESTINATION}", r"balances:(.*?)amount:(.*?)denom: ncheq(.*?)pagination:"),
         ]
     )
 def test_query(command, params, expected_output):
@@ -73,21 +49,21 @@ def test_query(command, params, expected_output):
     run(command_base, command, params, expected_output)
 
 
-# use fixture to recover sender + receiver keys
-@pytest.mark.skip
+@pytest.mark.usefixtures('restore_test_keys')
+# @pytest.mark.skip
 @pytest.mark.parametrize(
         "command, params, expected_output",
         [
             ("bank send", "", r"Error: accepts 3 arg\(s\), received 0"), # no args
-            ("bank send", f"{sender} {receiver} 0ncheq {TEST_NET_DESTINATION} {TEST_NET_FEES} {YES_FLAG}", r"Error: : invalid coins"), # 0
-            ("bank send", f"{sender} {receiver} 1ncheq {TEST_NET_DESTINATION} {TEST_NET_FEES} {YES_FLAG}", r"\"code\":0(.*?)\"value\":\"1ncheq\""), # 1 + fees
-            ("bank send", f"{sender} {receiver} 2ncheq {TEST_NET_DESTINATION} {TEST_NET_GAS_X_GAS_PRICES} {YES_FLAG}", r"\"code\":0(.*?)\"value\":\"2ncheq\""), # 2 + gas x price
-            ("bank send", f"{sender} {receiver} 99ncheq {TEST_NET_DESTINATION} {TEST_NET_FEES} {YES_FLAG}", r"\"code\":0(.*?)\"value\":\"99ncheq\""),
-            ("bank send", f"{sender} {receiver} 99ncheq {TEST_NET_DESTINATION} {YES_FLAG}", r"\"code\":13(.*?)insufficient fees"),
-            ("bank send", f"{receiver} {sender} 2ncheq {TEST_NET_DESTINATION} {TEST_NET_FEES} {YES_FLAG}", r"\"code\":0(.*?)\"value\":\"2ncheq\""), # transfer back 2 + fees
-            ("bank send", f"{receiver} {sender} 1ncheq {TEST_NET_DESTINATION} {TEST_NET_GAS_X_GAS_PRICES} {YES_FLAG}", r"\"code\":0(.*?)\"value\":\"1ncheq\""), # transfer back 1 + gas x price
-            ("bank send", f"{receiver} {sender} 999999999ncheq {TEST_NET_DESTINATION} {TEST_NET_FEES} {YES_FLAG}", r"\"code\":5(.*?)insufficient funds"),
-            ("bank send", f"{sender} {receiver} 1000ncheq {TEST_NET_DESTINATION} {TEST_NET_GAS_X_GAS_PRICES} {YES_FLAG} --note 'test123!=$'", r"\"code\":0(.*?)\"value\":\"1000ncheq\""),
+            ("bank send", f"{SENDER_ADDRESS} {RECEIVER_ADDRESS} 0ncheq {TEST_NET_DESTINATION} {TEST_NET_FEES} {YES_FLAG}", r"Error: : invalid coins"), # 0
+            ("bank send", f"{SENDER_ADDRESS} {RECEIVER_ADDRESS} 1ncheq {TEST_NET_DESTINATION} {TEST_NET_FEES} {YES_FLAG}", r"\"code\":0(.*?)\"value\":\"1ncheq\""), # 1 + fees
+            ("bank send", f"{SENDER_ADDRESS} {RECEIVER_ADDRESS} 2ncheq {TEST_NET_DESTINATION} {TEST_NET_GAS_X_GAS_PRICES} {YES_FLAG}", r"\"code\":0(.*?)\"value\":\"2ncheq\""), # 2 + gas x price
+            ("bank send", f"{SENDER_ADDRESS} {RECEIVER_ADDRESS} 99ncheq {TEST_NET_DESTINATION} {TEST_NET_FEES} {YES_FLAG}", r"\"code\":0(.*?)\"value\":\"99ncheq\""),
+            ("bank send", f"{SENDER_ADDRESS} {RECEIVER_ADDRESS} 99ncheq {TEST_NET_DESTINATION} {YES_FLAG}", r"\"code\":13(.*?)insufficient fees"),
+            ("bank send", f"{RECEIVER_ADDRESS} {SENDER_ADDRESS} 2ncheq {TEST_NET_DESTINATION} {TEST_NET_FEES} {YES_FLAG}", r"\"code\":0(.*?)\"value\":\"2ncheq\""), # transfer back 2 + fees
+            ("bank send", f"{RECEIVER_ADDRESS} {SENDER_ADDRESS} 1ncheq {TEST_NET_DESTINATION} {TEST_NET_GAS_X_GAS_PRICES} {YES_FLAG}", r"\"code\":0(.*?)\"value\":\"1ncheq\""), # transfer back 1 + gas x price
+            ("bank send", f"{RECEIVER_ADDRESS} {SENDER_ADDRESS} 999999999ncheq {TEST_NET_DESTINATION} {TEST_NET_FEES} {YES_FLAG}", r"\"code\":5(.*?)insufficient funds"),
+            ("bank send", f"{SENDER_ADDRESS} {RECEIVER_ADDRESS} 1000ncheq {TEST_NET_DESTINATION} {TEST_NET_GAS_X_GAS_PRICES} {YES_FLAG} --note 'test123!=$'", r"\"code\":0(.*?)\"value\":\"1000ncheq\""),
         ]
     )
 def test_tx(command, params, expected_output):
@@ -109,9 +85,7 @@ def test_tendermint(command, params, expected_output):
     run(command_base, command, params, expected_output)
 
 
-@pytest.mark.skip
-def test_production():
-    note = "test_memo"
-    cli = run("cheqd-noded tx", "bank send", f"{sender} {receiver} 1000ncheq {TEST_NET_DESTINATION} {TEST_NET_GAS_X_GAS_PRICES} {YES_FLAG} --note {note}", r"\"code\":0(.*?)\"value\":\"1000ncheq\"")
-    tx_hash = re.search(r"\"txhash\":\"(.+?)\"", cli.before).group(1).strip()
-    print(tx_hash)
+# @pytest.mark.skip
+def test_production(send_with_note):
+    tx_hash, tx_memo = send_with_note
+    run("cheqd-noded query", "tx", f"{tx_hash} {TEST_NET_DESTINATION}", fr"code: 0(.*?)memo: {tx_memo}(.*?)txhash: {tx_hash}")
