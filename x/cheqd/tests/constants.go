1771529d9b4d91daddfc44f4a9a1c0e2a4858fbb
Alexander Martynov <alexander.martynov@dsr-corporation.com>
2021-10-20 23:58:09 +0300

    added fixtures

---
+++ x/cheqd/tests/constants.go
@@ -0,0 +1,14 @@
+package tests
+
+const (
+	AliceDID    = "did:cheqd:test:alice"
+	BobDID      = "did:cheqd:test:bob"
+	CharlieDID  = "did:cheqd:test:charlie"
+	AliceKey1   = AliceDID + "#key-1"
+	BobKey1     = BobDID + "#key-1"
+	BobKey2     = BobDID + "#key-2"
+	BobKey3     = BobDID + "#key-3"
+	CharlieKey1 = CharlieDID + "#key-1"
+	CharlieKey2 = CharlieDID + "#key-2"
+	CharlieKey3 = CharlieDID + "#key-3"
+)
--- x/cheqd/tests/create_did_test.go
+++ x/cheqd/tests/create_did_test.go
@@ -4,6 +4,7 @@ import (
 	"crypto/ed25519"
 	"github.com/btcsuite/btcutil/base58"
 	"github.com/cheqd/cheqd-node/x/cheqd/types"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
@@ -15,83 +16,83 @@ func TestCreateDID(t *testing.T) {
 	}{
 		{
 			keys: map[string]KeyPair{
-				"did:cheqd:test:alice#key-1": GenerateKeyPair(),
+				AliceKey1: GenerateKeyPair(),
 			},
-			signers: []string{"did:cheqd:test:alice#key-1"},
+			signers: []string{AliceKey1},
 			msg: &types.MsgCreateDid{
-				Id:             "did:cheqd:test:alice",
-				Authentication: []string{"did:cheqd:test:alice#key-1"},
+				Id:             AliceDID,
+				Authentication: []string{AliceKey1},
 				VerificationMethod: []*types.VerificationMethod{
 					{
-						Id:         "did:cheqd:test:alice#key-1",
+						Id:         AliceKey1,
 						Type:       "Ed25519VerificationKey2020",
-						Controller: "did:cheqd:test:alice",
+						Controller: AliceDID,
 					},
 				},
 			},
 		},
 		{
 			keys: map[string]KeyPair{
-				"did:cheqd:test:bob#key-1": GenerateKeyPair(),
-				"did:cheqd:test:bob#key-2": GenerateKeyPair(),
-				"did:cheqd:test:bob#key-3": GenerateKeyPair(),
+				BobKey1: GenerateKeyPair(),
+				BobKey2: GenerateKeyPair(),
+				BobKey3: GenerateKeyPair(),
 			},
-			signers: []string{"did:cheqd:test:bob#key-2"},
+			signers: []string{BobKey2},
 			msg: &types.MsgCreateDid{
-				Id: "did:cheqd:test:bob",
+				Id: BobDID,
 				Authentication: []string{
-					"did:cheqd:test:bob#key-1",
-					"did:cheqd:test:bob#key-2",
-					"did:cheqd:test:bob#key-3",
+					BobKey1,
+					BobKey2,
+					BobKey3,
 				},
 				VerificationMethod: []*types.VerificationMethod{
 					{
-						Id:         "did:cheqd:test:bob#key-1",
+						Id:         BobKey1,
 						Type:       "Ed25519VerificationKey2020",
-						Controller: "did:cheqd:test:bob",
+						Controller: BobDID,
 					},
 					{
-						Id:         "did:cheqd:test:bob#key-2",
+						Id:         BobKey2,
 						Type:       "Ed25519VerificationKey2020",
-						Controller: "did:cheqd:test:bob",
+						Controller: BobDID,
 					},
 					{
-						Id:         "did:cheqd:test:bob#key-3",
+						Id:         BobKey3,
 						Type:       "Ed25519VerificationKey2020",
-						Controller: "did:cheqd:test:bob",
+						Controller: BobDID,
 					},
 				},
 			},
 		},
 		{
 			keys: map[string]KeyPair{
-				"did:cheqd:test:charlie#key-1": GenerateKeyPair(),
-				"did:cheqd:test:charlie#key-2": GenerateKeyPair(),
-				"did:cheqd:test:charlie#key-3": GenerateKeyPair(),
+				CharlieKey1: GenerateKeyPair(),
+				CharlieKey2: GenerateKeyPair(),
+				CharlieKey3: GenerateKeyPair(),
 			},
-			signers: []string{"did:cheqd:test:charlie#key-2"},
+			signers: []string{CharlieKey2},
 			msg: &types.MsgCreateDid{
-				Id: "did:cheqd:test:charlie",
+				Id: CharlieDID,
 				Authentication: []string{
-					"did:cheqd:test:charlie#key-1",
-					"did:cheqd:test:charlie#key-2",
-					"did:cheqd:test:charlie#key-3",
+					CharlieKey1,
+					CharlieKey2,
+					CharlieKey3,
 				},
 				VerificationMethod: []*types.VerificationMethod{
 					{
-						Id:         "did:cheqd:test:charlie#key-1",
+						Id:         CharlieKey1,
 						Type:       "Ed25519VerificationKey2020",
-						Controller: "did:cheqd:test:bob",
+						Controller: BobDID,
 					},
 					{
-						Id:         "did:cheqd:test:charlie#key-2",
+						Id:         CharlieKey2,
 						Type:       "Ed25519VerificationKey2020",
-						Controller: "did:cheqd:test:bob",
+						Controller: BobDID,
 					},
 					{
-						Id:         "did:cheqd:test:charlie#key-3",
+						Id:         CharlieKey3,
 						Type:       "Ed25519VerificationKey2020",
-						Controller: "did:cheqd:test:bob",
+						Controller: BobDID,
 					},
 				},
 			},
@@ -99,8 +100,7 @@ func TestCreateDID(t *testing.T) {
 	}
 
 	setup := Setup()
-
-	var prefilledPrivateKeys = map[string]map[string]ed25519.PrivateKey{}
+	keys := map[string]KeyPair{}
 
 	for _, prefilled := range prefilledDids {
 		msg := prefilled.msg
@@ -112,106 +112,102 @@ func TestCreateDID(t *testing.T) {
 		signerKeys := map[string]ed25519.PrivateKey{}
 		for _, signer := range prefilled.signers {
 			signerKeys[signer] = prefilled.keys[signer].PrivateKey
+			keys[signer] = prefilled.keys[signer]
 		}
 
-		prefilledPrivateKeys[msg.Id] = signerKeys
 		_, _ = setup.SendCreateDid(msg, signerKeys)
 	}
 
-	/*
-		cases := []struct {
-			valid  bool
-			name string
-			keys map[string]KeyPair
-			signers []string
-			msg *types.MsgCreateDid
-			errMsg string
-		}{
-			{
-				valid: true,
-				name: "Create DID works",
-				keys: map[string]KeyPair{
-					"did:cheqd:test:123456qwertyui#key-1" : GenerateKeyPair(),
-				},
-				signers: []string{"did:cheqd:test:123456qwertyui#key-1"},
-				msg: &types.MsgCreateDid{
-					Id: "did:cheqd:test:123456qwertyui",
-					Authentication: []string{"did:cheqd:test:123456qwertyui#key-1"},
-					VerificationMethod: []*types.VerificationMethod {
-						{
-							Id: "did:cheqd:test:123456qwertyui#key-1",
-							Type: "Ed25519VerificationKey2020",
-							Controller: "did:cheqd:test:123456qwertyui",
-						},
+	cases := []struct {
+		valid   bool
+		name    string
+		keys    map[string]KeyPair
+		signers []string
+		msg     *types.MsgCreateDid
+		errMsg  string
+	}{
+		{
+			valid: true,
+			name:  "Create DID works",
+			keys: map[string]KeyPair{
+				"did:cheqd:test:123456qwertyui#key-1": GenerateKeyPair(),
+			},
+			signers: []string{"did:cheqd:test:123456qwertyui#key-1"},
+			msg: &types.MsgCreateDid{
+				Id:             "did:cheqd:test:123456qwertyui",
+				Authentication: []string{"did:cheqd:test:123456qwertyui#key-1"},
+				VerificationMethod: []*types.VerificationMethod{
+					{
+						Id:         "did:cheqd:test:123456qwertyui#key-1",
+						Type:       "Ed25519VerificationKey2020",
+						Controller: "did:cheqd:test:123456qwertyui",
 					},
 				},
 			},
-			{
-				valid: true,
-				name: "Create DID with controller works",
-				msg: &types.MsgCreateDid{
-					Id: "did:cheqd:test:controller1",
-					Controller: []string {"did:cheqd:test:alice", "did:cheqd:test:bob"},
-				},
-				signers: []string {"did:cheqd:test:alice#key-1", "did:cheqd:test:bob"},
-				keys: map {prefilledPrivateKeys[],
+		},
+		{
+			valid: true,
+			name:  "Create DID with controller works",
+			msg: &types.MsgCreateDid{
+				Id:         "did:cheqd:test:controller1",
+				Controller: []string{AliceDID, BobDID},
 			},
-			{
-				valid: false,
-				name: "DID Doc already exists",
-				keys: map[string]KeyPair{
-					"did:cheqd:test:123456qwertyui#key-1" : GenerateKeyPair(),
-				},
-				signers: []string{"did:cheqd:test:123456qwertyui#key-1"},
-				msg: &types.MsgCreateDid{
-					Id: "did:cheqd:test:123456qwertyui",
-					Authentication: []string{"did:cheqd:test:123456qwertyui#key-1"},
-					VerificationMethod: []*types.VerificationMethod {
-						{
-							Id: "did:cheqd:test:123456qwertyui#key-1",
-							Type: "Ed25519VerificationKey2020",
-							Controller: "did:cheqd:test:123456qwertyui",
-						},
+			signers: []string{AliceKey1, BobKey2},
+		},
+		{
+			valid: false,
+			name:  "DID Doc already exists",
+			keys: map[string]KeyPair{
+				"did:cheqd:test:123456qwertyui#key-1": GenerateKeyPair(),
+			},
+			signers: []string{"did:cheqd:test:123456qwertyui#key-1"},
+			msg: &types.MsgCreateDid{
+				Id:             "did:cheqd:test:123456qwertyui",
+				Authentication: []string{"did:cheqd:test:123456qwertyui#key-1"},
+				VerificationMethod: []*types.VerificationMethod{
+					{
+						Id:         "did:cheqd:test:123456qwertyui#key-1",
+						Type:       "Ed25519VerificationKey2020",
+						Controller: "did:cheqd:test:123456qwertyui",
 					},
 				},
-				errMsg: "DID DOC already exists for DID did:cheqd:test:123456qwertyui: DID Doc exists",
 			},
-		}
+			errMsg: "DID DOC already exists for DID did:cheqd:test:123456qwertyui: DID Doc exists",
+		},
+	}
 
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			msg := tc.msg
 
+			for i, key := range msg.Authentication {
+				msg.VerificationMethod[i].PublicKeyMultibase = "z" + base58.Encode(tc.keys[key].PublicKey)
+			}
 
-		for _, tc := range cases {
-			t.Run(tc.name, func(t *testing.T) {
-				msg := tc.msg
+			signerKeys := map[string]ed25519.PrivateKey{}
+			for _, signer := range tc.signers {
+				signerKeys[signer] = tc.keys[signer].PrivateKey
+			}
 
-				for i, key := range msg.Authentication {
-					msg.VerificationMethod[i].PublicKeyMultibase = "z" + base58.Encode(tc.keys[key].PublicKey)
-				}
+			did, err := setup.SendCreateDid(msg, signerKeys)
 
-				signerKeys := map[string]ed25519.PrivateKey{}
-				for _, signer := range tc.signers {
-					signerKeys[signer] = tc.keys[signer].PrivateKey
-				}
-
-				did, err := setup.SendCreateDid(msg, signerKeys)
-
-				if tc.valid {
-					require.Nil(t, err)
-					require.Equal(t, did.Id, did.Id)
-					require.Equal(t, did.Controller, did.Controller)
-					require.Equal(t, did.VerificationMethod, did.VerificationMethod)
-					require.Equal(t, did.Authentication, did.Authentication)
-					require.Equal(t, did.AssertionMethod, did.AssertionMethod)
-					require.Equal(t, did.CapabilityInvocation, did.CapabilityInvocation)
-					require.Equal(t, did.CapabilityDelegation, did.CapabilityDelegation)
-					require.Equal(t, did.KeyAgreement, did.KeyAgreement)
-					require.Equal(t, did.AlsoKnownAs, did.AlsoKnownAs)
-					require.Equal(t, did.Service, did.Service)
-					require.Equal(t, did.Context, did.Context)
-				} else {
-					require.Error(t, err)
-					require.Equal(t, tc.errMsg, err.Error())
-				}
-			})
-		}*/
+			if tc.valid {
+				require.Nil(t, err)
+				require.Equal(t, did.Id, did.Id)
+				require.Equal(t, did.Controller, did.Controller)
+				require.Equal(t, did.VerificationMethod, did.VerificationMethod)
+				require.Equal(t, did.Authentication, did.Authentication)
+				require.Equal(t, did.AssertionMethod, did.AssertionMethod)
+				require.Equal(t, did.CapabilityInvocation, did.CapabilityInvocation)
+				require.Equal(t, did.CapabilityDelegation, did.CapabilityDelegation)
+				require.Equal(t, did.KeyAgreement, did.KeyAgreement)
+				require.Equal(t, did.AlsoKnownAs, did.AlsoKnownAs)
+				require.Equal(t, did.Service, did.Service)
+				require.Equal(t, did.Context, did.Context)
+			} else {
+				require.Error(t, err)
+				require.Equal(t, tc.errMsg, err.Error())
+			}
+		})
+	}
 }
