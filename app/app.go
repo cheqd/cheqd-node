49206a730b6c9eeefcf606461d2340f2460dd3a3
Andrew Nikitin <andrew.nikitin@evernym.com>
2021-11-01 17:52:16 +0300

    Add upgrade handler for future proposal upgrade_to_0.3.0
    Signed-off-by: Andrew Nikitin <andrew.nikitin@evernym.com>

--- app/app.go
+++ app/app.go
@@ -306,6 +306,12 @@ func New(
 	)
 	app.UpgradeKeeper = upgradekeeper.NewKeeper(skipUpgradeHeights, keys[upgradetypes.StoreKey], appCodec, homePath, app.BaseApp)
 
+	// Upgrade handler
+	app.UpgradeKeeper.SetUpgradeHandler("upgrade_to_0.3.0", func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		ctx.Logger().Info("Handler for upgrade plan: upgrade_to_0.3.0")
+		return fromVM, nil
+	})
+
 	// register the staking hooks
 	// NOTE: stakingKeeper above is passed by reference, so that it will contain these hooks
 	app.StakingKeeper = *stakingKeeper.SetHooks(
