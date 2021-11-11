145c559dd2c2b87e862813eb889511b334a7f3cd
Alexander Kolesov <alexander.kolesov@evernym.com>
2021-11-11 21:10:41 +0300

    Fix add_observer

--- tests/networks/docker_compose/add_observer.sh
+++ tests/networks/docker_compose/add_observer.sh
@@ -9,17 +9,12 @@ NODE0_ID=$(cat "${NODE_CONFIGS_BASE}/node0/node_id.txt")
 NODE0_PEER_INFO="${NODE0_ID}@127.0.0.1:26656"
 
 cheqd-noded configure p2p persistent-peers "${NODE0_PEER_INFO}"
-#cheqd-noded configure p2p laddr "tcp://0.0.0.0:26676"
-sed -ri "s|laddr = \"tcp://0.0.0.0:26656\"|laddr = \"tcp://0.0.0.0:26676\"|" "$HOME/.cheqdnode/config/config.toml"
+cheqd-noded configure p2p laddr "tcp://0.0.0.0:26676"
 cheqd-noded configure rpc-laddr "tcp://127.0.0.1:26677"
 
-# cp ~/.cheqdnode/config/* /etc/cheqd-node/               # /var/lib/cheqd/.cheqdnode/config    ->    /etc/cheqd-node/
-# cp ~/.cheqdnode/data/* /var/lib/cheqd/data/             # /var/lib/cheqd/.cheqdnode/data    ->    /var/lib/cheqd/data
+cp "${NODE_CONFIGS_BASE}/node0/.cheqdnode/config/genesis.json" "$HOME/.cheqdnode/config"
 
-cp "${NODE_CONFIGS_BASE}/node0/.cheqdnode/config/genesis.json" "$HOME/.cheqdnode/config/"
-
-#sudo chmod -R 777 /etc/cheqd-node
-#sudo chmod -R 777 /var/lib/cheqd/data
+sudo chmod -R 777 "$HOME/.cheqdnode"
 
 sudo systemctl start cheqd-noded
 systemctl status cheqd-noded
