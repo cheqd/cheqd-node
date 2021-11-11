d6c461a9b2bdfc1605a5203999abd5e9deb2ae9f
Andrew Nikitin <andrew.nikitin@evernym.com>
2021-11-12 02:36:05 +0300

    Debug: sudo
    Signed-off-by: Andrew Nikitin <andrew.nikitin@evernym.com>

--- tests/networks/docker_compose/add_observer.sh
+++ tests/networks/docker_compose/add_observer.sh
@@ -10,7 +10,7 @@ NODE0_ID=$(cat "${NODE_CONFIGS_BASE}/node0/node_id.txt")
 PERSISTENT_PEERS="${NODE0_ID}@127.0.0.1:26656"
 sudo -u cheqd cheqd-noded configure p2p persistent-peers "${PERSISTENT_PEERS}"
 
-cp "${NODE_CONFIGS_BASE}/node0/.cheqdnode/config/genesis.json" "/home/runner/cheqd/.cheqdnode/config"
+sudo cp "${NODE_CONFIGS_BASE}/node0/.cheqdnode/config/genesis.json" "/home/runner/cheqd/.cheqdnode/config"
 
 sudo chmod -R 777 "/home/runner/cheqd/.cheqdnode"
 
