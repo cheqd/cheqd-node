a63ca86fd3585532034d2163f2e29116633a6ddf
Alexander Kolesov <alexander.kolesov@evernym.com>
2021-11-11 17:46:43 +0300

    Fix node runner

--- tests/networks/docker_compose/run_docker.sh
+++ tests/networks/docker_compose/run_docker.sh
@@ -4,7 +4,16 @@
 
 set -euox pipefail
 
-source "../common.sh"
+set -euox pipefail
+
+# sed in macos requires extra argument
+
+sed_extension=''
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sed_extension=''
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    sed_extension='.orig'
+fi
 
 pushd "node_configs/node0"
 
