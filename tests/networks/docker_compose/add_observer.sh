85655e8b568ea1abc9f29a2d811db2e40d532ca1
Andrew Nikitin <andrew.nikitin@evernym.com>
2021-11-12 01:41:46 +0300

    Debug:: run as cheqd user
    Signed-off-by: Andrew Nikitin <andrew.nikitin@evernym.com>

--- tests/networks/docker_compose/add_observer.sh
+++ tests/networks/docker_compose/add_observer.sh
@@ -8,12 +8,8 @@ NODE0_ID=$(cat "${NODE_CONFIGS_BASE}/node0/node_id.txt")
 PERSISTENT_PEERS="${NODE0_ID}@127.0.0.1:26656"
 cheqd-noded configure p2p persistent-peers "${PERSISTENT_PEERS}"
 
-cp "${NODE_CONFIGS_BASE}/node0/.cheqdnode/config/genesis.json" "$HOME/.cheqdnode/config"
+cp "${NODE_CONFIGS_BASE}/node0/.cheqdnode/config/genesis.json" "/home/runner/cheqd/.cheqdnode/config"
 
 cheqd-noded configure p2p laddr "tcp://0.0.0.0:26676"
 cheqd-noded configure rpc-laddr "tcp://0.0.0.0:26677"
 
-sudo chmod -R 777 "$HOME/.cheqdnode"
-
-#sudo systemctl start cheqd-noded
-cheqd-noded start
