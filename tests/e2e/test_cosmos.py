50486201f9a41aa4fb4f30e3b8bf7a9a214eafb4
Vladimir Shishkin <vladimir.shishkin@dsr-corporation.com>
2021-11-12 15:46:17 +0300

    update
    Signed-off-by: Vladimir Shishkin <vladimir.shishkin@dsr-corporation.com>

--- tests/e2e/test_cosmos.py
+++ tests/e2e/test_cosmos.py
@@ -277,7 +277,7 @@ def test_tendermint(command, params, expected_output):
     run(command_base, command, params, expected_output)
 
 
-@settings(deadline=None, max_examples=1) # 20
+@settings(deadline=None, max_examples=20) # 20
 @given(note=strategies.text(ascii_letters, min_size=2, max_size=1024)) # start from 2 - cosmos issue with single word memo
 def test_memo(note): # intermittent failures here due to `Internal error: timed out waiting for tx to be included in a block`
     tx_hash, tx_memo = send_with_note(note)
@@ -288,7 +288,7 @@ def test_memo(note): # intermittent failures here due to `Internal error: timed 
         run("cheqd-noded query", "tx", f"{tx_hash} {LOCAL_NET_DESTINATION}", fr"code: 0(.*?)memo: {tx_memo}(.*?)txhash: {tx_hash}") # check that txn has correct memo value
 
 
-@settings(deadline=None, max_examples=1) # 20
+@settings(deadline=None, max_examples=20) # 20
 @given(value=strategies.integers(min_value=1, max_value=999999999))
 def test_token_transfer(value):
     sender_balance = get_balance(LOCAL_SENDER_ADDRESS, LOCAL_NET_DESTINATION)
