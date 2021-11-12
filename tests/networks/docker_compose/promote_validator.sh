c9fdb7f96f2c9a03134ffe789854bf4b94959715
Andrew Nikitin <andrew.nikitin@evernym.com>
2021-11-12 16:16:44 +0300

    Debug:: fix operators sequences
    Signed-off-by: Andrew Nikitin <andrew.nikitin@evernym.com>

--- tests/networks/docker_compose/promote_validator.sh
+++ tests/networks/docker_compose/promote_validator.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 sudo chown -R runner:docker ${NODE_CONFIGS_BASE}/client
-export HOME=${NODE_CONFIGS_BASE}/client
-ls -la ${NODE_CONFIGS_BASE}/node0/.cheqdnode/
-cheqd-noded keys list --keyring-backend "test" --home ${NODE_CONFIGS_BASE}/node0/
-OP0_ADDRESS=$(cheqd-noded keys list --keyring-backend "test" --home ${NODE_CONFIGS_BASE}/node0/ | sed -nr 's/.*address: (.*?).*/\1/p' | sed -n 1p | sed 's/\r//g')
+cheqd-noded keys list --keyring-backend "test" --home ${NODE_CONFIGS_BASE}/client/
+
+# Get operator0 address by setting --home flag
+OP0_ADDRESS=$(cheqd-noded keys list --keyring-backend "test" --home ${NODE_CONFIGS_BASE}/client/ | sed -nr 's/.*address: (.*?).*/\1/p' | sed -n 1p | sed 's/\r//g')
+
+# Create operator5 by running it under the `cheqd` user.
 sudo -u cheqd cheqd-noded keys add node5-operator --keyring-backend "test"
 OP5_ADDRESS=$(sudo -u cheqd cheqd-noded keys list --keyring-backend "test"| sed -nr 's/.*address: (.*?).*/\1/p' | sed -n 1p | sed 's/\r//g')
-export HOME=/home/runner
+
 NODE5_PUBKEY=$(sudo -u cheqd cheqd-noded tendermint show-validator | sed 's/\r//g')
-HOME=${NODE_CONFIGS_BASE}/client sudo -u cheqd cheqd-noded tx bank send ${OP0_ADDRESS} ${OP5_ADDRESS} 1100000000000000ncheq --chain-id cheqd --fees 5000000ncheq --node "http://localhost:26657" -y --keyring-backend "test"
-HOME=${NODE_CONFIGS_BASE}/client sudo -u cheqd cheqd-noded tx staking create-validator --amount 1000000000000000ncheq --from node5-operator --chain-id cheqd --min-self-delegation="1" --gas-prices="25ncheq" --pubkey ${NODE5_PUBKEY} --commission-max-change-rate="0.02" --commission-max-rate="0.02" --commission-rate="0.01" --gas 500000 --node "http://localhost:26657" -y --keyring-backend "test"
+# Send tokens from operator0
+cheqd-noded tx bank send ${OP0_ADDRESS} ${OP5_ADDRESS} 1100000000000000ncheq --chain-id cheqd --fees 5000000ncheq --node "http://localhost:26657" -y --keyring-backend "test" --home ${NODE_CONFIGS_BASE}/client/
+# Send promote validator from operator5
+sudo -u cheqd cheqd-noded tx staking create-validator --amount 1000000000000000ncheq --from node5-operator --chain-id cheqd --min-self-delegation="1" --gas-prices="25ncheq" --pubkey ${NODE5_PUBKEY} --commission-max-change-rate="0.02" --commission-max-rate="0.02" --commission-rate="0.01" --gas 500000 --node "http://localhost:26657" -y --keyring-backend "test"
