1c3e68ff9bd53a1e444cbfa42af61846407f12db
Alexandr Kolesov <alexandr.kolesov@dsr-corporation.com>
2021-12-03 15:01:40 +0300

    Add waiter script

---
+++ tests/networks/wait_for_chain.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -euox pipefail
+
+
+TARGET_HEIGHT=${1:-1} # Default is 1
+WAIT_TIME=${2:-100}    # In seconds, default - 10
+RPC_ENDPOINT=${3:-"http://localhost:26657/status"}
+
+WAITED=0
+WAIT_INTERVAL=1
+
+while true; do
+    sleep "${WAIT_INTERVAL}"
+    WAITED=$((WAITED + WAIT_INTERVAL))
+
+    CURRENT_HEIGHT=$(curl -s "${RPC_ENDPOINT}" | jq -r ".result.sync_info.latest_block_height" || echo "-1")
+
+    if ((CURRENT_HEIGHT >= TARGET_HEIGHT)); then
+        echo "Height ${TARGET_HEIGHT} is reached in $WAITED seconds"
+        break
+    fi
+
+    if ((WAITED > WAIT_TIME)); then
+        echo "Height $TARGET_HEIGHT is not reached in $WAIT_TIME seconds"
+        exit 1
+    fi
+
+    echo "Waiting for height: $TARGET_HEIGHT... Current height: $CURRENT_HEIGHT, time waited: $WAITED, time limit: $WAIT_TIME."
+done
