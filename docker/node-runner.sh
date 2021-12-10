0ce6d9e3a0c542c588ab328f5dfb28b6e930a5dd
jay-dee7 <jasdeepsingh.uppal@gmail.com>
2021-12-10 19:53:56 +0530

    Refactor: Docker Compose Easy Setup
    Signed-off-by: jay-dee7 <jasdeepsingh.uppal@gmail.com>

--- docker/node-runner.sh
+++ docker/node-runner.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
 # Inits node configuration and runs the node.
+# e -> exit immediately, u -> treat unset variables as errors and immediately, o -> sets the exit code to the rightmost command 
+set -euo pipefail
 
-set -euox pipefail
-
+# within the container, $HOME=/home/cheqd
 CHEQD_ROOT_DIR="$HOME/.cheqdnode"
 
 # Init node config directory
 if [ ! -d "${CHEQD_ROOT_DIR}/config" ]
 then
     echo "Node config not found. Initializing."
-    cheqd-noded init $NODE_MONIKER --home 
+    cheqd-noded init $NODE_MONIKER
+	curl -sSL "https://raw.githubusercontent.com/cheqd/cheqd-node/main/persistent_chains/${NETWORK}/genesis.json" > ${CHEQD_ROOT_DIR}/config/genesis.json
+	curl -sSL "https://raw.githubusercontent.com/cheqd/cheqd-node/main/persistent_chains/${NETWORK}/seeds.txt" > ${CHEQD_ROOT_DIR}/config/seeds.txt
+	P2P_SEEDS=$(cat ${CHEQD_ROOT_DIR}/config/seeds.txt)
 else
     echo "Node config exists. Skipping initialization."
 fi
 
 # Run configure
 # `! -z` is used instead of `-n` to distinguilsh null and empty values
-
 if [[ ! -z ${CREATE_EMPTY_BLOCKS+x} ]]; then cheqd-noded configure create-empty-blocks ${CREATE_EMPTY_BLOCKS}; fi
 if [[ ! -z ${FASTSYNC_VERSION+x} ]]; then cheqd-noded configure fastsync-version ${FASTSYNC_VERSION}; fi
 if [[ ! -z ${MIN_GAS_PRICES+x} ]]; then cheqd-noded configure min-gas-prices ${MIN_GAS_PRICES}; fi
@@ -31,15 +34,5 @@ if [[ ! -z ${P2P_SEED_MODE+x} ]]; then cheqd-noded configure p2p seed-mode ${P2P
 if [[ ! -z ${P2P_SEEDS+x} ]]; then cheqd-noded configure p2p seeds ${P2P_SEEDS}; fi
 if [[ ! -z ${P2P_SEND_RATE+x} ]]; then cheqd-noded configure p2p send-rate ${P2P_SEND_RATE}; fi
 
-
-# Update configs
-set - # Disable ditailed command logging
-
-echo "Updating genesis"
-echo "$GENESIS" | base64 --decode > $CHEQD_ROOT_DIR/config/genesis.json
-
-set -x # Re-enable ditailed command logging
-
 # Run node
-NODE_ARGS=${NODE_ARGS:-}  # Allo node args to be empty
-cheqd-noded start $NODE_ARGS
+cheqd-noded start
