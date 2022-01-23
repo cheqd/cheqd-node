c807ab4c9f3b436f700cbf79ed32ce7578b9a65a
Alexandr Kolesov <alexandr.kolesov@cheqd.io>
2022-01-24 00:51:48 +0300

    Fixx protogen.sh

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -14,26 +14,24 @@ cosmos_sdk_dir=$(go list -f '{{ .Dir }}' -m github.com/cosmos/cosmos-sdk)
 proto_dirs=$(find . -path ./third_party -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 
 for dir in $proto_dirs; do
-  read -ra proto_files < <(find "${dir}" -name '*.proto')
-
   # Generate protobuf bind
+  # shellcheck disable=SC2046
   buf protoc \
   -I "proto" \
   -I "$cosmos_sdk_dir/third_party/proto" \
   -I "$cosmos_sdk_dir/proto" \
   --gocosmos_out=plugins=interfacetype+grpc,\
 Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
-  "${proto_files[@]}"
-
-  read -ra proto_files < <(find "${dir}" -maxdepth 1 -name '*.proto')
+  $(find "${dir}" -name '*.proto')
 
   # Generate grpc gateway
+  # shellcheck disable=SC2046
   buf protoc \
   -I "proto" \
   -I "$cosmos_sdk_dir/third_party/proto" \
   -I "$cosmos_sdk_dir/proto" \
   --grpc-gateway_out=logtostderr=true,allow_colon_final_segments=true:. \
-  "${proto_files[@]}"
+  $(find "${dir}" -maxdepth 1 -name '*.proto')
 done
 
 cp -r ./github.com/cheqd/cheqd-node/* ./
