072dd3553b6369b8710ece05dfa5bea1f150eab0
toktar <renata.toktar@dsr-corporation.com>
2022-02-02 19:23:34 +0300

    Merge pull request #276 from cheqd/DEV-826
    [DEV-826] Additional txns before upgrade

--- tests/upgrade/prepare.sh
+++ tests/upgrade/prepare.sh
@@ -13,8 +13,27 @@ clean_env
 # Generate config files
 bash gen_node_configs.sh
 
-# Make all the data accessable
-make_777
-
 # Start the network on version which will be upgraded from
 docker_compose_up "${CHEQD_IMAGE_FROM}" $(pwd)
+
+# Wait for start ordering, till height 1
+bash ../networks/tools/wait-for-chain.sh 1
+
+# Get address of operator which will be used for sending tokens before upgrade
+get_addresses
+OP2_ADDRESS=${addresses[2]}
+
+# Send tokens before upgrade
+send_tokens $OP2_ADDRESS
+
+# Send DID transaction
+send_did $DID_1
+
+# Check that token transaction exists
+check_tx_hashes
+
+# Check that $DID was written
+check_did $DID_1
+
+# Check balance after token sending
+check_balance $OP2_ADDRESS
