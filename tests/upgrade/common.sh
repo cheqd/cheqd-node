fd0712497793539b9b373f7db2a16df5c4f8feee
Andrew Nikitin <andrew.nikitin@cheqd.io>
2022-02-02 12:31:54 +0300

    Change local_client_tx behaviour.
    Signed-off-by: Andrew Nikitin <andrew.nikitin@cheqd.io>

--- tests/upgrade/common.sh
+++ tests/upgrade/common.sh
@@ -64,7 +64,7 @@ function clean_env () {
 
 # Run command using local generated keys from node_configs/client
 function local_client_tx () {
-    result="$(cheqd_noded_docker $@ --home node_configs/client/.cheqdnode/ --keyring-backend test)"
+    cheqd_noded_docker $@ --home node_configs/client/.cheqdnode/ --keyring-backend test
 }
 
 function make_777 () {
@@ -81,8 +81,8 @@ function random_string() {
 }
 
 function get_addresses () {
-    local_client_tx keys list
-    addresses=( $(echo "$result" | grep -o 'cheqd1.*') )
+    all_keys=$(local_client_tx keys list)
+    addresses=( $(echo "$all_keys" | grep -o 'cheqd1.*') )
     echo "${addresses[@]}"
 }
 
@@ -97,19 +97,19 @@ function send_tokens() {
     OP2_ADDRESS=${addresses[2]}
     OP3_ADDRESS=${addresses[3]}
 
-    local_client_tx tx \
+    send_res=$(local_client_tx tx \
                     bank \
                     send $OP0_ADDRESS $OP1_ADDRESS $CHEQ_AMOUNT \
                     --gas auto \
                     --gas-adjustment 1.2 \
                     --gas-prices "25ncheq" \
                     --chain-id $CHAIN_ID \
-                    -y
-    txhash=$(echo $result | jq ".txhash" | tr -d '"')
+                    -y)
+    txhash=$(echo $send_res | jq ".txhash" | tr -d '"')
     echo $txhash >> $FNAME_TXHASHES
 }
 
-
+# Send DID
 function send_did () {
 
     # Generate Alice identity key
@@ -125,20 +125,20 @@ function send_did () {
     MSG_CREATE_DID='{"id":"'${DID}'","verification_method":[{"id":"'${KEY_ID}'","type":"Ed25519VerificationKey2020","controller":"'${DID}'","public_key_multibase":"'${ALICE_VER_PUB_MULTIBASE_58}'"}],"authentication":["'${KEY_ID}'"]}'
 
     # Post the message
-    local_client_tx tx cheqd create-did ${MSG_CREATE_DID} ${KEY_ID} \
+    did=$(local_client_tx tx cheqd create-did ${MSG_CREATE_DID} ${KEY_ID} \
         --ver-key "${ALICE_VER_PRIV_BASE_64}" \
         --from operator0 \
         --gas-prices "25ncheq" \
         --chain-id $CHAIN_ID \
         --output json \
-        -y
+        -y)
 
 
-    txhash=$(echo $result | jq ".txhash" | tr -d '"')
+    txhash=$(echo $did | jq ".txhash" | tr -d '"')
     echo $txhash >> $FNAME_TXHASHES
 }
 
-
+# Check transaction hashes
 function check_tx_hashes () {
     for txhash in $(cat $FNAME_TXHASHES); 
     do
