85d94bf8f26e1c919a1aaf5948e162a5de66667a
Alexandr Kolesov <alexandr.kolesov@dsr-corporation.com>
2022-02-09 18:30:40 +0300

    Fix build-tar, build-deb scripts

--- build-tools/build-deb.sh
+++ build-tools/build-deb.sh
@@ -2,43 +2,48 @@
 
 set -euox pipefail
 
-if [ -n "$1" ]; then
-    PKG_NAME=$1
-else
-    echo "It seems that parameter 'PKG_NAME' was missed. Try: "
-    echo "$0 <package name> <version of deb>"
-    exit 1
+if [ -z ${1+x} ]; then
+  echo "Binary path must be passed as the first parameter"
 fi
 
-if [ -n "$2" ]; then
-    VERSION=$2
-else
-    echo "It seems that parameter 'VERSION' was missed. Try: "
-    echo "$0 <package name> <version of deb>"
-    exit 1
-fi
+BINARY_PATH="$1"
+VERSION=$("${BINARY_PATH}" version 2>&1)
+PKG_NAME="cheqd-node"
 
-source ./common.sh
+BUILD_DIR="build"
+OUTPUT_DIR="output"
 
+mkdir "${BUILD_DIR}"
+mkdir "${OUTPUT_DIR}"
+
+# Prepare content
+PACKAGE_CONTENT="${BUILD_DIR}/deb-package-content"
+mkdir -p "${PACKAGE_CONTENT}/usr/bin/"
+cp "${BINARY_PATH}" "${PACKAGE_CONTENT}/usr/bin/"
+
+# Make intermediate archive
+PACKAGE_CONTENT_TAR="${BUILD_DIR}/deb-package-content.tar.gz"
+tar -cvzf "${PACKAGE_CONTENT_TAR}" -C "${PACKAGE_CONTENT}" "."
+
+# Build deb based on the archive
 ARCH="amd64"
-FULL_PKG_NAME=${PKG_NAME}_${VERSION}_${ARCH}.deb
-PKG_PATH=$OUTPUT_DIR/$FULL_PKG_NAME
+DEB_PACKAGE="${OUTPUT_DIR}/${PKG_NAME}_${VERSION}_${ARCH}.deb"
 
 fpm \
-    --input-type "tar" \
-    --output-type "deb" \
-    --version "${VERSION}" \
-    --name "cheqd-node" \
-    --description "cheqd node" \
-    --url "https://github.com/cheqd/cheqd-node"
-    --architecture "${ARCH}" \
-    --deb-generate-changes \
-    --deb-compression gz \
-    --after-install "postinst" \
-    --deb-after-purge "postpurge" \
-    --deb-systemd-enable \
-    --deb-systemd cheqd-noded.service \
-    --depends "logrotate" \
-    --verbose \
-    --package "${PKG_PATH}" \
-    $PATH_TAR
+  --input-type "tar" \
+  --output-type "deb" \
+  --version "${VERSION}" \
+  --name "cheqd-node" \
+  --description "cheqd node" \
+  --url "https://github.com/cheqd/cheqd-node"
+  --architecture "${ARCH}" \
+  --deb-generate-changes \
+  --deb-compression gz \
+  --after-install "postinst" \
+  --deb-after-purge "postpurge" \
+  --deb-systemd-enable \
+  --deb-systemd cheqd-noded.service \
+  --depends "logrotate" \
+  --verbose \
+  --package "${DEB_PACKAGE}" \
+  "${PACKAGE_CONTENT_TAR}"
