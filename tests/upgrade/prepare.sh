2c9d1b0b66580db2f4323411383499d7d8e6c53c
Alexandr Kolesov <alexandr.kolesov@dsr-corporation.com>
2022-02-10 18:35:12 +0300

    Fix linter issues

--- tests/upgrade/prepare.sh
+++ tests/upgrade/prepare.sh
@@ -2,6 +2,7 @@
 
 set -euox pipefail
 
+# shellcheck disable=SC1091
 . common.sh
 
 # Stop docker compose
@@ -14,26 +15,27 @@ clean_env
 bash gen_node_configs.sh
 
 # Start the network on version which will be upgraded from
-docker_compose_up "${CHEQD_IMAGE_FROM}" $(pwd)
+docker_compose_up "${CHEQD_IMAGE_FROM}" "$(pwd)"
 
 # Wait for start ordering, till height 1
 bash ../networks/tools/wait-for-chain.sh 1
 
 # Get address of operator which will be used for sending tokens before upgrade
 get_addresses
+# shellcheck disable=SC2154
 OP2_ADDRESS=${addresses[2]}
 
 # Send tokens before upgrade
-send_tokens $OP2_ADDRESS
+send_tokens "$OP2_ADDRESS"
 
 # Send DID transaction
-send_did $DID_1
+send_did "$DID_1"
 
 # Check that token transaction exists
 check_tx_hashes
 
 # Check that $DID was written
-check_did $DID_1
+check_did "$DID_1"
 
 # Check balance after token sending
-check_balance $OP2_ADDRESS
+check_balance "$OP2_ADDRESS"
--- tests/upgrade/upgrade_and_check.sh
+++ tests/upgrade/upgrade_and_check.sh
@@ -2,10 +2,11 @@
 
 set -euox pipefail
 
+# shellcheck disable=SC1091
 . common.sh
 
 # Wait for upgrade height
-bash ../networks/tools/wait-for-chain.sh $UPGRADE_HEIGHT $(echo "2*$VOTING_PERIOD" | bc)
+bash ../networks/tools/wait-for-chain.sh "$UPGRADE_HEIGHT" $((2 * VOTING_PERIOD))
 
 # Stop docker-compose service
 docker_compose_down
@@ -14,43 +15,44 @@ docker_compose_down
 make_777
 
 # Start docker-compose with new base image on new version
-docker_compose_up "$CHEQD_IMAGE_TO" $(pwd)
+docker_compose_up "$CHEQD_IMAGE_TO" "$(pwd)"
 
 # Check that upgrade was successful
 
 # Wait for upgrade height
-bash ../networks/tools/wait-for-chain.sh $(echo $UPGRADE_HEIGHT+2 | bc)
+bash ../networks/tools/wait-for-chain.sh $((UPGRADE_HEIGHT + 2))
 
-CURRENT_VERSION=$(docker run --entrypoint cheqd-noded $CHEQD_IMAGE_TO version 2>&1)
+CURRENT_VERSION=$(docker run --entrypoint cheqd-noded "$CHEQD_IMAGE_TO" version 2>&1)
 
-if [ $CURRENT_VERSION != $CHEQD_VERSION_TO ] ; then
+if [ "$CURRENT_VERSION" != "$CHEQD_VERSION_TO" ] ; then
      echo "Upgrade to version $CHEQD_VERSION_TO was not successful"
      exit 1
 fi
 
 get_addresses
 # "To" address was used for sending tokens before upgrade
+# shellcheck disable=SC2154
 OP_ADDRESS_BEFORE=${addresses[2]}
 # "To" address was used for sending tokens after upgrade
 OP_ADDRESS_AFTER=${addresses[3]}
 
 # Check balances after tokens sending
-check_balance $OP_ADDRESS_BEFORE
+check_balance "$OP_ADDRESS_BEFORE"
 
 # Check that did written before upgrade stil exist
-check_did $DID_1
+check_did "$DID_1"
 
 # Send tokens for checking functionality after upgrade
-send_tokens $OP_ADDRESS_AFTER
+send_tokens "$OP_ADDRESS_AFTER"
 
 # Send DID after upgrade
-send_did $DID_2
+send_did "$DID_2"
 
 # Check balance after token sending
-check_balance $OP_ADDRESS_AFTER
+check_balance "$OP_ADDRESS_AFTER"
 
 # Check that did written before upgrade stil exist
-check_did $DID_2
+check_did "$DID_2"
 
 # Check that token transaction exists after upgrade too
 check_tx_hashes
