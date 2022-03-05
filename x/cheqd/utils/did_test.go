eb198b796880b32230aa9b467953ed3576f89495
Andrew Nikitin <andrew.nikitin@cheqd.io>
2022-03-05 18:36:01 +0300

    [DEV-705] make each test as independent
    Signed-off-by: Andrew Nikitin <andrew.nikitin@cheqd.io>

--- x/cheqd/utils/did_test.go
+++ x/cheqd/utils/did_test.go
@@ -7,76 +7,63 @@ import (
 
 func TestIsDid(t *testing.T) {
 	cases := []struct {
+		name string
 		valid bool
 		did   string
 		method string
 		allowedNS []string
 	}{
-		{true, "did:cheqd:testnet:123456789abcdefg", "cheqd", []string{"testnet"}},
-		{true, "did:cheqd:testnet:123456789abcdefg", "cheqd", []string{"testnet", "mainnet"}},
-		{true, "did:cheqd:testnet:123456789abcdefg", "", []string{"testnet"}},
-		{true, "did:cheqd:testnet:123456789abcdefg", "", []string{}},
-		{true, "did:cheqd:123456789abcdefg", "", []string{}},
+		{"Valid: Inputs: Method and namespace are set", true, "did:cheqd:testnet:123456789abcdefg", "cheqd", []string{"testnet"}},
+		{"Valid: Inputs: Method and namespaces are set", true, "did:cheqd:testnet:123456789abcdefg", "cheqd", []string{"testnet", "mainnet"}},
+		{"Valid: Inputs: Method not set", true, "did:cheqd:testnet:123456789abcdefg", "", []string{"testnet"}},
+		{"Valid: Inputs: Method and namespaces are empty", true, "did:cheqd:testnet:123456789abcdefg", "", []string{}},
+		{"Valid: Namespace is absent in DID", true, "did:cheqd:123456789abcdefg", "", []string{}},
 		// Generic method validation
-		{true, "did:NOTcheqd:123456789abcdefg", "", []string{}},
-		{true, "did:NOTcheqd:123456789abcdefg123456789abcdefg", "", []string{}},
+		{"Valid: Inputs: Method is not set and passed for NOTcheqd", true, "did:NOTcheqd:123456789abcdefg", "", []string{}},
+		{"Valid: Inputs: Method and Namespaces are not set and passed for NOTcheqd", true, "did:NOTcheqd:123456789abcdefg123456789abcdefg", "", []string{}},
 
-		{true, "did:cheqd:testnet:123456789abcdefg", "cheqd", []string{"mainnet", "testnet"}},
+		{"Valid: Inputs: Order of namespaces changed", true, "did:cheqd:testnet:123456789abcdefg", "cheqd", []string{"mainnet", "testnet"}},
 		// Wrong splitting (^did:([^:]+?)(:([^:]+?))?:([^:]+)$)
-		{false, "did123:cheqd:::123456789abcdefg", "cheqd", []string{"testnet"}},
-		{false, "did:cheqd::123456789abcdefg", "cheqd", []string{"testnet"}},
-		{false, "did:cheqd:::123456789abcdefg", "cheqd", []string{"testnet"}},
-		{false, "did:cheqd:testnet:123456789abcdefg:did:cheqd:testnet:123456789abcdefg", "cheqd", []string{"testnet"}},
+		{"Not valid: DID is not started from 'did'", false, "did123:cheqd:::123456789abcdefg", "cheqd", []string{"testnet"}},
+		{"Not valid: empty namespace", false, "did:cheqd::123456789abcdefg", "cheqd", []string{"testnet"}},
+		{"Not valid: a lot of ':'", false, "did:cheqd:::123456789abcdefg", "cheqd", []string{"testnet"}},
+		{"Not valid: several DIDs in one string", false, "did:cheqd:testnet:123456789abcdefg:did:cheqd:testnet:123456789abcdefg", "cheqd", []string{"testnet"}},
 		// Wrong method
-		{false, "did:NOTcheqd:testnet:123456789abcdefg", "cheqd", []string{"mainnet", "testnet"}},
-		{false, "did:cheqd:testnet:123456789abcdefg", "NOTcheqd", []string{"mainnet", "testnet"}},
+		{"Not valid: method in DID is not the same as from Inputs", false, "did:NOTcheqd:testnet:123456789abcdefg", "cheqd", []string{"mainnet", "testnet"}},
+		{"Not valid: method in Inputs is not the same as from DID", false, "did:cheqd:testnet:123456789abcdefg", "NOTcheqd", []string{"mainnet", "testnet"}},
 		// Wrong namespace (^[a-zA-Z0-9]*)
-		{false, "did:cheqd:testnet/:123456789abcdefg", "cheqd", []string{}},
-		{false, "did:cheqd:testnet_:123456789abcdefg", "cheqd", []string{}},
-		{false, "did:cheqd:testnet%:123456789abcdefg", "cheqd", []string{}},
-		{false, "did:cheqd:testnet*:123456789abcdefg", "cheqd", []string{}},
-		{false, "did:cheqd:testnet&:123456789abcdefg", "cheqd", []string{}},
-		{false, "did:cheqd:testnet@/:123456789abcdefg", "cheqd", []string{}},
-		{false, "did:cheqd:testnet:123456789abcdefg", "cheqd", []string{"not_testnet"}},
+		{"Not valid: / is not allowed for namespace", false, "did:cheqd:testnet/:123456789abcdefg", "cheqd", []string{}},
+		{"Not valid: _ is not allowed for namespace", false, "did:cheqd:testnet_:123456789abcdefg", "cheqd", []string{}},
+		{"Not valid: % is not allowed for namespace", false, "did:cheqd:testnet%:123456789abcdefg", "cheqd", []string{}},
+		{"Not valid: * is not allowed for namespace", false, "did:cheqd:testnet*:123456789abcdefg", "cheqd", []string{}},
+		{"Not valid: & is not allowed for namespace", false, "did:cheqd:testnet&:123456789abcdefg", "cheqd", []string{}},
+		{"Not valid: @ is not allowed for namespace", false, "did:cheqd:testnet@:123456789abcdefg", "cheqd", []string{}},
+		{"Not valid: namespace from Inputs is not the same as from DID", false, "did:cheqd:testnet:123456789abcdefg", "cheqd", []string{"not_testnet"}},
 		// Base58 checks (^[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]*$)
-		{false, "did:cheqd:testnet:123456789abcdefO", "cheqd", []string{}},
-		{false, "did:cheqd:testnet:123456789abcdefI", "cheqd", []string{}},
-		{false, "did:cheqd:testnet:123456789abcdefl", "cheqd", []string{}},
-		{false, "did:cheqd:testnet:123456789abcdef0", "cheqd", []string{}},
+		{"Not valid: O - is not allowed for UniqueID", false, "did:cheqd:testnet:123456789abcdefO", "cheqd", []string{}},
+		{"Not valid: I - is not allowed for UniqueID", false, "did:cheqd:testnet:123456789abcdefI", "cheqd", []string{}},
+		{"Not valid: l - is not allowed for UniqueID", false, "did:cheqd:testnet:123456789abcdefl", "cheqd", []string{}},
+		{"Not valid: 0 - is not allowed for UniqueID", false, "did:cheqd:testnet:123456789abcdef0", "cheqd", []string{}},
 		// Length checks (should be exactly 16 or 32)
-		{false, "did:cheqd:testnet:123", "cheqd", []string{}},
-		{false, "did:cheqd:testnet:123456789abcdefgABCDEF", "cheqd", []string{}},
-		{false, "did:cheqd:testnet:123456789abcdefg123456789abcdefgABCDEF", "cheqd", []string{}},
+		{"Not valid: UniqueID less then 16 symbols", false, "did:cheqd:testnet:123", "cheqd", []string{}},
+		{"Not valid: UniqueID more then 16 symbols but less then 32", false, "did:cheqd:testnet:123456789abcdefgABCDEF", "cheqd", []string{}},
+		{"Not valid: UniqueID more then 32 symbols", false, "did:cheqd:testnet:123456789abcdefg123456789abcdefgABCDEF", "cheqd", []string{}},
 
 	}
 
 	for _, tc := range cases {
-		isDid := IsValidDID(tc.did, tc.method, tc.allowedNS)
+		t.Run(tc.name, func(t *testing.T) {
+			isDid := IsValidDID(tc.did, tc.method, tc.allowedNS)
 
-		if tc.valid {
-			require.True(t, isDid)
-		} else {
-			require.False(t, isDid)
-		}
+			if tc.valid {
+				require.True(t, isDid)
+			} else {
+				require.False(t, isDid)
+			}
+		})
 	}
 }
 
-
-//func TestDIDURLErrors(t *testing.T) {
-//	cases := []struct {
-//		didUrl   string
-//		error_code int
-//		error_string string
-//	}{
-//		{"did:cheqd:testnet:123456789abcdefg/", ErrStaticDIDURLPathAbemptyNotValid},
-//	}
-//
-//	for _, tc := range cases {
-//		err := ValidateDIDUrl(tc.didUrl, "", []string{})
-//		require.NotNil(t, err)
-//		require.Exactly(t, err.code, tc.error_code)
-//
-//}
 
 
 
