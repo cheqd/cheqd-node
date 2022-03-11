e05c3bc19c454a2457c33bda558e81c491527928
Alexandr Kolesov <alexandr.kolesov@dsr-corporation.com>
2022-03-11 11:51:16 +0300

    For the previous commit

--- x/cheqd/types/did_verification_method_test.go
+++ x/cheqd/types/did_verification_method_test.go
@@ -15,7 +15,7 @@ type TestJWKKey struct {
 	Kid string `json:"kid"`
 }
 
-var	ValidJWKKey = TestJWKKey{
+var ValidJWKKey = TestJWKKey{
 	Kty: "RSA",
 	N:   "o76AudS2rsCvlz_3D47sFkpuz3NJxgLbXr1cHdmbo9xOMttPMJI97f0rHiSl9stltMi87KIOEEVQWUgMLaWQNaIZThgI1seWDAGRw59AO5sctgM1wPVZYt40fj2Qw4KT7m4RLMsZV1M5NYyXSd1lAAywM4FT25N0RLhkm3u8Hehw2Szj_2lm-rmcbDXzvjeXkodOUszFiOqzqBIS0Bv3c2zj2sytnozaG7aXa14OiUMSwJb4gmBC7I0BjPv5T85CH88VOcFDV51sO9zPJaBQnNBRUWNLh1vQUbkmspIANTzj2sN62cTSoxRhSdnjZQ9E_jraKYEW5oizE9Dtow4EvQ",
 	Use: "sig",
@@ -24,7 +24,7 @@ var	ValidJWKKey = TestJWKKey{
 	Kid: "6a8ba5652a7044121d4fedac8f14d14c54e4895b",
 }
 
-var	NotValidJWKKey = TestJWKKey{
+var NotValidJWKKey = TestJWKKey{
 	Kty: "SomeOtherKeyType",
 	N:   "o76AudS2rsCvlz_3D47sFkpuz3NJxgLbXr1cHdmbo9xOMttPMJI97f0rHiSl9stltMi87KIOEEVQWUgMLaWQNaIZThgI1seWDAGRw59AO5sctgM1wPVZYt40fj2Qw4KT7m4RLMsZV1M5NYyXSd1lAAywM4FT25N0RLhkm3u8Hehw2Szj_2lm-rmcbDXzvjeXkodOUszFiOqzqBIS0Bv3c2zj2sytnozaG7aXa14OiUMSwJb4gmBC7I0BjPv5T85CH88VOcFDV51sO9zPJaBQnNBRUWNLh1vQUbkmspIANTzj2sN62cTSoxRhSdnjZQ9E_jraKYEW5oizE9Dtow4EvQ",
 	Use: "sig",
@@ -63,10 +63,10 @@ func TestVerificationMethodValidation(t *testing.T) {
 		{
 			name: "valid method with jwk key",
 			struct_: VerificationMethod{
-				Id:         "did:cheqd:aaaaaaaaaaaaaaaa#rty",
-				Type:       "JsonWebKey2020",
-				Controller: "did:cheqd:bbbbbbbbbbbbbbbb",
-				PublicKeyJwk: ValidPublicKeyJWK,
+				Id:                 "did:cheqd:aaaaaaaaaaaaaaaa#rty",
+				Type:               "JsonWebKey2020",
+				Controller:         "did:cheqd:bbbbbbbbbbbbbbbb",
+				PublicKeyJwk:       ValidPublicKeyJWK,
 				PublicKeyMultibase: "",
 			},
 			isValid:  true,
@@ -75,10 +75,10 @@ func TestVerificationMethodValidation(t *testing.T) {
 		{
 			name: "base did: positive",
 			struct_: VerificationMethod{
-				Id:         "did:cheqd:aaaaaaaaaaaaaaaa#rty",
-				Type:       "JsonWebKey2020",
-				Controller: "did:cheqd:bbbbbbbbbbbbbbbb",
-				PublicKeyJwk: ValidPublicKeyJWK,
+				Id:                 "did:cheqd:aaaaaaaaaaaaaaaa#rty",
+				Type:               "JsonWebKey2020",
+				Controller:         "did:cheqd:bbbbbbbbbbbbbbbb",
+				PublicKeyJwk:       ValidPublicKeyJWK,
 				PublicKeyMultibase: "",
 			},
 			baseDid:  "did:cheqd:aaaaaaaaaaaaaaaa",
@@ -88,10 +88,10 @@ func TestVerificationMethodValidation(t *testing.T) {
 		{
 			name: "base did: negative",
 			struct_: VerificationMethod{
-				Id:         "did:cheqd:aaaaaaaaaaaaaaaa#rty",
-				Type:       "JsonWebKey2020",
-				Controller: "did:cheqd:bbbbbbbbbbbbbbbb",
-				PublicKeyJwk: ValidPublicKeyJWK,
+				Id:                 "did:cheqd:aaaaaaaaaaaaaaaa#rty",
+				Type:               "JsonWebKey2020",
+				Controller:         "did:cheqd:bbbbbbbbbbbbbbbb",
+				PublicKeyJwk:       ValidPublicKeyJWK,
 				PublicKeyMultibase: "",
 			},
 			baseDid:  "did:cheqd:bbbbbbbbbbbbbbbb",
@@ -101,10 +101,10 @@ func TestVerificationMethodValidation(t *testing.T) {
 		{
 			name: "allowed namespaces: positive",
 			struct_: VerificationMethod{
-				Id:         "did:cheqd:mainnet:aaaaaaaaaaaaaaaa#rty",
-				Type:       "JsonWebKey2020",
-				Controller: "did:cheqd:bbbbbbbbbbbbbbbb",
-				PublicKeyJwk: ValidPublicKeyJWK,
+				Id:                 "did:cheqd:mainnet:aaaaaaaaaaaaaaaa#rty",
+				Type:               "JsonWebKey2020",
+				Controller:         "did:cheqd:bbbbbbbbbbbbbbbb",
+				PublicKeyJwk:       ValidPublicKeyJWK,
 				PublicKeyMultibase: "",
 			},
 			allowedNamespaces: []string{"mainnet", ""},
@@ -113,10 +113,10 @@ func TestVerificationMethodValidation(t *testing.T) {
 		{
 			name: "allowed namespaces: positive",
 			struct_: VerificationMethod{
-				Id:         "did:cheqd:mainnet:aaaaaaaaaaaaaaaa#rty",
-				Type:       "JsonWebKey2020",
-				Controller: "did:cheqd:bbbbbbbbbbbbbbbb",
-				PublicKeyJwk: ValidPublicKeyJWK,
+				Id:                 "did:cheqd:mainnet:aaaaaaaaaaaaaaaa#rty",
+				Type:               "JsonWebKey2020",
+				Controller:         "did:cheqd:bbbbbbbbbbbbbbbb",
+				PublicKeyJwk:       ValidPublicKeyJWK,
 				PublicKeyMultibase: "",
 			},
 			allowedNamespaces: []string{"testnet"},
@@ -129,11 +129,10 @@ func TestVerificationMethodValidation(t *testing.T) {
 				Id:                 "did:cheqd:aaaaaaaaaaaaaaaa#qwe",
 				Type:               "JsonWebKey2020",
 				Controller:         "did:cheqd:bbbbbbbbbbbbbbbb",
-				PublicKeyJwk: ValidPublicKeyJWK,
+				PublicKeyJwk:       ValidPublicKeyJWK,
 				PublicKeyMultibase: "",
 			},
-			isValid:           true,
-			errorMsg:          "controller: did namespace must be one of: testnet; id: did namespace must be one of: testnet.",
+			isValid: true,
 		},
 		{
 			name: "JWK: not valid key",
@@ -141,11 +140,11 @@ func TestVerificationMethodValidation(t *testing.T) {
 				Id:                 "did:cheqd:aaaaaaaaaaaaaaaa#qwe",
 				Type:               "JsonWebKey2020",
 				Controller:         "did:cheqd:bbbbbbbbbbbbbbbb",
-				PublicKeyJwk: NotValidPublicKeyJWK,
+				PublicKeyJwk:       NotValidPublicKeyJWK,
 				PublicKeyMultibase: "",
 			},
-			isValid:           false,
-			errorMsg:          "public_key_jwk: invalid format for JWK key, error from validation: failed to unmarshal JWK set: failed to parse sole key in key set: invalid key type from JSON (SomeOtherKeyType).",
+			isValid:  false,
+			errorMsg: "public_key_jwk: invalid format for JWK key, error from validation: failed to unmarshal JWK set: failed to parse sole key in key set: invalid key type from JSON (SomeOtherKeyType).",
 		},
 	}
 
