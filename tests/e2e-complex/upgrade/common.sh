6784128652e003ae46dc61adb4c44d29fd274600
Andrew Nikitin <andrew.nikitin@cheqd.io>
2022-03-18 11:46:54 +0300

    [DEV-705] Fix upgrade test
    Signed-off-by: Andrew Nikitin <andrew.nikitin@cheqd.io>

--- tests/e2e-complex/upgrade/common.sh
+++ tests/e2e-complex/upgrade/common.sh
@@ -92,7 +92,8 @@ function random_string() {
 
 function get_addresses () {
     all_keys=$(local_client_tx keys list)
-    mapfile -t addresses < <(echo "$all_keys" | grep -o 'cheqd1.*')
+#    mapfile -t addresses < <(echo "$all_keys" | grep -o 'cheqd1.*')
+    addresses=( $(echo "$all_keys" | grep -o 'cheqd1.*') )
     echo "${addresses[@]}"
 }
 
@@ -148,7 +149,7 @@ function send_tokens() {
 
 # Send DID
 # input: DID to write
-function send_did() {
+function send_did () {
     did_to_write=$1
 
     # Generate Alice identity key
@@ -164,13 +165,12 @@ function send_did() {
     MSG_CREATE_DID='{"id":"'${did_to_write}'","verification_method":[{"id":"'"${KEY_ID}"'","type":"Ed25519VerificationKey2020","controller":"'${did_to_write}'","public_key_multibase":"'${ALICE_VER_PUB_MULTIBASE_58}'"}],"authentication":["'${KEY_ID}'"]}'
 
     # Post the message
-    did=$(cheqd_noded_docker tx cheqd create-did "${MSG_CREATE_DID}" "${KEY_ID}" "${ALICE_VER_PRIV_BASE_64}" \
+    did=$(local_client_tx tx cheqd create-did "${MSG_CREATE_DID}" "${KEY_ID}" \
+        --ver-key "${ALICE_VER_PRIV_BASE_64}" \
         --from operator0 \
         --gas-prices "25ncheq" \
         --chain-id $CHAIN_ID \
         --output json \
-        --home node_configs/client/.cheqdnode/ \
-        --keyring-backend test
         -y)
 
 
