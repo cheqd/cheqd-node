5d5f84a55f507e3e1ce04b9a384632a191ac039a
Andrew Nikitin <andrew.nikitin@cheqd.io>
2022-04-12 13:29:48 +0300

    build(tooling): Fix release artifact version numbers
    * Deactivate lint and release workflows
    
    * Deactivate lint workflow dependency
    
    * [DEV-1096] Revert changes and fix pipelines
    
    Signed-off-by: Andrew Nikitin <andrew.nikitin@cheqd.io>
    
    * [DEV-1096] Remove set pipefail for workflow
    
    Signed-off-by: Andrew Nikitin <andrew.nikitin@cheqd.io>
    
    * [DEV-1096] add script instead of one command
    
    Signed-off-by: Andrew Nikitin <andrew.nikitin@cheqd.io>
    
    * [DEV-1096] Change node to localhost
    
    Signed-off-by: Andrew Nikitin <andrew.nikitin@cheqd.io>
    
    * [DEV-1096] Fix release pipelines
    
    Signed-off-by: Andrew Nikitin <andrew.nikitin@cheqd.io>
    
    * [DEV-1096] fix release pipeline
    
    Signed-off-by: Andrew Nikitin <andrew.nikitin@cheqd.io>
    
    * [DEV-1096] debug release
    
    Signed-off-by: Andrew Nikitin <andrew.nikitin@cheqd.io>
    
    * [DEV-1096] Fix release tagging
    
    Signed-off-by: Andrew Nikitin <andrew.nikitin@cheqd.io>
    
    Co-authored-by: Ankur Banerjee <ankurdotb@users.noreply.github.com>

---
+++ tests/e2e-complex/deb-install/check_promotion.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euox pipefail
+
+all_validators_cmd='cheqd-noded query staking validators --node http://localhost:26657'
+
+amount_bonded="$(${all_validators_cmd} | grep BOND_STATUS_BONDED | wc -l | xargs)"
+amount_all="$(${all_validators_cmd} | grep status | wc -l | xargs)"
+
+if [ "${amount_all}" != "${amount_bonded}" ]; 
+then 
+    exit 1
+fi
