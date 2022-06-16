5b0524f935e62bdafaff8fc394c6dc0835cc3e29
toktar <renata.toktar@dsr-corporation.com>
2022-06-16 18:39:47 +0300

    dev-1281: update resource unit tests

--- x/resource/tests/constants.go
+++ x/resource/tests/constants.go
@@ -14,16 +14,17 @@ const (
 	ResourceId          = "988b0ab3-6a39-4598-83ec-b84c6cf8da15"
 	IncorrectResourceId = "1234"
 
-	NotFounDID     = "did:cheqd:test:nfdnfdnfdnfdnfdd"
-	ExistingDID    = "did:cheqd:test:aaaaaaaaaaaaaaaa"
-	ExistingDIDKey = ExistingDID + "#key-1"
+	NotFoundDIDIdentifier = "nfdnfdnfdnfdnfdd"
+	ExistingDIDIdentifier = "eeeeeeeeeeeeeeee"
+	ExistingDID           = "did:cheqd:test:" + ExistingDIDIdentifier
+	ExistingDIDKey        = ExistingDID + "#key-1"
 )
 
 func ExistingResource() types.Resource {
 	data := []byte(SchemaData)
 	checksum := string(sha256.New().Sum(data))
 	return types.Resource{
-		CollectionId: ExistingDID,
+		CollectionId: ExistingDIDIdentifier,
 		Id:           "a09abea0-22e0-4b35-8f70-9cc3a6d0b5fd",
 		Name:         "Existing Resource Name",
 		ResourceType: CLSchemaType,
