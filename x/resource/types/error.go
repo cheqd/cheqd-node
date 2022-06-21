e339b374f472e320d3d79048c5c94fb5e0105c60
Alexandr Kolesov <alexandr.kolesov@dsr-corporation.com>
2022-06-21 14:31:37 +0300

    Fix formatting

--- x/resource/types/error.go
+++ x/resource/types/error.go
@@ -8,8 +8,8 @@ import (
 
 // x/resource module sentinel errors
 var (
-	ErrBadRequest                 = sdkerrors.Register(ModuleName, 2000, "bad request")
-	ErrResourceExists             = sdkerrors.Register(ModuleName, 2200, "Resoure exists")
-	ErrBasicValidation            = sdkerrors.Register(ModuleName, 2205, "basic validation failed")
-	ErrInternal                   = sdkerrors.Register(ModuleName, 2500, "internal error")
+	ErrBadRequest      = sdkerrors.Register(ModuleName, 2000, "bad request")
+	ErrResourceExists  = sdkerrors.Register(ModuleName, 2200, "Resoure exists")
+	ErrBasicValidation = sdkerrors.Register(ModuleName, 2205, "basic validation failed")
+	ErrInternal        = sdkerrors.Register(ModuleName, 2500, "internal error")
 )
