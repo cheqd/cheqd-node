32dbc90655b3fbe7d872f058d4cc2b7c7da94949
Andrew Nikitin <andrew.nikitin@cheqd.io>
2022-07-11 20:28:39 +0300

    debug

--- tests/e2e-complex/upgrade/cosmovisor/promote-validator.sh
+++ tests/e2e-complex/upgrade/cosmovisor/promote-validator.sh
@@ -17,7 +17,7 @@ NODE5_PUBKEY=$(sudo -u cheqd cheqd-noded tendermint show-validator | sed 's/\r//
 cheqd-noded status --node http://localhost:26657 
 
 # Send tokens from operator0
-cheqd-noded tx bank send "${OP0_ADDRESS}" "${OP5_ADDRESS}" 1100000000000000ncheq --chain-id cheqd --fees 5000000ncheq --node "http://localhost:26657" -y --keyring-backend "test" --home "${NODE_CONFIGS_BASE}/client/.cheqdnode"
+cheqd-noded tx bank send "${OP0_ADDRESS}" "${OP5_ADDRESS}" 1100000000000000ncheq --chain-id cheqd --fees 5000000ncheq --node "http://localhost:26657" --keyring-backend "test" --home "${NODE_CONFIGS_BASE}/client/.cheqdnode" -y
 
 # Send promote validator from operator5
-sudo -u cheqd cheqd-noded tx staking create-validator --amount 1000000000000000ncheq --from node5-operator --chain-id cheqd --min-self-delegation="1" --gas-prices="25ncheq" --pubkey "${NODE5_PUBKEY}" --commission-max-change-rate="0.02" --commission-max-rate="0.02" --commission-rate="0.01" --gas 500000 --node "http://localhost:26657" -y --keyring-backend "test"
+sudo -u cheqd cheqd-noded tx staking create-validator --amount 1000000000000000ncheq --from node5-operator --chain-id cheqd --min-self-delegation="1" --gas-prices="25ncheq" --pubkey "${NODE5_PUBKEY}" --commission-max-change-rate="0.02" --commission-max-rate="0.02" --commission-rate="0.01" --gas 500000 --node "http://localhost:26657" --keyring-backend "test" -y
