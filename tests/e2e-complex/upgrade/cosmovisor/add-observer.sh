b517b8abe2c787b123881a8fd8c3fafd9ac1e0c6
Andrew Nikitin <andrew.nikitin@cheqd.io>
2022-07-12 21:43:29 +0300

    debug

--- tests/e2e-complex/upgrade/cosmovisor/add-observer.sh
+++ tests/e2e-complex/upgrade/cosmovisor/add-observer.sh
@@ -33,6 +33,7 @@ sudo chown -R cheqd:cheqd "/home/runner/cheqd/"
 
 sudo systemctl start cheqd-cosmovisor
 systemctl status cheqd-cosmovisor
+sleep 10
 journalctl --since "2 days ago" | grep cosmovisor
 
 bash wait.sh "[[ $(cheqd-noded status -n 'tcp://localhost:26677' 2>&1 | wc -l) == 1 ]] && echo \"Observer node is up\""
