608d01272115210b13e01c783e96c54e445bbfa2
Andrew Nikitin <andrew.nikitin@evernym.com>
2022-07-12 19:17:28 +0300

    debug

--- tests/e2e-complex/upgrade/cosmovisor/add-observer.sh
+++ tests/e2e-complex/upgrade/cosmovisor/add-observer.sh
@@ -3,7 +3,7 @@
 set -euox pipefail
 sudo chown -R cheqd:cheqd "/home/runner/cheqd/"
 
-sudo su -c 'cheqd-noded init node5' cheqd
+sudo -u cheqd -H cheqd-noded init node5
 
 if [ -z ${GENESIS_PATH+x} ]; then
   GENESIS_PATH=${NODE_CONFIGS_BASE}/node0/.cheqdnode/config/genesis.json
@@ -12,15 +12,15 @@ fi
 VALIDATOR_0_ID=`cheqd-noded tendermint show-node-id --home ${NODE_CONFIGS_BASE}/node0/.cheqdnode`
 
 PERSISTENT_PEERS="${VALIDATOR_0_ID}@127.0.0.1:26656"
-sudo su -c 'cheqd-noded configure p2p persistent-peers "${PERSISTENT_PEERS}"' cheqd
+sudo -u cheqd -H cheqd-noded configure p2p persistent-peers "${PERSISTENT_PEERS}"
 
 sudo cp "${GENESIS_PATH}" "/home/runner/cheqd/.cheqdnode/config"
 
 sudo chmod -R 755 "/home/runner/cheqd/.cheqdnode"
 
 # Configure ports because they conflict with localnet
-sudo su -c 'cheqd-noded configure p2p laddr "tcp://0.0.0.0:26676"' cheqd 
-sudo su -c 'cheqd-noded configure rpc-laddr "tcp://0.0.0.0:26677"' cheqd
+sudo -u cheqd -H cheqd-noded configure p2p laddr "tcp://0.0.0.0:26676"
+sudo -u cheqd -H cheqd-noded configure rpc-laddr "tcp://0.0.0.0:26677"
 
 # TODO: Use environment variables
 sudo sed -i.bak 's|pprof_laddr = "localhost:6060"|pprof_laddr = "localhost:6070"|g' /home/runner/cheqd/.cheqdnode/config/config.toml
@@ -32,12 +32,10 @@ sudo sed -i.bak 's|address = ":8080"|address = ":8090"|g' /home/runner/cheqd/.ch
 sudo chown -R cheqd:cheqd "/home/runner/cheqd/"
 
 sudo systemctl start cheqd-cosmovisor
-sleep 2
-systemctl status cheqd-cosmovisor
 
 bash wait.sh "[[ $(cheqd-noded status -n 'tcp://localhost:26677' 2>&1 | wc -l) == 1 ]] && echo \"Observer node is up\""
 
-NODE_CONFIGS_BASE=${NODE_CONFIGS_BASE} bash promote-validator.sh
+NODE_CONFIGS_BASE="${NODE_CONFIGS_BASE}" bash promote-validator.sh
 
 bash check-promotion.sh
 # shellcheck disable=SC2016
