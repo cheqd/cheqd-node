0924b0f0e4d2df79eaffc2882998917fa7b1b44d
Andrew Nikitin <andrew.nikitin@evernym.com>
2022-07-13 18:57:07 +0300

    fix height comparison

--- tests/e2e-complex/upgrade/cosmovisor/check_upgrade.sh
+++ tests/e2e-complex/upgrade/cosmovisor/check_upgrade.sh
@@ -10,4 +10,8 @@ if [ "$(cheqd-noded version 2>&1)" != "${UPGRADE_VERSION_COSMOVISOR}" ]; then
     exit 1
 fi
 
-bash wait.sh "[[ $(cheqd-noded status --node tcp://127.0.0.1:26677 2>&1 | jq \".SyncInfo.latest_block_height\" | tr -d '\"') > $UPGRADE_HEIGHT ]] && echo \"Current hieght more then upgrade\""
+if [[ $(cheqd-noded status --node tcp://127.0.0.1:26677 2>&1 | jq ".SyncInfo.latest_block_height" | tr -d '\"' ) < $UPGRADE_HEIGHT ]]; 
+then
+    echo "Current height less then upgrade"
+    exit 1
+fi
