3166e9e971d8ca2cb38e8e86f0bda7dd1a4225c2
Andrew Nikitin <andrew.nikitin@evernym.com>
2022-07-13 12:31:57 +0300

    Add start upgrade height calculation

--- tests/e2e-complex/upgrade/cosmovisor/promote-validator.sh
+++ tests/e2e-complex/upgrade/cosmovisor/promote-validator.sh
@@ -6,7 +6,7 @@ set -euox pipefail
 cheqd-noded keys list --keyring-backend "test" --home "${NODE_CONFIGS_BASE}/client/.cheqdnode"
 
 # Get operator0 address by setting --home flag
-OP0_ADDRESS=$(cheqd-noded keys list --keyring-backend "test" --home "${NODE_CONFIGS_BASE}/client/.cheqdnode" | sed -nr 's/.*address: (.*?).*/\1/p' | sed -n 1p | sed 's/\r//g')
+OP2_ADDRESS=$(cheqd-noded keys list --keyring-backend "test" --home "${NODE_CONFIGS_BASE}/client/.cheqdnode" | sed -nr 's/.*address: (.*?).*/\1/p' | sed -n 3p | sed 's/\r//g')
 
 # Create operator5 by running it under the `cheqd` user.
 sudo su -c 'cheqd-noded keys add node5-operator --keyring-backend "test"' cheqd
@@ -23,14 +23,11 @@ pushd ../
 # shellcheck disable=SC1091
 . common.sh
 
-cheqd_noded_docker query bank balances "${OP0_ADDRESS}"
+cheqd_noded_docker query bank balances "${OP2_ADDRESS}"
 
-local_client_tx tx bank send "${OP0_ADDRESS}" "${OP5_ADDRESS}" 1100000000000000ncheq --chain-id cheqd --fees 5000000ncheq -y
+local_client_tx tx bank send "${OP2_ADDRESS}" "${OP5_ADDRESS}" 1100000000000000ncheq --chain-id cheqd --fees 5000000ncheq -y
 
 popd
-
-# Send tokens from operator0
-# sudo -u runner -H cheqd-noded tx bank send "${OP0_ADDRESS}" "${OP5_ADDRESS}" 1100000000000000ncheq --chain-id cheqd --fees 5000000ncheq --node http://127.0.0.1:26657 --keyring-backend "test" --home "${NODE_CONFIGS_BASE}/client/.cheqdnode" -y
 
 # Send promote validator from operator5
 sudo -H -u cheqd cheqd-noded tx staking create-validator --amount 1000000000000000ncheq --from node5-operator --chain-id cheqd --min-self-delegation="1" --gas-prices="25ncheq" --pubkey "${NODE5_PUBKEY}" --commission-max-change-rate="0.02" --commission-max-rate="0.02" --commission-rate="0.01" --gas 500000 --node http://127.0.0.1:26657 --keyring-backend "test" -y
