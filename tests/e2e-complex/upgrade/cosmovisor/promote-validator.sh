4a54d77e59126605e1f1e4e19210e5a79448db2e
Andrew Nikitin <andrew.nikitin@evernym.com>
2022-07-13 13:56:01 +0300

    debug

--- tests/e2e-complex/upgrade/cosmovisor/promote-validator.sh
+++ tests/e2e-complex/upgrade/cosmovisor/promote-validator.sh
@@ -6,7 +6,7 @@ set -euox pipefail
 cheqd-noded keys list --keyring-backend "test" --home "${NODE_CONFIGS_BASE}/client/.cheqdnode"
 
 # Get operator0 address by setting --home flag
-OP1_ADDRESS=$(cheqd-noded keys list --keyring-backend "test" --home "${NODE_CONFIGS_BASE}/client/.cheqdnode" | sed -nr 's/.*address: (.*?).*/\1/p' | sed -n 2p | sed 's/\r//g')
+OP0_ADDRESS=$(cheqd-noded keys list --keyring-backend "test" --home "${NODE_CONFIGS_BASE}/client/.cheqdnode" | sed -nr 's/.*address: (.*?).*/\1/p' | sed -n 1p | sed 's/\r//g')
 
 # Create operator5 by running it under the `cheqd` user.
 sudo su -c 'cheqd-noded keys add node5-operator --keyring-backend "test"' cheqd
@@ -23,11 +23,11 @@ pushd ../
 # shellcheck disable=SC1091
 . common.sh
 
-cheqd_noded_docker query bank balances "${OP1_ADDRESS}"
+cheqd_noded_docker query bank balances "${OP0_ADDRESS}"
 
-local_client_tx tx bank send "${OP1_ADDRESS}" "${OP5_ADDRESS}" 1100000000000000ncheq --chain-id cheqd --fees 5000000ncheq -y
+local_client_tx tx bank send "${OP0_ADDRESS}" "${OP5_ADDRESS}" 11000000000000ncheq --chain-id cheqd --fees 5000000ncheq -y
 
 popd
 
 # Send promote validator from operator5
-sudo -H -u cheqd cheqd-noded tx staking create-validator --amount 1000000000000000ncheq --from node5-operator --chain-id cheqd --min-self-delegation="1" --gas-prices="25ncheq" --pubkey "${NODE5_PUBKEY}" --commission-max-change-rate="0.02" --commission-max-rate="0.02" --commission-rate="0.01" --gas 500000 --node http://127.0.0.1:26657 --keyring-backend "test" -y
+sudo -H -u cheqd cheqd-noded tx staking create-validator --amount 10000000000000ncheq --from node5-operator --chain-id cheqd --min-self-delegation="1" --gas-prices="25ncheq" --pubkey "${NODE5_PUBKEY}" --commission-max-change-rate="0.02" --commission-max-rate="0.02" --commission-rate="0.01" --gas 500000 --node http://127.0.0.1:26657 --keyring-backend "test" -y
