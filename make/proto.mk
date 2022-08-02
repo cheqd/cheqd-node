5a11ff0b77eda309dd7d74682367147c9bf69b25
Andrew Nikitin <andrew.nikitin@evernym.com>
2022-08-02 15:20:56 +0300

    Co-authored-by: Ankur Banerjee <ankurdotb@users.noreply.github.com>

--- make/proto.mk
+++ make/proto.mk
@@ -28,7 +28,7 @@ proto-lint:
 	@$(DOCKER_BUF) lint --error-format=json
 
 proto-check-breaking:
-	@$(DOCKER_BUF) breaking --against https://github.com/cheqd/cheqd-node.git#branch=master
+	@$(DOCKER_BUF) breaking --against https://github.com/cheqd/cheqd-node.git#branch=main
 
 GOGO_PROTO_URL           = https://raw.githubusercontent.com/regen-network/protobuf/cosmos
 GOOGLE_PROTO_URL         = https://raw.githubusercontent.com/googleapis/googleapis/master
