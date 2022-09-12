2aa40a7637c524c75d29d22a9a3e0bbb4bda10d4
Alexandr Kolesov <alexandr.kolesov@cheqd.io>
2022-09-12 12:35:32 +0300

    Move across laptops

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Inits node configuration and runs the node.
 # e -> exit immediately, u -> treat unset variables as errors and immediately, o -> sets the exit code to the rightmost command 
-set -euo pipefail
+set -euox pipefail
 
 # within the container, $HOME=/home/cheqd
 CHEQD_ROOT_DIR="$HOME/.cheqdnode"
