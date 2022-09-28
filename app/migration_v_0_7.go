f52bb0dddcab4a839c654afd410da71948514c88
Andrew Nikitin <andrew.nikitin@cheqd.io>
2022-09-28 11:43:04 +0300

    Add migration
    Signed-off-by: Andrew Nikitin <andrew.nikitin@cheqd.io>

---
+++ app/migration_v_0_7.go
@@ -0,0 +1,17 @@
+package app
+
+import (
+	"crypto/sha256"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func (app *App) Migration07(ctx sdk.Context) {
+	all_resources := app.resourceKeeper.GetAllResources(&ctx)
+	for _, resource := range all_resources {
+		h := sha256.New()
+		h.Write(resource.Data) 
+		resource.Header.Checksum = h.Sum(nil)
+		app.resourceKeeper.SetResource(&ctx, &resource)
+	}
+}
