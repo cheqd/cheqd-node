a99e1c30214a388ddb1fecc5f8121aeef861d5f7
Alexandr Kolesov <kolesov.alex.s@gmail.com>
2022-10-24 17:52:02 +0500

    Add review for migration in format of todo

--- app/migration_v_0_7.go
+++ app/migration_v_0_7.go
@@ -6,6 +6,9 @@ import (
 
 // Here we need to make all the links the
 func (app *App) Migration07(ctx sdk.Context) {
+	// TODO: We have to reset links first. Then we can use GetLastResourceVersionHeader
+	// but only because resources in state are corted by creation time.
+	// Also, we need to avoid loading all resources in memory.
 	resourceList := app.resourceKeeper.GetAllResources(&ctx)
 	for _, resource := range resourceList {
 		previousResourceVersionHeader, found := app.resourceKeeper.GetLastResourceVersionHeader(&ctx, resource.Header.CollectionId, resource.Header.Name, resource.Header.ResourceType)
