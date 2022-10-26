f025ed7871094bc4cf34ee437fcbdf0b9954f198
Alexandr Kolesov <kolesov.alex.s@gmail.com>
2022-10-26 13:39:28 +0500

    Debugging

--- x/cheqd/keeper/msg_server_deactivate_did.go
+++ x/cheqd/keeper/msg_server_deactivate_did.go
@@ -7,7 +7,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-func (k msgServer) DeactivateDid(goCtx context.Context, msg *types.MsgDeactivateDid) (*types.MsgDeactivateDidResponse, error) {
+func (k MsgServer) DeactivateDid(goCtx context.Context, msg *types.MsgDeactivateDid) (*types.MsgDeactivateDidResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
 	// Validate DID does exist
