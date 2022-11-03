201fa1bc655521a66f41b451ccdffcb74fb7bd85
Tasos <50984242+Eengineer1@users.noreply.github.com>
2022-11-03 18:34:35 +0200

    Removed bindings

--- tests/upgrade/setup.go
+++ tests/upgrade/setup.go
@@ -11,12 +11,8 @@ import (
 
 // Setup helpers for upgrade tests
 func Setup() error {
-	By("waiting for chain to start")
-	err := cli.WaitForChainHeight(cli.VALIDATOR0, cli.CLI_BINARY_NAME, 1, 60)
-	Expect(err).To(BeNil())
-
 	By("Copying keys to each container volume")
-	_, err = cli.LocalnetExecCopyKeys()
+	_, err := cli.LocalnetExecCopyKeys()
 	Expect(err).To(BeNil())
 
 	return nil
