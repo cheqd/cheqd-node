29eb07b5d130a0b70cb57f9b1473e8ddcb4d04ee
Tasos <50984242+Eengineer1@users.noreply.github.com>
2022-11-23 17:09:29 +0200

    Debugging

--- tests/integration/cli/tx.go
+++ tests/integration/cli/tx.go
@@ -2,6 +2,7 @@ package cli
 
 import (
 	"encoding/json"
+	"fmt"
 	"strings"
 
 	"github.com/cheqd/cheqd-node/tests/integration/helpers"
@@ -40,7 +41,12 @@ func Tx(module, tx, from string, feeParams []string, txArgs ...string) (sdk.TxRe
 	// Other args
 	args = append(args, txArgs...)
 
+	fmt.Println("tx args: ", strings.Join(args, " "))
+
 	output, err := Exec(args...)
+
+	fmt.Println("tx output: ", output)
+
 	if err != nil {
 		return sdk.TxResponse{}, err
 	}
