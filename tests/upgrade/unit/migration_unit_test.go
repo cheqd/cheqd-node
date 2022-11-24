2ac37a248aa835c095b3a4d02f45760f84831994
Alexandr Kolesov <kolesov.alex.s@gmail.com>
2022-11-24 14:58:50 +0500

    Fixes

--- tests/upgrade/unit/migration_unit_test.go
+++ tests/upgrade/unit/migration_unit_test.go
@@ -4,22 +4,67 @@ import (
 	. "github.com/cheqd/cheqd-node/tests/upgrade/unit/scenarios"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+
+	migrationsetup "github.com/cheqd/cheqd-node/tests/upgrade/unit/setup"
+
+	appmigrations "github.com/cheqd/cheqd-node/app/migrations"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 var _ = Describe("Migration - Unit", func() {
 	It("checks that Checksum migration handler works", func() {
 		By("Ensuring the Checksum migration scenario is successful")
 
-		// Run checksum migration scenario
-		err := RunChecksumScenario()
+		// Init storages, keepers and setup the migration context.
+		setup := migrationsetup.Setup()
+
+		builder := NewChecksumBuilder(setup)
+
+		dataSet, err := builder.BuildDataSet(setup)
+		Expect(err).To(BeNil())
+
+		resourceChecksumScenario := NewMigrationScenario(
+			"ResourceChecksum",
+			func(ctx sdk.Context, migrationCtx appmigrations.MigrationContext) error {
+				return appmigrations.MigrateResourceChecksumV2(ctx, migrationCtx)
+			},
+		)
+		// Init Migrator structure
+		migrator := NewMigrator(
+			[]MigrationScenario{resourceChecksumScenario},
+			setup,
+			&dataSet)
+
+		// Run migration scenario
+		err = migrator.Run()
 		Expect(err).To(BeNil())
 	})
 
 	It("checks that Protobuf migration handler works", func() {
 		By("Ensuring the Protobuf migration handler is working as expected")
 
-		// Run Protobuf migration
-		err := RunProtobufScenario()
+		// Init storages, keepers and setup the migration context.
+		setup := migrationsetup.Setup()
+
+		builder := NewProtobufBuilder(setup)
+
+		dataSet, err := builder.BuildDataSet(setup)
+		Expect(err).To(BeNil())
+
+		resourceProtobufScenario := NewMigrationScenario(
+			"ResourceChecksum",
+			func(ctx sdk.Context, migrationCtx appmigrations.MigrationContext) error {
+				return appmigrations.MigrateDidProtobufV1(ctx, migrationCtx)
+			},
+		)
+		// Init Migrator structure
+		migrator := NewMigrator(
+			[]MigrationScenario{resourceProtobufScenario},
+			setup,
+			&dataSet)
+
+		// Run migration scenario
+		err = migrator.Run()
 		Expect(err).To(BeNil())
 	})
 
@@ -27,7 +72,28 @@ var _ = Describe("Migration - Unit", func() {
 		By("Ensuring the IndyStyle migration handler is working as expected")
 
 		// Run IndyStyle migration
-		err := RunIndyStyleScenario()
+		// Init storages, keepers and setup the migration context.
+		setup := migrationsetup.Setup()
+
+		builder := NewIndyStyleBuilder(setup)
+
+		dataSet, err := builder.BuildDataSet(setup)
+		Expect(err).To(BeNil())
+
+		indyStyleScenario := NewMigrationScenario(
+			"IndyStyle Migration",
+			func(ctx sdk.Context, migrationCtx appmigrations.MigrationContext) error {
+				return appmigrations.MigrateDidIndyStyleIdsV1(ctx, migrationCtx)
+			},
+		)
+		// Init Migrator structure
+		migrator := NewMigrator(
+			[]MigrationScenario{indyStyleScenario},
+			setup,
+			&dataSet)
+
+		// Run migration scenario
+		err = migrator.Run()
 		Expect(err).To(BeNil())
 	})
 })
