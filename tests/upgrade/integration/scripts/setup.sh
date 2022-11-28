e7c595b055cc6f60d5687b4c82db15cc666ebfa9
Andrew Nikitin <andrew.nikitin@cheqd.io>
2022-11-28 18:28:26 +0300

    Unstash

--- tests/upgrade/integration/scripts/setup.sh
+++ tests/upgrade/integration/scripts/setup.sh
@@ -8,7 +8,7 @@ pushd "$DIR/../../../../docker/localnet"
 
 # Generate configs (make sure old binary is installed locally)
 bash gen-network-config.sh
-sudo chown -R 1000:1000 network-config
+# sudo chown -R 1000:1000 network-config
 
 # Start network
 docker compose --env-file mainnet-latest.env up --detach --no-build
@@ -23,9 +23,9 @@ sudo docker compose --env-file mainnet-latest.env cp network-config/validator-2/
 sudo docker compose --env-file mainnet-latest.env cp network-config/validator-3/keyring-test validator-3:/home/cheqd/.cheqdnode
 
 # Restore permissions
-sudo docker compose --env-file mainnet-latest.env exec -it --user root validator-0 chown -R cheqd:cheqd /home/cheqd
-sudo docker compose --env-file mainnet-latest.env exec -it --user root validator-1 chown -R cheqd:cheqd /home/cheqd
-sudo docker compose --env-file mainnet-latest.env exec -it --user root validator-2 chown -R cheqd:cheqd /home/cheqd
-sudo docker compose --env-file mainnet-latest.env exec -it --user root validator-3 chown -R cheqd:cheqd /home/cheqd
+sudo docker compose --env-file mainnet-latest.env exec --user root validator-0 chown -R cheqd:cheqd /home/cheqd
+sudo docker compose --env-file mainnet-latest.env exec --user root validator-1 chown -R cheqd:cheqd /home/cheqd
+sudo docker compose --env-file mainnet-latest.env exec --user root validator-2 chown -R cheqd:cheqd /home/cheqd
+sudo docker compose --env-file mainnet-latest.env exec --user root validator-3 chown -R cheqd:cheqd /home/cheqd
 
 popd
