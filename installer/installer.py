93fd2a4aea1e9dda55fc1b01e34dfba330ca6bb7
Benyam Seifu <benyamseifu@Benyams-Air.bbrouter>
2022-11-28 16:02:59 +0300

    search and replace using python

--- installer/installer.py
+++ installer/installer.py
@@ -82,6 +82,12 @@ def sigint_handler(signal, frame):
 
 signal.signal(signal.SIGINT, sigint_handler)
 
+def search_and_replace(search_text, replace_text, file_path):
+    with open(file_path, 'r') as file:
+        data = file.read()
+        data = data.replace(search_text, replace_text)
+    with open(file_path, 'w') as file:
+        file.write(data)    
 
 class Release:
     def __init__(self, release_map):
@@ -428,26 +434,26 @@ class Installer():
 
         # Setting up the external_address
         if self.interviewer.external_address:
-            external_address_value_to_replace='external_address = ""'
-            self.exec(f"""sudo su -c 'perl -0777 -i.original -pe 's/{external_address_value_to_replace}/"{self.interviewer.external_address}:{self.interviewer.p2p_port}"/igs' {os.path.join(self.cheqd_config_dir, "config.toml")}' {DEFAULT_CHEQD_USER}""")
-
+            external_address_search_text='external_address = ""'
+            external_address_replace_text='external_address = "{self.interviewer.external_address}:{self.interviewer.p2p_port}"'
+            search_and_replace(external_address_search_text, external_address_replace_text, os.path.join(self.cheqd_config_dir, "config.toml"))
         # Setting up the seeds
         seeds = self.exec(f"curl {SEEDS_FILE.format(self.interviewer.chain)}").stdout.decode("utf-8").strip()
-        seeds_value_to_replace = 'seeds = ""'
-        self.exec(f"""sudo su -c 'perl -0777 -i.original -pe 's/{seeds_value_to_replace}/{seeds}/igs' {os.path.join(self.cheqd_config_dir, "config.toml")}' {DEFAULT_CHEQD_USER}""")
-
+        seeds_search_text = 'seeds = ""'
+        seeds_replace_text= 'seeds = "{seeds}"'
+        search_and_replace(seeds_search_text, seeds_replace_text, os.path.join(self.cheqd_config_dir, "config.toml"))
         # Setting up the RPC port
-        rpc_laddr_text_fragment_to_be_replaced= '# TCP or UNIX socket address for the RPC server to listen on\nladdr = "tcp:\/\/0.0.0.0:{DEFAULT_RPC_PORT}"'
-        self.exec(f"""sudo su -c 'perl -0777 -i.original -pe 's/{rpc_laddr_text_fragment_to_be_replaced}/# TCP or UNIX socket address for the RPC server to listen on\nladdr = "tcp:\/\/0.0.0.0:{self.interviewer.rpc_port}"/igs' {os.path.join(self.cheqd_config_dir, "config.toml")}' {DEFAULT_CHEQD_USER}""")
-
+        rpc_laddr_replace_text= '# TCP or UNIX socket address for the RPC server to listen on\nladdr = "tcp://0.0.0.0:{DEFAULT_RPC_PORT}"'
+        rpc_laddr_search_text= '# TCP or UNIX socket address for the RPC server to listen on\nladdr = "tcp://0.0.0.0:{self.interviewer.rpc_port}"'
+        search_and_replace(rpc_laddr_search_text,rpc_laddr_replace_text, os.path.join(self.cheqd_config_dir, "config.toml"))
         # Setting up the P2P port
-        p2p_laddr_text_fragment_to_be_replaced='# Address to listen for incoming connections\nladdr = "tcp:\/\/0.0.0.0:{DEFAULT_P2P_PORT}"'
-        self.exec(f"""sudo su -c 'perl -0777 -i.original -pe 's/{p2p_laddr_text_fragment_to_be_replaced}/# Address to listen for incoming connections\nladdr = "tcp:\/\/0.0.0.0:{self.interviewer.p2p_port}"/igs' {os.path.join(self.cheqd_config_dir, "config.toml")}' {DEFAULT_CHEQD_USER}""")
-
+        p2p_laddr_search_text='# Address to listen for incoming connections\nladdr = "tcp://0.0.0.0:{DEFAULT_P2P_PORT}"'
+        p2p_laddr_replace_text='# Address to listen for incoming connections\nladdr = "tcp://0.0.0.0:{self.interviewer.p2p_port}"'
+        search_and_replace(p2p_laddr_search_text,p2p_laddr_replace_text, os.path.join(self.cheqd_config_dir, "config.toml"))
+        
         # Setting up min gas-price
         min_gas_price_value_to_be_replaced='minimum-gas-prices = {DEFAULT_GAS_PRICE}'
-        self.exec(f"""sudo su -c 'perl -0777 -i.original -pe 's/{min_gas_price_value_to_be_replaced}/'minimum-gas-prices = "{self.interviewer.gas_price}"'/igs' {os.path.join(self.cheqd_config_dir, "app.toml")}' {DEFAULT_CHEQD_USER}""")
-
+        search_and_replace(min_gas_price_value_to_be_replaced, 'minimum-gas-prices = "{self.interviewer.gas_price}"', os.path.join(self.cheqd_config_dir, "app.toml"))
 
     def prepare_cheqd_user(self):
         try:
