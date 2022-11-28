df3491d16fdffdec685a896da67dc32011c6e0d6
Benyam Seifu <benyamseifu@Benyams-Air.bbrouter>
2022-11-28 17:01:50 +0300

    add trailing spaces

--- installer/installer.py
+++ installer/installer.py
@@ -443,12 +443,13 @@ class Installer():
         seeds_replace_text= 'seeds = "{}"'.format(seeds)
         search_and_replace(seeds_search_text, seeds_replace_text, os.path.join(self.cheqd_config_dir, "config.toml"))
         # Setting up the RPC port
-        rpc_laddr_replace_text= '# TCP or UNIX socket address for the RPC server to listen on\nladdr = "tcp://0.0.0.0:{}"'.format(DEFAULT_RPC_PORT)
-        rpc_laddr_search_text= '# TCP or UNIX socket address for the RPC server to listen on\nladdr = "tcp://0.0.0.0:{}"'.format(self.interviewer.rpc_port)
+        rpc_laddr_replace_text= '# TCP or UNIX socket address for the RPC server to listen on \nladdr = "tcp://0.0.0.0:{}"'.format(DEFAULT_RPC_PORT)
+        
+        rpc_laddr_search_text= '# TCP or UNIX socket address for the RPC server to listen on \nladdr = "tcp://0.0.0.0:{}"'.format(self.interviewer.rpc_port)
         search_and_replace(rpc_laddr_search_text,rpc_laddr_replace_text, os.path.join(self.cheqd_config_dir, "config.toml"))
         # Setting up the P2P port
-        p2p_laddr_search_text='# Address to listen for incoming connections\nladdr = "tcp://0.0.0.0:{}"'.format(DEFAULT_P2P_PORT)
-        p2p_laddr_replace_text='# Address to listen for incoming connections\nladdr = "tcp://0.0.0.0:{}"'.format(self.interviewer.p2p_port)
+        p2p_laddr_search_text='# Address to listen for incoming connections \nladdr = "tcp://0.0.0.0:{}"'.format(DEFAULT_P2P_PORT)
+        p2p_laddr_replace_text='# Address to listen for incoming connections \nladdr = "tcp://0.0.0.0:{}"'.format(self.interviewer.p2p_port)
         search_and_replace(p2p_laddr_search_text,p2p_laddr_replace_text, os.path.join(self.cheqd_config_dir, "config.toml"))
         
         # Setting up min gas-price
