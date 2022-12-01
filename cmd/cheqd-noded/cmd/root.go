53c1b5c2c90d0883734628b5e7d9054689bb8b30
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2022-12-01 18:55:35 +0000

    Set --gas flag correctly

--- cmd/cheqd-noded/cmd/root.go
+++ cmd/cheqd-noded/cmd/root.go
@@ -162,10 +162,10 @@ func txCommand() *cobra.Command {
 	)
 
 	app.ModuleBasics.AddTxCommands(cmd)
-	cmd.PersistentFlags().String(flags.FlagChainID, , "The network chain ID")
+	cmd.PersistentFlags().String(flags.FlagChainID, "cheqd-mainnet-1", "The network chain ID")
 	cmd.PersistentFlags().String(flags.FlagGasPrices, "50ncheq", "Gas prices in decimal format")
 	cmd.PersistentFlags().Float64(flags.FlagGasAdjustment, 1.3, "Gas adjustment factor to be multiplied against estimated gas")
-	cmd.PersistentFlags().String(flags.GasFlagAuto, "auto", "Automatically estimate gas")
+	cmd.PersistentFlags().String(flags.FlagGas, flags.GasFlagAuto, "Gas limit to set per-transaction; set to 'auto' to calculate sufficient gas automatically")
 
 	return cmd
 }
