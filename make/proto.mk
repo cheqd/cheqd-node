adac12dfc39865809116e76e17916b6fe226536d
Benyam Seifu <binychanyalew@gmail.com>
2022-12-05 18:17:47 +0300

    set up proto.mk for swagger functionality

--- make/proto.mk
+++ make/proto.mk
@@ -27,41 +27,41 @@ DOCKER_BUF := docker run -v $(shell pwd):/workspace --workdir /workspace bufbuil
 # Uncomment next lines while working on pulsar and swagger functionality
 
 
-# proto-lint:
-# 	@$(DOCKER_BUF) lint --error-format=json
+proto-lint:
+	@$(DOCKER_BUF) lint --error-format=json
 
-# proto-check-breaking:
-# 	@$(DOCKER_BUF) breaking --against https://github.com/cheqd/cheqd-node.git#branch=main
+proto-check-breaking:
+	@$(DOCKER_BUF) breaking --against https://github.com/cheqd/cheqd-node.git#branch=main
 
-# GOGO_PROTO_URL           = https://raw.githubusercontent.com/regen-network/protobuf/cosmos
-# GOOGLE_PROTO_URL         = https://raw.githubusercontent.com/googleapis/googleapis/master
-# COSMOS_PROTO_URL         = https://raw.githubusercontent.com/cosmos/cosmos-sdk/v0.45.4/proto/cosmos
-# COSMOS_ORM_PROTO_URL     = https://raw.githubusercontent.com/cosmos/cosmos-sdk/orm/v1.0.0-alpha.10/proto/cosmos
+GOGO_PROTO_URL           = https://raw.githubusercontent.com/regen-network/protobuf/cosmos
+GOOGLE_PROTO_URL         = https://raw.githubusercontent.com/googleapis/googleapis/master
+COSMOS_PROTO_URL         = https://raw.githubusercontent.com/cosmos/cosmos-sdk/v0.45.4/proto/cosmos
+COSMOS_ORM_PROTO_URL     = https://raw.githubusercontent.com/cosmos/cosmos-sdk/orm/v1.0.0-alpha.10/proto/cosmos
 
-# GOGO_PROTO_TYPES         = third_party/proto/gogoproto
-# GOOGLE_PROTO_TYPES       = third_party/proto/google
-# COSMOS_PROTO_TYPES       = third_party/proto/cosmos
+GOGO_PROTO_TYPES         = third_party/proto/gogoproto
+GOOGLE_PROTO_TYPES       = third_party/proto/google
+COSMOS_PROTO_TYPES       = third_party/proto/cosmos
 
-# proto-swagger-deps:
-# 	@mkdir -p $(GOGO_PROTO_TYPES)
-# 	@curl -sSL $(GOGO_PROTO_URL)/gogoproto/gogo.proto > $(GOGO_PROTO_TYPES)/gogo.proto
+proto-swagger-deps:
+	@mkdir -p $(GOGO_PROTO_TYPES)
+	@curl -sSL $(GOGO_PROTO_URL)/gogoproto/gogo.proto > $(GOGO_PROTO_TYPES)/gogo.proto
 
-# 	@mkdir -p $(GOOGLE_PROTO_TYPES)/api/
-# 	@curl -sSL $(GOOGLE_PROTO_URL)/google/api/annotations.proto > $(GOOGLE_PROTO_TYPES)/api/annotations.proto
-# 	@curl -sSL $(GOOGLE_PROTO_URL)/google/api/http.proto > $(GOOGLE_PROTO_TYPES)/api/http.proto
+	@mkdir -p $(GOOGLE_PROTO_TYPES)/api/
+	@curl -sSL $(GOOGLE_PROTO_URL)/google/api/annotations.proto > $(GOOGLE_PROTO_TYPES)/api/annotations.proto
+	@curl -sSL $(GOOGLE_PROTO_URL)/google/api/http.proto > $(GOOGLE_PROTO_TYPES)/api/http.proto
 
-# 	@mkdir -p $(COSMOS_PROTO_TYPES)/base/v1beta1/
-# 	@curl -sSL $(COSMOS_PROTO_URL)/base/v1beta1/coin.proto > $(COSMOS_PROTO_TYPES)/base/v1beta1/coin.proto
+	@mkdir -p $(COSMOS_PROTO_TYPES)/base/v1beta1/
+	@curl -sSL $(COSMOS_PROTO_URL)/base/v1beta1/coin.proto > $(COSMOS_PROTO_TYPES)/base/v1beta1/coin.proto
 
-# 	@mkdir -p $(COSMOS_PROTO_TYPES)/base/query/v1beta1/
-# 	@curl -sSL $(COSMOS_PROTO_URL)/base/query/v1beta1/pagination.proto > $(COSMOS_PROTO_TYPES)/base/query/v1beta1/pagination.proto
+	@mkdir -p $(COSMOS_PROTO_TYPES)/base/query/v1beta1/
+	@curl -sSL $(COSMOS_PROTO_URL)/base/query/v1beta1/pagination.proto > $(COSMOS_PROTO_TYPES)/base/query/v1beta1/pagination.proto
 
-# 	@mkdir -p $(COSMOS_PROTO_TYPES)/orm/v1alpha1/
-# 	@curl -sSL $(COSMOS_ORM_PROTO_URL)/orm/v1alpha1/orm.proto > $(COSMOS_PROTO_TYPES)/orm/v1alpha1/orm.proto
+	@mkdir -p $(COSMOS_PROTO_TYPES)/orm/v1alpha1/
+	@curl -sSL $(COSMOS_ORM_PROTO_URL)/orm/v1alpha1/orm.proto > $(COSMOS_PROTO_TYPES)/orm/v1alpha1/orm.proto
 
-# proto-swagger-gen: proto-swagger-deps
-# 	@echo "Generating Protobuf Swagger"
-# 	@if docker ps -a --format '{{.Names}}' | grep -Eq "^${containerProtoGenSwagger}$$"; then docker start -a $(containerProtoGenSwagger); else docker run --name $(containerProtoGenSwagger) -v $(CURDIR):/workspace --workdir /workspace $(containerProtoImage) \
-# 		sh ./scripts/protoc-swagger-gen.sh; fi
+proto-swagger-gen: proto-swagger-deps
+	@echo "Generating Protobuf Swagger"
+	@if docker ps -a --format '{{.Names}}' | grep -Eq "^${containerProtoGenSwagger}$$"; then docker start -a $(containerProtoGenSwagger); else docker run --name $(containerProtoGenSwagger) -v $(CURDIR):/workspace --workdir /workspace $(containerProtoImage) \
+		sh ./scripts/protoc-swagger-gen.sh; fi
 
-.PHONY: proto-all proto-gen proto-format #proto-lint proto-check-breaking proto-swagger-deps proto-swagger-gen
+.PHONY: proto-all proto-gen proto-format proto-lint proto-check-breaking proto-swagger-deps proto-swagger-gen
