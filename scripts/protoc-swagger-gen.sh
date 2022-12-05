9708fc85c04f8c9c7bae78da475c1789e68a55d6
Benyam Seifu <binychanyalew@gmail.com>
2022-12-05 19:33:24 +0300

    use -n instead of ! -z

--- scripts/protoc-swagger-gen.sh
+++ scripts/protoc-swagger-gen.sh
@@ -12,7 +12,7 @@ proto_dirs=$(find ./cheqd -path -prune -o -name '*.proto' -print0 | xargs -0 -n1
 for dir in $proto_dirs; do
   # generate swagger files (filter query files)
   query_file=$(find "${dir}" -maxdepth 1 \( -name 'query.proto' \))
-  if [[ ! -n "$query_file" ]]; then
+  if [[ -n "$query_file" ]]; then
     buf generate --template buf.gen.swagger.yaml "$query_file"
   fi
 done
