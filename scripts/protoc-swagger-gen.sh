68b0e9dac3246a3a1ef1d64980e3bc8a2f51c382
Benyam Seifu <binychanyalew@gmail.com>
2022-12-05 18:16:00 +0300

    add swagger gen script

---
+++ scripts/protoc-swagger-gen.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+SWAGGER_DIR=./app/client/docs
+
+cd ./proto
+
+
+# find all proto directories
+proto_dirs=$(find ./cheqd -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+
+# loop through proto directories
+for dir in $proto_dirs; do
+  # generate swagger files (filter query files)
+  query_file=$(find "${dir}" -maxdepth 1 \( -name 'query.proto' \))
+  if [[ ! -z "$query_file" ]]; then
+    buf generate --template buf.gen.swagger.yaml $query_file
+  fi
+done
