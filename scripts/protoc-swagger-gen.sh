8b382e1b3c9cfd9676210506e26ee2f8bb76bd67
Benyam Seifu <binychanyalew@gmail.com>
2022-12-06 11:51:42 +0300

    update protoc-swagger-gen.sh to use swagger-combine

--- scripts/protoc-swagger-gen.sh
+++ scripts/protoc-swagger-gen.sh
@@ -16,3 +16,16 @@ for dir in $proto_dirs; do
     buf generate --template buf.gen.swagger.yaml "$query_file"
   fi
 done
+
+cd ..
+
+# install swagger-combine if not already installed
+npm list -g | grep swagger-combine > /dev/null || npm install -g swagger-combine --no-shrinkwrap
+
+# combine swagger files
+# uses nodejs package `swagger-combine`.
+# all the individual swagger files need to be configured in `config.json` for merging
+swagger-combine ./client/docs/config.json -o ./client/docs/swagger-ui/swagger.yaml -f yaml --continueOnConflictingPaths true --includeDefinitions true
+
+# clean swagger files
+rm -rf ./tmp-swagger-gen
