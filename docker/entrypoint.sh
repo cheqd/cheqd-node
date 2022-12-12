a0d6f1810605f788e43da4cef9453e582f65ef67
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2022-12-12 15:17:12 +0000

    Add key import to entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,11 +7,11 @@ set -euo pipefail
 # within the container, $HOME=/home/cheqd
 CHEQD_ROOT_DIR="$HOME/.cheqdnode"
 
-# Init node config directory
+# Initialise node config directory
 if [ ! -d "${CHEQD_ROOT_DIR}/config" ]
 then
-    echo "Node config not found. Initializing."
-    cheqd-noded init "moniker-placeholder"
+    echo "Node configuration directory not found. Initializing."
+    cheqd-noded init "${CHEQD_NODED_MONIKER}"
 else
     echo "Node config exists. Skipping initialization."
 fi
@@ -19,7 +19,7 @@ fi
 # Check if a genesis file has been passed in config
 if [ -f "/genesis" ]
 then
-    echo "Genesis file passed. Adding/replacing current genesis file."
+    echo "Genesis file passed. Replacing current genesis file."
     cp /genesis "${CHEQD_ROOT_DIR}/config/genesis.json"
 else
     echo "No genesis file passed. Skipping and retaining existing genesis."
@@ -28,7 +28,7 @@ fi
 # Check if a seeds file has been passed in config
 if [ -f "/seeds" ]
 then
-    echo "Seeds file passed. Overriding current seeds."
+    echo "Seeds file passed. Replacing current seeds."
     cp /seeds "${CHEQD_ROOT_DIR}/config/seeds.txt"
     CHEQD_NODED_P2P_SEEDS="$(cat "${CHEQD_ROOT_DIR}/config/seeds.txt")"
     export CHEQD_NODED_P2P_SEEDS
@@ -37,21 +37,48 @@ else
 fi
 
 # Check if a node_key file has been passed in config
-if [ -f "/node_key" ]
+if [ -f "/node-key" ]
 then
-    echo "Node key file passed. Overriding current key."
-    cp /node_key "${CHEQD_ROOT_DIR}/config/node_key.json"
+    echo "node_key.json file passed. Replacing existing node_key.json."
+    cp /node-key "${CHEQD_ROOT_DIR}/config/node_key.json"
 else
     echo "No node key file passed. Skipping and retaining existing node key."
 fi
 
 # Check if a priv_validator_key file has been passed in config
-if [ -f "/priv_validator_key" ]
+if [ -f "/private-validator-key" ]
 then
-    echo "Private validator key file passed. Replacing current key."
-    cp /priv_validator_key "${CHEQD_ROOT_DIR}/config/priv_validator_key.json"
+    echo "priv_validator_key.json file passed. Replacing current validator key."
+    cp /private-validator-key "${CHEQD_ROOT_DIR}/config/priv_validator_key.json"
 else
     echo "No private validator key file passed. Skipping and retaining existing key."
+fi
+
+# Check if a priv_validator_state file has been passed in config
+if [ -f "/private-validator-state" ]
+then
+    echo "priv_validator_state.json file passed. Replacing current validator state."
+    cp /private-validator-state "${CHEQD_ROOT_DIR}/data/priv_validator_state.json"
+else
+    echo "No private validator state file passed. Skipping and retaining existing validator state."
+fi
+
+# Check if a upgrade_info file has been passed in config
+if [ -f "/upgrade-info" ]
+then
+    echo "upgrade_info.json file passed. Replacing current upgrade_info.json file."
+    cp /upgrade-info "${CHEQD_ROOT_DIR}/data/upgrade-info.json"
+else
+    echo "No upgrade_info.json file passed. Skipping and retaining existing upgrade_info.json file."
+fi
+
+# Check if an account key mnemonic file has been passed in config
+if [ -f "/account-keys" ]
+then
+    echo "Account key mnemonic file passed. Importing key."
+    cheqd-noded keys add "${ACCOUNT_KEY_NAME}" --keyring-backend "${KEYRING_BACKEND}" --recover < /account-keys
+else
+    echo "No account key mnemonic passed. No keys imported."
 fi
 
 # Run node
