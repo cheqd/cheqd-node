5a7c3ffef5138c57e8987b21ede98a38f8ad60ef
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2022-12-21 11:04:28 +0000

    Remove strip components

--- installer/installer.py
+++ installer/installer.py
@@ -263,7 +263,7 @@ class Installer():
         try:
             self.exec(f"wget -c {binary_url}")
             if fname.find(".tar.gz") != -1:
-                self.exec(f"tar -xzf {fname} -C . --strip-components=1")
+                self.exec(f"tar -xzf {fname} -C .")
                 self.remove_safe(fname)
             self.exec(f"chmod +x {DEFAULT_BINARY_NAME}")
         except:
