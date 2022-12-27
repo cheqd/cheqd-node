3eb43efddc33a9911b254f810fe0f80405db115e
Andrew Nikitin <andrew.nikitin@cheqd.io>
2022-12-27 18:48:18 +0300

    Fix unit tests after changing field in VerifictionMethod

--- app/migrations/migration_did_version_id.go
+++ app/migrations/migration_did_version_id.go
@@ -8,12 +8,12 @@ import (
 
 // This migration should be run after protobuf that's why we use new DidDocWithMetadata
 func MigrateDidVersionID(sctx sdk.Context, mctx MigrationContext) error {
-	sctx.Logger().Debug("MigrateDidVersionId: Starting migration")
+	sctx.Logger().Debug("MigrateDidVersionID: Starting migration")
 
 	return MigrateDidSimple(sctx, mctx, func(didDocWithMetadata *didtypes.DidDocWithMetadata) {
-		sctx.Logger().Debug("MigrateDidVersionId: DID: " + didDocWithMetadata.DidDoc.Id + " OldVersionId: " + didDocWithMetadata.Metadata.VersionId)
+		sctx.Logger().Debug("MigrateDidVersionID: DID: " + didDocWithMetadata.DidDoc.Id + " OldVersionId: " + didDocWithMetadata.Metadata.VersionId)
 		versionID := uuid.NewSHA1(uuid.Nil, []byte(didDocWithMetadata.DidDoc.Id))
 		didDocWithMetadata.Metadata.VersionId = versionID.String()
-		sctx.Logger().Debug("MigrateDidVersionId: DID: " + didDocWithMetadata.DidDoc.Id + " NewVersionId: " + didDocWithMetadata.Metadata.VersionId)
+		sctx.Logger().Debug("MigrateDidVersionID: DID: " + didDocWithMetadata.DidDoc.Id + " NewVersionId: " + didDocWithMetadata.Metadata.VersionId)
 	})
 }
