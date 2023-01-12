5ecd4b41950f2310e244feed089b6201ac12dc13
Tasos Derisiotis <50984242+Eengineer1@users.noreply.github.com>
2023-01-12 15:57:02 +0200

    Added descriptive resource annotations, in-line with did module

--- x/resource/types/error.go
+++ x/resource/types/error.go
@@ -9,7 +9,7 @@ import (
 // x/resource module sentinel errors
 var (
 	ErrBadRequest      = sdkerrors.Register(ModuleName, 2000, "bad request")
-	ErrResourceExists  = sdkerrors.Register(ModuleName, 2200, "Resoure exists")
+	ErrResourceExists  = sdkerrors.Register(ModuleName, 2200, "Resource exists")
 	ErrBasicValidation = sdkerrors.Register(ModuleName, 2205, "basic validation failed")
 	ErrInternal        = sdkerrors.Register(ModuleName, 2500, "internal error")
 )
