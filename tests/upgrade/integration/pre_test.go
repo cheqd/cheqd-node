703896db12d8e80a3da61cf1155bf4ffa9805aaa
Tasos Derisiotis <50984242+Eengineer1@users.noreply.github.com>
2023-01-19 23:11:51 +0200

    Removed redundant

--- tests/upgrade/integration/pre_test.go
+++ tests/upgrade/integration/pre_test.go
@@ -11,7 +11,7 @@ import (
 	didtypesv1 "github.com/cheqd/cheqd-node/x/did/types/v1"
 	resourcetypesv1 "github.com/cheqd/cheqd-node/x/resource/types/v1"
 	govtypesv1beta1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1beta1"
-	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+	// upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types" <-- TODO: uncomment when whole sequence of upgrade tests is ready
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
@@ -22,19 +22,6 @@ var _ = Describe("Upgrade - Pre", func() {
 			By("pinging the node status until the dvoting end height is reached")
 			err := cli.WaitForChainHeight(cli.Validator0, cli.CliBinaryName, cli.BootstrapHeight, cli.BootstrapPeriod)
 			Expect(err).To(BeNil())
-		})
-
-		It("should match the expected module version map", func() {
-			By("loading the expected module version map")
-			var expected upgradetypes.QueryModuleVersionsResponse
-			_, err := Loader(filepath.Join(GeneratedJSONDir, "pre", "query - module-version-map", "v069.json"), &expected)
-			Expect(err).To(BeNil())
-
-			By("matching the expected module version map")
-			actual, err := cli.QueryModuleVersionMap(cli.Validator0)
-			Expect(err).To(BeNil())
-
-			Expect(actual.ModuleVersions).To(Equal(expected.ModuleVersions), "module version map mismatch")
 		})
 
 		It("should load and run existing diddoc payloads - case: create", func() {
@@ -111,7 +98,6 @@ var _ = Describe("Upgrade - Pre", func() {
 				_, err = cli.LocalnetExecCopyAbsoluteWithPermissions(ResourceFile, cli.DockerHome, cli.Validator0)
 				Expect(err).To(BeNil())
 
-				// TODO: Add resource file. Right now, it is not possible to create a resource without a file. So we need to copy a file to the container home directory.
 				res, err := cli.CreateResourceLegacy(
 					ResourceCreatePayload.CollectionId,
 					ResourceCreatePayload.Id,
