5a1a0b8eb5948c67ef66973ab9bf1a58199efe81
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2023-01-20 12:37:11 +0000

    Update tx_update_diddoc.go
    - Added example payload file
    - Clarified fees

--- x/did/client/cli/tx_update_diddoc.go
+++ x/did/client/cli/tx_update_diddoc.go
@@ -15,12 +15,52 @@ import (
 
 func CmdUpdateDidDoc() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "update-did [payload-file]",
+		Use:   "update-did [payload-file] --version-id [version-id]",
 		Short: "Updates a DID and its associated DID Document.",
 		Long: `Update DID Document associated with a given DID. 
 [payload-file] is JSON encoded DID Document alongside with sign inputs. 
 Version ID is optional and is determined by the '--version-id' flag. 
-If not provided, a random UUID will be used as version-id.`,
+If not provided, a random UUID will be used as version-id.
+
+NOTE: DID update operations require the FULL new DID Document to be provided. Specifying just the changes is not supported.
+
+Payload file should be a JSON file containing properties specified in the DID Core Specification. Rules from DID Core spec are followed on which properties are mandatory and which ones are optional.
+
+Example payload file:
+{
+    "didDoc": {
+        "context": [ "https://www.w3.org/ns/did/v1" ],
+        "id": "did:cheqd:<namespace>:<unique-identifier>",
+        "controller": [
+            "did:cheqd:<namespace>:<unique-identifier>"
+        ],
+        "authentication": [
+            "did:cheqd:<namespace>:<unique-identifier>#<key-id>"
+        ],
+        "assertionMethod": [],
+        "capabilityInvocation": [],
+        "capabilityDelegation": [],
+        "keyAgreement": [],
+        "alsoKnownAs": [],
+        "verificationMethod": [
+            {
+                "id": "did:cheqd:<namespace>:<unique-identifier>#<key-id>",
+                "verificationMethodType": "<verification-method-type>",
+                "controller": "did:cheqd:<namespace>:<unique-identifier>",
+                "publicKeyMultibase": "<public-key>"
+            }
+        ],
+        "service": [
+			{
+                "id": "did:cheqd:<namespace>:<unique-identifier>#<service-id>",
+                "serviceType": "<service-type>",
+                "serviceEndpoint": [
+                    "<service-endpoint>"
+                ]
+            }
+		]
+    }
+}`,
 		Args: cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientTxContext(cmd)
@@ -104,7 +144,7 @@ If not provided, a random UUID will be used as version-id.`,
 
 	// add custom / override flags
 	cmd.Flags().String(FlagVersionID, "", "Version ID of the DID Document")
-	cmd.Flags().String(flags.FlagFees, sdk.NewCoin(types.BaseMinimalDenom, sdk.NewInt(types.DefaultUpdateDidTxFee)).String(), "Fees to pay along with transaction; eg: 25000000000ncheq")
+	cmd.Flags().String(flags.FlagFees, sdk.NewCoin(types.BaseMinimalDenom, sdk.NewInt(types.DefaultUpdateDidTxFee)).String(), "Fixed fee for DID update, e.g,, 25000000000ncheq. Please check what the current fees by running 'cheqd-noded query <insert query>")
 
 	_ = cmd.MarkFlagRequired(flags.FlagFees)
 	_ = cmd.MarkFlagRequired(flags.FlagGas)
