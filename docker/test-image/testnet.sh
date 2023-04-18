652121a15b83baed0dc99e695cdf0030f22418cb
Andrew Nikitin <andrew.nikitin@cheqd.io>
2023-04-18 10:16:05 +0400

    Makes linter happy

--- docker/test-image/testnet.sh
+++ docker/test-image/testnet.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -euo pipefail
 
 # Enable config parameters as environment variables
 source validator-0.env
@@ -16,4 +17,4 @@ mv network-config/validator-0/* ${CHEQD_ROOT_DIR}
 NODE_ID=$(cheqd-noded tendermint show-node-id)
 
 # Start node
-cheqd-noded start --home ${CHEQD_ROOT_DIR} --log_level info --log_format json --trace --p2p.persistent_peers $NODE_ID@${NODE_MONIKER}:26656
+cheqd-noded start --home ${CHEQD_ROOT_DIR} --log_level info --log_format json --trace --p2p.persistent_peers "$NODE_ID"@"$NODE_MONIKER":26656
