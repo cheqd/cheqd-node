d8a9bf87b27fb208ec7a865eb948ccd19ed015ac
Andrew Nikitin <andrew.nikitin@cheqd.io>
2023-04-18 09:58:47 +0400

    Add publishing test image for other projects

---
+++ docker/test-image/testnet.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Enable config parameters as environment variables
+source validator-0.env
+
+HOME_DIR="/home/cheqd"
+CHEQD_ROOT_DIR="${HOME_DIR}/.cheqdnode/"
+TESTNET_NAME="cheqd"
+NODE_MONIKER="node0"
+
+mkdir -p ${CHEQD_ROOT_DIR}
+
+# Generate network config
+bash -x gen-network ${TESTNET_NAME} 1 0 0
+mv network-config/validator-0/* ${CHEQD_ROOT_DIR}
+NODE_ID=$(cheqd-noded tendermint show-node-id)
+
+# Start node
+cheqd-noded start --home ${CHEQD_ROOT_DIR} --log_level info --log_format json --trace --p2p.persistent_peers $NODE_ID@${NODE_MONIKER}:26656
