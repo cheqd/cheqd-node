51e42bd21b73b504da325e07f99364c89f4113da
Andrew Nikitin <andrew.nikitin@cheqd.io>
2023-04-18 10:43:07 +0400

    Linter

--- docker/test-image/testnet.sh
+++ docker/test-image/testnet.sh
@@ -8,7 +8,7 @@ NODE_MONIKER="node0"
 
 # Enable config parameters as environment variables
 # shellcheck source=validator-0.env
-source validator-0.env
+source "$HOME_DIR/validator-0.env"
 
 mkdir -p ${CHEQD_ROOT_DIR}
 
