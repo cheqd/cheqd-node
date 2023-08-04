1793ccbd5977d7a5c33acce83fc2debe0ab602f7
bwty <whalelephant@users.noreply.github.com>
2023-08-04 18:11:52 +0800

    chore: remove deadcode

--- x/resource/tests/setup/setup_port.go
+++ x/resource/tests/setup/setup_port.go
@@ -5,10 +5,6 @@ import (
 	"github.com/cheqd/cheqd-node/x/resource/types"
 )
 
-func (s *TestSetup) StorePort() {
-	s.ResourceKeeper.SetPort(s.SdkCtx, types.ResourcePortId)
-}
-
 func (s *TestSetup) StorePortWithGenesis() {
 	genesis := types.DefaultGenesis()
 	resource.InitGenesis(s.SdkCtx, s.ResourceKeeper, genesis)
