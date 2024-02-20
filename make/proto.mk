c7c4554d42673e4d1ef38c2c4274d87f818d527e
Deepan kumar <150890063+deepan95dev@users.noreply.github.com>
2024-02-20 14:02:18 +0530

    ci: Go releaser fix (#689) (#693)
    * fix: release permission check
    
    * try fix go releaser
    
    * touch
    
    * touch
    
    * release check
    
    * try fix
    
    * release check
    
    * fix swagger
    
    * fix swagger gen
    
    * fix
    
    * release check
    
    * update goreleaser config
    
    * revert changes
    
    * fix: ante handler ibc fix
    
    * review changes
    
    ---------
    
    Co-authored-by: atheesh <atheesh@vitwit.com>
    Co-authored-by: akhilkumarpilli <akhilkumar7947@gmail.com>

--- make/proto.mk
+++ make/proto.mk
@@ -16,6 +16,7 @@ protoImage=$(DOCKER) run --rm -v $(CURDIR):/workspace --workdir /workspace $(con
 proto-all: proto-gen proto-swagger-gen
 
 proto-gen:
+	sudo find ../ -type d -exec chmod 777 {} +
 	@echo "Generating Protobuf files"
 	@$(protoImage) sh ./scripts/protocgen.sh;
 	go mod tidy
@@ -30,6 +31,8 @@ proto-lint:
 	@$(protoImage) buf lint --error-format=json
 
 proto-swagger-gen:
+	sudo find ../ -type d -exec chmod 777 {} +
+	sudo chmod 666 ./app/client/docs/swagger.yaml
 	@echo "Generating Protobuf Swagger"
 	@$(protoImage) sh ./scripts/protoc-swagger-gen.sh;
 
