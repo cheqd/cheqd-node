87564b3c51ce3380427d1a37c5258bc78ab23697
vishal <kannavish329@gmail.com>
2024-08-26 17:42:53 +0530

    chore: used feemarket NewFeeMarketDeductDecorator

--- post/post.go
+++ post/post.go
@@ -1,25 +1,25 @@
 package posthandler
 
 import (
-	cheqdante "github.com/cheqd/cheqd-node/ante"
+	// cheqdante "github.com/cheqd/cheqd-node/ante"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/x/auth/ante"
+	feemarketpost "github.com/skip-mev/feemarket/x/feemarket/post"
 )
 
 // HandlerOptions are the options required for constructing a default post handler
 type HandlerOptions struct {
-	AccountKeeper   ante.AccountKeeper
-	BankKeeper      cheqdante.BankKeeper
-	FeegrantKeeper  ante.FeegrantKeeper
-	DidKeeper       cheqdante.DidKeeper
-	ResourceKeeper  cheqdante.ResourceKeeper
-	FeeMarketKeeper cheqdante.FeeMarketKeeper
+	AccountKeeper  feemarketpost.AccountKeeper
+	BankKeeper     feemarketpost.BankKeeper
+	FeegrantKeeper feemarketpost.FeeGrantKeeper
+	// DidKeeper       cheqdante.DidKeeper
+	// ResourceKeeper  cheqdante.ResourceKeeper
+	FeeMarketKeeper feemarketpost.FeeMarketKeeper
 }
 
 // NewPostHandler returns a default post handler
 func NewPostHandler(options HandlerOptions) (sdk.PostHandler, error) {
 	postDecorators := []sdk.PostDecorator{
-		NewTaxDecorator(options.AccountKeeper, options.BankKeeper, options.FeegrantKeeper, options.DidKeeper, options.ResourceKeeper, options.FeeMarketKeeper),
+		feemarketpost.NewFeeMarketDeductDecorator(options.AccountKeeper, options.BankKeeper, options.FeegrantKeeper, options.FeeMarketKeeper),
 	}
 	return sdk.ChainPostDecorators(postDecorators...), nil
 }
