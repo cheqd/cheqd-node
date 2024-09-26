cd6e266bfd91c25e79238fc40d72840aedb0a2bc
vishal <kannavish329@gmail.com>
2024-09-26 23:04:12 +0530

    fix tests

--- tests/integration/mint_proposal_test.go
+++ tests/integration/mint_proposal_test.go
@@ -1,31 +1,28 @@
-//go:build integration
-
 package integration
 
 import (
-	"fmt"
 	"path/filepath"
 
 	cli "github.com/cheqd/cheqd-node/tests/integration/cli"
+	govtypesv1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
 
-	"github.com/cheqd/cheqd-node/tests/integration/testdata"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Upgrade - Fee parameter change proposal", func() {
+var _ = Describe("Integration - Mint coins to given address", func() {
 	It("should wait for node catching up", func() {
-		By("pinging the node status until catching up is flagged as false")
+		By("pinging the node status until catching up is flagged as false ")
 		err := cli.WaitForCaughtUp(cli.Validator0, cli.CliBinaryName, cli.VotingPeriod*6)
 		Expect(err).To(BeNil())
 	})
-	It("should submit a parameter change proposal for did module (optimistic)", func() {
+	It("should submit a mint  proposal ", func() {
 		By("passing the proposal file to the container")
 		_, err := cli.LocalnetExecCopyAbsoluteWithPermissions(filepath.Join("proposal.json"), cli.DockerHome, cli.Validator0)
 		Expect(err).To(BeNil())
 
 		By("sending a SubmitParamChangeProposal transaction from `validator0` container")
-		res, err := cli.SubmitProposal(cli.Validator0, testdata.BASE_ACCOUNT_1, cli.CliGasParams, "proposal.json")
+		res, err := cli.SubmitProposal(cli.Validator0, cli.GasParams, "proposal.json")
 		Expect(err).To(BeNil())
 		Expect(res.Code).To(BeEquivalentTo(0))
 	})
@@ -40,37 +37,33 @@ var _ = Describe("Upgrade - Fee parameter change proposal", func() {
 		Expect(err).To(BeNil())
 	})
 
-	It("should vote for the parameter change proposal from `validator1` container", func() {
+	It("should vote for the mint proposal from `validator1` container", func() {
 		By("sending a VoteProposal transaction from `validator1` container")
-		res, err := cli.VoteProposal(cli.Validator1, "1", "yes", testdata.BASE_ACCOUNT_2, cli.CliGasParams)
+		res, err := cli.VoteProposal(cli.Validator1, "1", "yes", cli.CliGasParams)
 		Expect(err).To(BeNil())
-		fmt.Println("res>>>>>>>>>>>>", res)
 		Expect(res.Code).To(BeEquivalentTo(0))
 	})
 
-	It("should vote for the parameter change proposal from `validator2` container", func() {
+	It("should vote for the mint proposal from `validator2` container", func() {
 		By("sending a VoteProposal transaction from `validator2` container")
-		res, err := cli.VoteProposal(cli.Validator2, "1", "yes", testdata.BASE_ACCOUNT_4, cli.CliGasParams)
+		res, err := cli.VoteProposal(cli.Validator2, "1", "yes", cli.CliGasParams)
 		Expect(err).To(BeNil())
-		fmt.Println("res>>>>>>>>>>>>", res)
-
 		Expect(res.Code).To(BeEquivalentTo(0))
 	})
 
-	It("should vote for the parameter change proposal from `validator3` containe r", func() {
+	It("should vote for the mint proposal from `validator3` container", func() {
 		By("sending a VoteProposal transaction from `validator3` container")
-		res, err := cli.VoteProposal(cli.Validator3, "1", "yes", testdata.BASE_ACCOUNT_5, cli.CliGasParams)
+		res, err := cli.VoteProposal(cli.Validator3, "1", "yes", cli.CliGasParams)
 		Expect(err).To(BeNil())
-		fmt.Println("res>>>>>>>>>>>>", res)
+		Expect(res.Code).To(BeEquivalentTo(0))
 
+	})
+	It("should vote for the mint proposal from `validator0` container", func() {
+		By("sending a VoteProposal transaction from `validator0` container")
+		res, err := cli.VoteProposal(cli.Validator0, "1", "yes", cli.CliGasParams)
+		Expect(err).To(BeNil())
 		Expect(res.Code).To(BeEquivalentTo(0))
 	})
-	// It("should vote for the parameter change proposal from `validator3` container", func() {
-	// 	By("sending a VoteProposal transaction from `validator3` container")
-	// 	res, err := cli.VoteProposal(cli.Validator0, "1", "yes", cli, cli.CliGasParams)
-	// 	Expect(err).To(BeNil())
-	// 	Expect(res.Code).To(BeEquivalentTo(0))
-	// })
 	It("should wait for the proposal to pass", func() {
 		By("getting the current block height")
 		currentHeight, err := cli.GetCurrentBlockHeight(cli.Validator0, cli.CliBinaryName)
@@ -80,6 +73,10 @@ var _ = Describe("Upgrade - Fee parameter change proposal", func() {
 		err = cli.WaitForChainHeight(cli.Validator0, cli.CliBinaryName, currentHeight+20, cli.VotingPeriod*3)
 		Expect(err).To(BeNil())
 	})
-
-	// It("should check the proposal status to ensure it has passed", func() {
+	It("should check the proposal status to ensure it has passed", func() {
+		By("sending a QueryProposal query from `validator0` container")
+		proposal, err := cli.QueryProposal(cli.Validator0, "1")
+		Expect(err).To(BeNil())
+		Expect(proposal.Status).To(BeEquivalentTo(govtypesv1.StatusPassed))
+	})
 })
