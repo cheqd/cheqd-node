1f9f3c5890363975902f9bbaf3ff1d0facb8d4bb
atheesh <atheesh@vitwit.com>
2024-09-26 14:47:35 +0530

    add tests for fee-abs

---
+++ tests/fee-abs/hermes/set_hermes_accounts.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+set -euox pipefail
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+function info() {
+    printf "${GREEN}[info] %s${NC}\n" "${1}"
+}
+
+function err() {
+    printf "${RED}[err] %s${NC}\n" "${1}"
+}
+
+function assert_tx_successful() {
+  RES="$1"
+
+  if [[ $(echo "${RES}" | jq --raw-output '.code') == 0 ]]
+  then
+    info "tx successful"
+  else
+    err "non zero tx return code"
+    exit 1
+  fi
+}
+
+info "Create relayer user on cheqd"
+CHEQD_RELAYER_KEY_NAME="cheqd-relayer"
+CHEQD_RELAYER_ACCOUNT=$(cheqd-noded keys add ${CHEQD_RELAYER_KEY_NAME} --keyring-backend test --output json 2>&1)
+CHEQD_RELAYER_ADDRESS=$(echo "${CHEQD_RELAYER_ACCOUNT}" | jq --raw-output '.address')
+CHEQD_RELAYER_MNEMONIC=$(echo "${CHEQD_RELAYER_ACCOUNT}" | jq --raw-output '.mnemonic')
+
+echo "${CHEQD_RELAYER_MNEMONIC}" > cheqd_relayer_mnemonic.txt
+
+info "Send some tokens to it"
+RES=$(cheqd-noded tx bank send cheqd-user "${CHEQD_RELAYER_ADDRESS}" 500000000000000000ncheq --gas-prices 50ncheq --chain-id cheqd -y --keyring-backend test)
+assert_tx_successful "${RES}"
+
+info "Create relayer user on osmosis"
+OSMOSIS_RELAYER_KEY_NAME="osmosis-relayer"
+OSMOSIS_RELAYER_ACCOUNT=$(osmosisd keys add ${OSMOSIS_RELAYER_KEY_NAME} --output json --keyring-backend test 2>&1)
+OSMOSIS_RELAYER_ADDRESS=$(echo "${OSMOSIS_RELAYER_ACCOUNT}" | jq --raw-output '.address')
+OSMOSIS_RELAYER_MNEMONIC=$(echo "${OSMOSIS_RELAYER_ACCOUNT}" | jq --raw-output '.mnemonic')
+
+echo "${OSMOSIS_RELAYER_MNEMONIC}" > osmo_relayer_mnemonic.txt
+
+info "Send some tokens to it"
+RES=$(osmosisd tx bank send osmosis-user "${OSMOSIS_RELAYER_ADDRESS}" 1000000000uosmo --fees 500uosmo --chain-id osmosis -y --keyring-backend test --output json --node http://localhost:26667)
+assert_tx_successful "${RES}"
+
+sleep 10 
+
+# Create dirs for keys
+mkdir -p ~/.hermes/keys/cheqd/keyring-test
+mkdir -p ~/.hermes/keys/osmosis/keyring-test
+
+# Copy keys
+cp cheqd_relayer_mnemonic.txt ~/.hermes/keys/cheqd/keyring-test/
+cp osmo_relayer_mnemonic.txt ~/.hermes/keys/osmosis/keyring-test/
+
+# Import keys
+hermes keys add --chain cheqd --mnemonic-file ~/.hermes/keys/cheqd/keyring-test/cheqd_relayer_mnemonic.txt --key-name cheqd-key --overwrite
+hermes keys add --chain osmosis --mnemonic-file ~/.hermes/keys/osmosis/keyring-test/osmo_relayer_mnemonic.txt --key-name osmosis-key --overwrite
+
+info "Open channel"
+
+hermes create channel --a-chain cheqd --b-chain osmosis --a-port transfer --b-port transfer --new-client-connection --yes
+# hermes create connection --a-chain cheqd --b-chain osmosis --new-client-connection --yes
+hermes create channel --a-chain osmosis --b-chain cheqd --a-port icqhost --b-port feeabs --new-client-connection --yes
+# hermes create channel --a-chain cheqd --b-chain osmosis --a-port icqhost --b-port feeabs --new-client-connection --yes
+
+hermes start
---
+++ tests/fee-abs/osmosis/send_ibc_and_create_pool.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+set -euox pipefail
+
+# Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+function info() {
+    printf "${GREEN}[info] %s${NC}\n" "${1}"
+}
+
+function err() {
+    printf "${RED}[err] %s${NC}\n" "${1}"
+}
+
+function assert_tx_successful() {
+  RES="$1"
+
+  if [[ $(echo "${RES}" | jq --raw-output '.code') == 0 ]]
+  then
+    info "tx successful"
+  else
+    err "non zero tx return code"
+    exit 1
+  fi
+}
+
+CHEQD_USER_ADDRESS=$(cheqd-noded keys show --address cheqd-user --keyring-backend test | tr -d '\r')
+OSMOSIS_USER_ADDRESS=$(osmosisd keys show --address osmosis-user --keyring-backend test | tr -d '\r')
+
+CHEQD_RELAYER_ADDRESS=$(cheqd-noded keys show --address cheqd-relayer --keyring-backend test | tr -d '\r')
+OSMOSIS_RELAYER_ADDRESS=$(osmosisd keys show --address osmosis-relayer --keyring-backend test | tr -d '\r')
+
+info "Transfer cheqd -> osmosis" # ---
+PORT="transfer"
+CHANNEL="channel-0"
+cheqd-noded tx ibc-transfer transfer $PORT $CHANNEL "$OSMOSIS_USER_ADDRESS" 10000000000ncheq --from cheqd-user --chain-id cheqd --gas-prices 50ncheq --keyring-backend test -y
+sleep 30 # Wait for relayer
+
+info "Get balances" # ---
+CHEQD_BALANCE_2=$(cheqd-noded query bank balances "$CHEQD_USER_ADDRESS" --output json)
+BALANCES=$(osmosisd query bank balances "$OSMOSIS_USER_ADDRESS" --output json)
+
+info "Denom trace" # ---
+DENOM=$(echo "$BALANCES" | jq --raw-output '.balances[0].denom')
+DENOM_CUT=$(echo "$DENOM" | cut -c 5-)
+osmosisd query ibc-transfer denom-trace "$DENOM_CUT"
+
+info "Send 100OSMO to cheqd"
+osmosisd tx ibc-transfer transfer $PORT $CHANNEL "$CHEQD_USER_ADDRESS" 100000000uosmo --from osmosis-user --chain-id osmosis --fees 500uosmo --keyring-backend test -y
+sleep 30
+
+CHEQD_BALANCE_2=$(cheqd-noded query bank balances "$CHEQD_USER_ADDRESS" --output json)
+DENOM=$(echo "$CHEQD_BALANCE_2" | jq --raw-output '.balances[0].denom')
+
+
+info "create pool"
+# create pool
+TX_HASH=$(osmosisd tx gamm create-pool --pool-file pool.json --from $OSMOSIS_USER_ADDRESS --keyring-backend test  --gas-prices 1uosmo --gas-adjustment 1 -y --chain-id osmosis --output json --gas 350000 | jq -r '.txhash')
+echo "tx hash: $TX_HASH"
+sleep 5
+
+POOL_ID=$(osmosisd q tx $TX_HASH --output json | jq -r '.logs[0].events[-10].attributes[-1].value')
+echo "pool id: $POOL_ID"
+
+info "enable fee abs"
+cheqd-noded tx gov submit-legacy-proposal param-change proposal.json --from $CHEQD_USER_ADDRESS --keyring-backend test --chain-id cheqd --yes --gas-prices 50ncheq --gas 350000
+sleep 5 
+cheqd-noded tx gov vote 1 yes --from $CHEQD_USER_ADDRESS --keyring-backend test --chain-id cheqd --yes --gas-prices 50ncheq
+sleep 5 
+
+info "add host zone config"
+cheqd-noded tx gov submit-legacy-proposal add-hostzone-config host_zone.json --from $CHEQD_USER_ADDRESS --keyring-backend test --chain-id cheqd --yes --gas-prices 50ncheq --gas 350000
+sleep 5
+cheqd-noded tx gov vote 2 yes --from $CHEQD_USER_ADDRESS --keyring-backend test --chain-id cheqd --yes --gas-prices 50ncheq
