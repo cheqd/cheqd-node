48b19ee4a424a625fe63c99d943cc238def40283
vishal <kannavish329@gmail.com>
2024-09-30 12:36:32 +0530

    testing

--- tests/integration/mint_proposal_test.go
+++ tests/integration/mint_proposal_test.go
@@ -1,7 +1,7 @@
 package integration
 
 import (
-	"path/filepath"
+	"time"
 
 	cli "github.com/cheqd/cheqd-node/tests/integration/cli"
 	govtypesv1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
@@ -11,30 +11,11 @@ import (
 )
 
 var _ = Describe("Integration - Mint coins to given address", func() {
-	It("should wait for node catching up", func() {
-		By("pinging the node status until catching up is flagged as false ")
-		err := cli.WaitForCaughtUp(cli.Validator0, cli.CliBinaryName, cli.VotingPeriod*6)
-		Expect(err).To(BeNil())
-	})
 	It("should submit a mint  proposal ", func() {
-		By("passing the proposal file to the container")
-		_, err := cli.LocalnetExecCopyAbsoluteWithPermissions(filepath.Join("proposal.json"), cli.DockerHome, cli.Validator0)
-		Expect(err).To(BeNil())
-
 		By("sending a SubmitParamChangeProposal transaction from `validator0` container")
 		res, err := cli.SubmitProposal(cli.Validator0, cli.GasParams, "proposal.json")
 		Expect(err).To(BeNil())
 		Expect(res.Code).To(BeEquivalentTo(0))
-	})
-
-	It("should wait for the proposal submission to be included in a block", func() {
-		By("getting the current block height")
-		currentHeight, err := cli.GetCurrentBlockHeight(cli.Validator0, cli.CliBinaryName)
-		Expect(err).To(BeNil())
-
-		By("waiting for the proposal to be included in a block>>")
-		err = cli.WaitForChainHeight(cli.Validator0, cli.CliBinaryName, currentHeight+10, cli.VotingPeriod*3)
-		Expect(err).To(BeNil())
 	})
 
 	It("should vote for the mint proposal from `validator1` container", func() {
@@ -63,15 +44,10 @@ var _ = Describe("Integration - Mint coins to given address", func() {
 		Expect(err).To(BeNil())
 		Expect(res.Code).To(BeEquivalentTo(0))
 	})
-	It("should wait for the proposal to pass", func() {
-		By("getting the current block height")
-		currentHeight, err := cli.GetCurrentBlockHeight(cli.Validator0, cli.CliBinaryName)
-		Expect(err).To(BeNil())
-
-		By("waiting for the proposal to pass")
-		err = cli.WaitForChainHeight(cli.Validator0, cli.CliBinaryName, currentHeight+20, cli.VotingPeriod*3)
-		Expect(err).To(BeNil())
+	It("wait until the voting period is done", func() {
+		time.Sleep(time.Second * 40)
 	})
+
 	It("should check the proposal status to ensure it has passed", func() {
 		By("sending a QueryProposal query from `validator0` container")
 		proposal, err := cli.QueryProposal(cli.Validator0, "1")
