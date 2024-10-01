b373a07d723cd6b7e0aaeaa6f172158cf0d21533
atheesh <atheesh@vitwit.com>
2024-10-01 16:13:12 +0530

    keys tests

--- tests/integration/mint_proposal_test.go
+++ tests/integration/mint_proposal_test.go
@@ -19,6 +19,33 @@ var _ = Describe("Integration - Mint coins to given address", func() {
 		err := cli.WaitForCaughtUp(cli.Validator0, cli.CliBinaryName, cli.VotingPeriod*6)
 		Expect(err).To(BeNil())
 	})
+
+	It("fetch the keys", func() {
+		By("sending a QueryKeys query from `validator0` container")
+		keys, err := cli.QueryKeys(cli.Validator0)
+		fmt.Printf("keys>>>>>>>>>>>>>>>>>>>>>: %v\n", keys)
+		Expect(err).To(BeNil())
+	})
+
+	It("fetch the keys", func() {
+		By("sending a QueryKeys query from `validator0` container")
+		keys, err := cli.QueryKeys(cli.Validator1)
+		fmt.Printf("keys>>>>>>>>>>>>>>>>>>>>>: %v\n", keys)
+		Expect(err).To(BeNil())
+	})
+	It("fetch the keys", func() {
+		By("sending a QueryKeys query from `validator0` container")
+		keys, err := cli.QueryKeys(cli.Validator2)
+		fmt.Printf("keys>>>>>>>>>>>>>>>>>>>>>: %v\n", keys)
+		Expect(err).To(BeNil())
+	})
+	It("fetch the keys", func() {
+		By("sending a QueryKeys query from `validator0` container")
+		keys, err := cli.QueryKeys(cli.Validator3)
+		fmt.Printf("keys>>>>>>>>>>>>>>>>>>>>>: %v\n", keys)
+		Expect(err).To(BeNil())
+	})
+
 	It("should submit a mint  proposal ", func() {
 		By("passing the proposal file to the container")
 		_, err := cli.LocalnetExecCopyAbsoluteWithPermissions(filepath.Join("proposal.json"), cli.DockerHome, cli.Validator0)
