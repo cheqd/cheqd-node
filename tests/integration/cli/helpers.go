9c4759f597fc3611661603ce9b831098b9b77483
vishal <kannavish329@gmail.com>
2024-10-01 10:45:10 +0530

    testing

--- tests/integration/cli/helpers.go
+++ tests/integration/cli/helpers.go
@@ -77,7 +77,7 @@ func GetNodeStatus(container string, binary string) (NodeStatus, error) {
 	if err != nil {
 		return NodeStatus{}, err
 	}
-	fmt.Println("string>>>>>>>>>>>>>>>>>>>>>", string(out))
+	fmt.Println("string>>>>>>>>>>>>>>>>>>>>>", out)
 	var result NodeStatus
 	err = json.Unmarshal([]byte(out), &result)
 	if err != nil {
