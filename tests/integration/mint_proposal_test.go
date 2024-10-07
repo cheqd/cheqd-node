687fb120cc5cfa29e2af3f483cb5c8c5124849cc
vishal <kannavish329@gmail.com>
2024-10-07 12:20:43 +0530

    testing

--- tests/integration/mint_proposal_test.go
+++ tests/integration/mint_proposal_test.go
@@ -79,27 +79,27 @@ var _ = Describe("Integration - Mint coins to given address", func() {
 
 	It("should vote for the mint proposal from `validator1` container", func() {
 		By("sending a VoteProposal transaction from `validator1` container")
-		res, err := cli.VoteProposalTx(cli.Validator1, "1", "yes", cli.CliGasParams)
+		res, err := cli.VoteProposalTx(cli.Operator0, "1", "yes", cli.CliGasParams)
 		Expect(err).To(BeNil())
 		Expect(res.Code).To(BeEquivalentTo(0))
 	})
 
 	It("should vote for the mint proposal from `validator2` container", func() {
 		By("sending a VoteProposal transaction from `validator2` container")
-		res, err := cli.VoteProposalTx(cli.Validator2, "1", "yes", cli.CliGasParams)
+		res, err := cli.VoteProposalTx(cli.Operator1, "1", "yes", cli.CliGasParams)
 		Expect(err).To(BeNil())
 		Expect(res.Code).To(BeEquivalentTo(0))
 	})
 
 	It("should vote for the mint proposal from `validator3` container", func() {
 		By("sending a VoteProposal transaction from `validator3` container")
-		res, err := cli.VoteProposalTx(cli.Validator3, "1", "yes", cli.CliGasParams)
+		res, err := cli.VoteProposalTx(cli.Operator3, "1", "yes", cli.CliGasParams)
 		Expect(err).To(BeNil())
 		Expect(res.Code).To(BeEquivalentTo(0))
 	})
 	It("should vote for the mint proposal from `validator0` container", func() {
 		By("sending a VoteProposal transaction from `validator0` container")
-		res, err := cli.VoteProposalTx(cli.Validator0, "1", "yes", cli.CliGasParams)
+		res, err := cli.VoteProposalTx(cli.Operator2, "1", "yes", cli.CliGasParams)
 		Expect(err).To(BeNil())
 		Expect(res.Code).To(BeEquivalentTo(0))
 	})
