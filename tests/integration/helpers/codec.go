484f699521bbe28624db8b4abd708e781db0cdcf
vishal <kannavish329@gmail.com>
2024-10-10 19:00:20 +0530

    testing

--- tests/integration/helpers/codec.go
+++ tests/integration/helpers/codec.go
@@ -8,6 +8,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec/types"
 	"github.com/cosmos/cosmos-sdk/std"
 	govtypesv1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
+	govtypesv1beta1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1beta1"
 )
 
 var (
@@ -22,6 +23,7 @@ func init() {
 	resourcetypes.RegisterInterfaces(encodingConfig.InterfaceRegistry)
 	didtypes.RegisterInterfaces(encodingConfig.InterfaceRegistry)
 	govtypesv1.RegisterInterfaces(encodingConfig.InterfaceRegistry)
+	govtypesv1beta1.RegisterInterfaces(encodingConfig.InterfaceRegistry)
 	Codec = encodingConfig.Codec
 	Registry = encodingConfig.InterfaceRegistry
 }
