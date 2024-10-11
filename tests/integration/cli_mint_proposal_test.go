7fc78c55c5f2114980b23e0ef925d4ff34eae50e
vishal <kannavish329@gmail.com>
2024-10-11 09:50:44 +0530

    chore: added balance check after minting

--- tests/integration/cli_mint_proposal_test.go
+++ tests/integration/cli_mint_proposal_test.go
@@ -3,10 +3,11 @@
 package integration
 
 import (
-	"fmt"
 	"path/filepath"
 
 	cli "github.com/cheqd/cheqd-node/tests/integration/cli"
+	didtypes "github.com/cheqd/cheqd-node/x/did/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	govtypesv1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -14,7 +15,15 @@ import (
 )
 
 var _ = Describe("Integration - Mint coins to given address", func() {
+	var balanceBefore sdk.Coin
+	var err error
 	var Proposal_id string
+
+	It("check the balances", func() {
+		// Query balances
+		balanceBefore, err = cli.QueryBalance("cheqd1lhl9g4rgldadgtz7v6rt50u45uhhj8hhv8d8uf", didtypes.BaseMinimalDenom)
+		Expect(err).To(BeNil())
+	})
 	It("should wait for node catching up", func() {
 		By("pinging the node status until catching up is flagged as false ")
 		err := cli.WaitForCaughtUp(cli.Validator0, cli.CliBinaryName, cli.VotingPeriod*6)
@@ -50,7 +59,6 @@ var _ = Describe("Integration - Mint coins to given address", func() {
 		By("sending a VoteProposal transaction from `validator1` container")
 		res, err := cli.VoteProposalTx(cli.Operator1, Proposal_id, "yes", cli.CliGasParams)
 		Expect(err).To(BeNil())
-		fmt.Println("proposal_id>>>>>>>>>>Here>>>>>", Proposal_id)
 		Expect(res.Code).To(BeEquivalentTo(0))
 	})
 
@@ -83,5 +91,16 @@ var _ = Describe("Integration - Mint coins to given address", func() {
 		proposal, err := cli.QueryProposal(cli.Validator0, Proposal_id)
 		Expect(err).To(BeNil())
 		Expect(proposal.Status).To(BeEquivalentTo(govtypesv1.StatusPassed))
+	})
+	It("should have the correct balance after minting", func() {
+		By("querying the balance of the given address after minting")
+		balanceAfter, err := cli.QueryBalance("cheqd1lhl9g4rgldadgtz7v6rt50u45uhhj8hhv8d8uf", didtypes.BaseMinimalDenom)
+		Expect(err).To(BeNil())
+
+		// Calculate expected balance by adding 9000 to the balance before
+		expectedBalance := balanceBefore.Add(sdk.NewCoin(didtypes.BaseMinimalDenom, sdk.NewInt(9000)))
+
+		// Check if the balance after minting matches the expected balance
+		Expect(balanceAfter.Amount.Int64()).To(Equal(expectedBalance.Amount.Int64()))
 	})
 })
