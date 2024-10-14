11b9ad3909e1dc30be11fb186282bf38dff01ca7
Tasos Derisiotis <50984242+Eengineer1@users.noreply.github.com>
2024-10-14 17:00:36 +0300

    Aligned v3 with v2 + removed redundancy

--- tests/upgrade/integration/v3/post_test.go
+++ tests/upgrade/integration/v3/post_test.go
@@ -6,12 +6,12 @@ import (
 	"fmt"
 	"path/filepath"
 
+	integrationcli "github.com/cheqd/cheqd-node/tests/integration/cli"
 	clihelpers "github.com/cheqd/cheqd-node/tests/integration/helpers"
 	cli "github.com/cheqd/cheqd-node/tests/upgrade/integration/v3/cli"
 	didtypes "github.com/cheqd/cheqd-node/x/did/types"
 	resourcetypes "github.com/cheqd/cheqd-node/x/resource/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	govtypesv1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -143,82 +143,28 @@ var _ = Describe("Upgrade - Post", func() {
 				Expect(res.Resource.Metadata.NextVersionId).To(Equal(ResourceCreateRecord.Metadata.NextVersionId))
 			}
 		})
-	})
-	var Proposal_id string
-	Context("After a software upgrade execution has concluded", func() {
-		It("should wait for node catching up", func() {
-			By("pinging the node status until catching up is flagged as false")
-			err := cli.WaitForCaughtUp(cli.Validator0, cli.CliBinaryName, cli.VotingPeriod*6)
-			Expect(err).To(BeNil())
-		})
-		It("should submit a parameter change proposal for did module (optimistic)", func() {
-			By("passing the proposal file to the container")
-			_, err := cli.LocalnetExecCopyAbsoluteWithPermissions(filepath.Join("proposal.json"), cli.DockerHome, cli.Validator0)
-			Expect(err).To(BeNil())
-			By("sending a SubmitParamChangeProposal transaction from `validator0` container")
-			res, err := cli.SubmitProposal(cli.Validator0, "proposal.json")
-			Expect(err).To(BeNil())
-			Expect(res.Code).To(BeEquivalentTo(0))
-			res, err = cli.QueryTxn(cli.Validator0, res.TxHash)
-			Expect(err).To(BeNil())
 
-			proposal_id, err := cli.GetProposalID(res.RawLog)
-			Proposal_id = proposal_id
-			Expect(err).To(BeNil())
-		})
-		It("should vote for the parameter change proposal from `validator1` container", func() {
-			By("sending a VoteProposal transaction from `validator1` container")
-			res, err := cli.VoteProposal(cli.Validator1, Proposal_id, "yes")
-			Expect(err).To(BeNil())
-			Expect(res.Code).To(BeEquivalentTo(0))
-		})
-
-		It("should vote for the parameter change proposal from `validator2` container", func() {
-			By("sending a VoteProposal transaction from `validator2` container")
-			res, err := cli.VoteProposal(cli.Validator2, Proposal_id, "yes")
-			Expect(err).To(BeNil())
-			Expect(res.Code).To(BeEquivalentTo(0))
-		})
-
-		It("should vote for the parameter change proposal from `validator3` container", func() {
-			By("sending a VoteProposal transaction from `validator3` container")
-			res, err := cli.VoteProposal(cli.Validator3, Proposal_id, "yes")
-			Expect(err).To(BeNil())
-			Expect(res.Code).To(BeEquivalentTo(0))
-		})
-		It("should vote for the parameter change proposal from `validator3` container", func() {
-			By("sending a VoteProposal transaction from `validator3` container")
-			res, err := cli.VoteProposal(cli.Validator0, Proposal_id, "yes")
-			Expect(err).To(BeNil())
-			Expect(res.Code).To(BeEquivalentTo(0))
-		})
-		It("should wait for the proposal to pass", func() {
-			By("getting the current block height")
-			currentHeight, err := cli.GetCurrentBlockHeight(cli.Validator0, cli.CliBinaryName)
-			Expect(err).To(BeNil())
-
-			By("waiting for the proposal to pass")
-			err = cli.WaitForChainHeight(cli.Validator0, cli.CliBinaryName, currentHeight+20, cli.VotingPeriod*100)
-			Expect(err).To(BeNil())
-		})
-
-		It("should check the proposal status to ensure it has passed", func() {
-			By("sending a QueryProposal query from `validator0` container")
-			proposal, err := cli.QueryProposal(cli.Validator0, Proposal_id)
-			Expect(err).To(BeNil())
-			Expect(proposal.Status).To(BeEquivalentTo(govtypesv1.StatusPassed))
-		})
-
-		It("should wait for node catching up", func() {
-			By("pinging the node status until catching up is flagged as false")
-			err := cli.WaitForCaughtUp(cli.Validator0, cli.CliBinaryName, cli.VotingPeriod*6)
-			Expect(err).To(BeNil())
-		})
 		It("should burn the coins from the given address (here container/validator)", func() {
-			coins := sdk.NewCoins(sdk.Coin{Denom: "ncheq", Amount: sdk.NewInt(1000)})
+			By("querying the account balance")
+			balanceBefore, err := integrationcli.QueryBalance(integrationcli.Operator0, didtypes.BaseMinimalDenom)
+			Expect(err).To(BeNil())
+			Expect(balanceBefore.Denom).To(Equal(didtypes.BaseMinimalDenom))
+
+			By("burning the coins")
+			coins := sdk.NewCoins(sdk.Coin{Denom: didtypes.BaseMinimalDenom, Amount: sdk.NewInt(1000)})
 			res, err := cli.BurnMsg(cli.Validator0, coins.String())
 			Expect(err).To(BeNil())
 			Expect(res.Code).To(BeEquivalentTo(0))
+
+			By("querying the account balance again")
+			balanceAfter, err := integrationcli.QueryBalance(integrationcli.Operator0, didtypes.BaseMinimalDenom)
+			Expect(err).To(BeNil())
+			Expect(balanceAfter.Denom).To(Equal(didtypes.BaseMinimalDenom))
+
+			By("checking if the balance has been reduced by the expected amount")
+			expectedBalance := balanceBefore.Sub(sdk.NewCoin(didtypes.BaseMinimalDenom, sdk.NewInt(1000)))
+			diff := balanceAfter.Amount.Sub(expectedBalance.Amount)
+			Expect(diff).To(Equal(sdk.NewInt(1000)))
 		})
 	})
 })
--- tests/upgrade/integration/v3/pre_test.go
+++ tests/upgrade/integration/v3/pre_test.go
@@ -4,19 +4,99 @@ package integration
 
 import (
 	"fmt"
+	"path/filepath"
 
+	clihelpers "github.com/cheqd/cheqd-node/tests/integration/helpers"
+	cliv2 "github.com/cheqd/cheqd-node/tests/upgrade/integration/v2/cli"
 	cli "github.com/cheqd/cheqd-node/tests/upgrade/integration/v3/cli"
+	didcli "github.com/cheqd/cheqd-node/x/did/client/cli"
+	didtypes "github.com/cheqd/cheqd-node/x/did/types"
+	resourcetypes "github.com/cheqd/cheqd-node/x/resource/types"
 	govtypesv1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Upgrade - Pre", func() {
+	var didFeeParams didtypes.FeeParams
+	var resourceFeeParams resourcetypes.FeeParams
+
+	BeforeEach(func() {
+		// query fee params - case: did
+		res, err := cli.QueryParams(cli.Validator0, didtypes.ModuleName, string(didtypes.ParamStoreKeyFeeParams))
+		Expect(err).To(BeNil())
+		err = clihelpers.Codec.UnmarshalJSON([]byte(res.Value), &didFeeParams)
+		Expect(err).To(BeNil())
+
+		// query fee params - case: resource
+		res, err = cli.QueryParams(cli.Validator0, resourcetypes.ModuleName, string(resourcetypes.ParamStoreKeyFeeParams))
+		Expect(err).To(BeNil())
+		err = clihelpers.Codec.UnmarshalJSON([]byte(res.Value), &resourceFeeParams)
+		Expect(err).To(BeNil())
+	})
+
 	Context("Before a software upgrade execution is initiated", func() {
 		It("should wait for chain to bootstrap", func() {
 			By("pinging the node status until the voting end height is reached")
 			err := cli.WaitForChainHeight(cli.Validator0, cli.CliBinaryName, cli.BootstrapHeight, cli.BootstrapPeriod+20)
 			Expect(err).To(BeNil())
+		})
+
+		It("should load and run existing diddoc payloads - case: create", func() {
+			By("matching the glob pattern for existing diddoc payloads")
+			ExistingDidDocCreatePayloads, err := RelGlob(GeneratedJSONDir, "pre", "create - diddoc", "*.json")
+			Expect(err).To(BeNil())
+
+			for _, payload := range ExistingDidDocCreatePayloads {
+				var DidDocCreatePayload didcli.DIDDocument
+				var DidDocCreateSignInput []didcli.SignInput
+
+				testCase := GetCaseName(payload)
+				By("Running: " + testCase)
+				fmt.Println("Running: " + testCase)
+
+				By("reading ")
+				DidDocCreateSignInput, err = Loader(payload, &DidDocCreatePayload)
+				Expect(err).To(BeNil())
+
+				res, err := cliv2.CreateDid(DidDocCreatePayload, DidDocCreateSignInput, cli.Validator0, "", didFeeParams.CreateDid.String())
+				Expect(err).To(BeNil())
+				Expect(res.Code).To(BeEquivalentTo(0))
+			}
+		})
+
+		It("should load and run existing resource payloads - case: create", func() {
+			By("matching the glob pattern for existing resource payloads")
+			ExistingResourceCreatePayloads, err := RelGlob(GeneratedJSONDir, "pre", "create - resource", "*.json")
+			Expect(err).To(BeNil())
+
+			for _, payload := range ExistingResourceCreatePayloads {
+				var ResourceCreatePayload resourcetypes.MsgCreateResourcePayload
+				var ResourceCreateSignInput []didcli.SignInput
+
+				testCase := GetCaseName(payload)
+				By("Running: " + testCase)
+				fmt.Println("Running: " + testCase)
+
+				ResourceCreateSignInput, err = Loader(payload, &ResourceCreatePayload)
+				Expect(err).To(BeNil())
+
+				By("copying the existing resource file to the container")
+				ResourceFile, err := CreateTestJSON(GinkgoT().TempDir(), ResourceCreatePayload.Data)
+				Expect(err).To(BeNil())
+				_, err = cli.LocalnetExecCopyAbsoluteWithPermissions(ResourceFile, cli.DockerHome, cli.Validator0)
+				Expect(err).To(BeNil())
+
+				res, err := cliv2.CreateResource(
+					ResourceCreatePayload,
+					filepath.Base(ResourceFile),
+					ResourceCreateSignInput,
+					cli.Validator0,
+					resourceFeeParams.Json.String(),
+				)
+				Expect(err).To(BeNil())
+				Expect(res.Code).To(BeEquivalentTo(0))
+			}
 		})
 
 		var UpgradeHeight int64
@@ -85,6 +165,7 @@ var _ = Describe("Upgrade - Pre", func() {
 			err = cli.WaitForChainHeight(cli.Validator0, cli.CliBinaryName, currentHeight+3, cli.VotingPeriod*6)
 			Expect(err).To(BeNil())
 		})
+
 		It("should vote for the software upgrade proposal from `validator1` container", func() {
 			By("sending a VoteProposal transaction from `validator1` container")
 			res, err := cli.VoteProposal(cli.Validator1, "1", "yes")
@@ -93,6 +174,7 @@ var _ = Describe("Upgrade - Pre", func() {
 
 			Expect(res.Code).To(BeEquivalentTo(0))
 		})
+
 		It("should wait for a certain number of blocks to be produced", func() {
 			By("fetching the current chain height")
 			currentHeight, err := cli.GetCurrentBlockHeight(cli.Validator0, cli.CliBinaryName)
@@ -120,14 +202,6 @@ var _ = Describe("Upgrade - Pre", func() {
 			err = cli.WaitForChainHeight(cli.Validator0, cli.CliBinaryName, currentHeight+3, cli.VotingPeriod*6)
 			Expect(err).To(BeNil())
 		})
-		// It("should vote for the software upgrade proposal from `validator3` container", func() {
-		// 	By("sending a VoteProposal transaction from `validator3` container")
-		// 	res, err := cli.VoteProposal(cli.Validator3, "1", "yes")
-		// 	Expect(err).To(BeNil())
-		// 	fmt.Println("response>>>>>>>>>>>>>>", res)
-
-		// 	Expect(res.Code).To(BeEquivalentTo(0))
-		// })
 
 		It("should wait for the voting end height to be reached", func() {
 			By("pinging the node status until the voting end height is reached")
