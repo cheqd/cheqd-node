4d05a260527cee9abdf7d2f5acc3cee4c62fe699
Tasos Derisiotis <50984242+Eengineer1@users.noreply.github.com>
2025-01-13 18:22:41 +0200

    fix: Updated JSON-escaped assertion method integration test suite

--- tests/integration/cli_diddoc_test.go
+++ tests/integration/cli_diddoc_test.go
@@ -465,7 +465,7 @@ var _ = Describe("cheqd cli - positive did", func() {
 
 		assertionMethodJSONEscaped2 := func() string {
 			b, _ := json.Marshal(types.AssertionMethodJSONUnescaped{
-				Id:                 keyID,
+				Id:                 did + "#key2",
 				Type:               "Ed25519VerificationKey2020",
 				Controller:         did,
 				PublicKeyMultibase: &publicKeyMultibase, // arbitrarily chosen, loosely validated
