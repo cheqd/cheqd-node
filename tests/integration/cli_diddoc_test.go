b59c262b894bc87f2ccdf82514360ec9a8b0eb33
Tasos Derisiotis <50984242+Eengineer1@users.noreply.github.com>
2025-01-16 19:05:01 +0200

    Added external controller integration test case

--- tests/integration/cli_diddoc_test.go
+++ tests/integration/cli_diddoc_test.go
@@ -433,7 +433,7 @@ var _ = Describe("cheqd cli - positive did", func() {
 				Type:            "Ed25519VerificationKey2018",
 				Controller:      did,
 				PublicKeyBase58: &publicKeyBase58, // arbitrarily chosen, loosely validated
-				Metadata: 	  &types.AssertionMethodJSONUnescapedMetadata{
+				Metadata: &types.AssertionMethodJSONUnescapedMetadata{
 					ParticipantId: &participantId,
 					ParamsRef:     &paramsRef,
 					CurveType:     &curveType,
@@ -568,7 +568,7 @@ var _ = Describe("cheqd cli - positive did", func() {
 		publicKeyMultibase2 := testsetup.GenerateEd25519VerificationKey2020VerificationMaterial(publicKey2)
 
 		payload := didcli.DIDDocument{
-			ID: did,
+			ID:         did,
 			Controller: []string{},
 			VerificationMethod: []didcli.VerificationMethod{
 				map[string]any{
@@ -635,5 +635,99 @@ var _ = Describe("cheqd cli - positive did", func() {
 
 		// Check that DIDDoc is not deactivated
 		Expect(resp.Value.Metadata.Deactivated).To(BeFalse())
+	})
+
+	It("can create diddoc with controller being another diddoc (Ed25519VerificationKey2020)", func() {
+		AddReportEntry("Integration", fmt.Sprintf("%sPositive: %s", cli.Green, "can create diddoc with controller being another diddoc (Ed25519VerificationKey2020)"))
+		// Create a new DID Doc
+		did1 := "did:cheqd:" + network.DidNamespace + ":" + uuid.NewString()
+		keyID1 := did1 + "#key1"
+
+		publicKey1, privateKey1, err := ed25519.GenerateKey(nil)
+		Expect(err).To(BeNil())
+
+		publicKeyMultibase1 := testsetup.GenerateEd25519VerificationKey2020VerificationMaterial(publicKey1)
+
+		did2 := "did:cheqd:" + network.DidNamespace + ":" + uuid.NewString()
+		keyID2 := did2 + "#key1"
+
+		publicKey2, privateKey2, err := ed25519.GenerateKey(nil)
+		Expect(err).To(BeNil())
+
+		publicKeyMultibase2 := testsetup.GenerateEd25519VerificationKey2020VerificationMaterial(publicKey2)
+
+		payload1 := didcli.DIDDocument{
+			ID: did1,
+			VerificationMethod: []didcli.VerificationMethod{
+				map[string]any{
+					"id":                 keyID1,
+					"type":               "Ed25519VerificationKey2020",
+					"controller":         did1,
+					"publicKeyMultibase": publicKeyMultibase1,
+				},
+			},
+			Authentication: []string{keyID1},
+		}
+
+		signInputs1 := []didcli.SignInput{
+			{
+				VerificationMethodID: keyID1,
+				PrivKey:              privateKey1,
+			},
+		}
+
+		versionID1 := uuid.NewString()
+
+		res1, err := cli.CreateDidDoc(tmpDir, payload1, signInputs1, versionID1, testdata.BASE_ACCOUNT_1, helpers.GenerateFees(feeParams.CreateDid.String()))
+		Expect(err).To(BeNil())
+		Expect(res1.Code).To(BeEquivalentTo(0))
+
+		payload2 := didcli.DIDDocument{
+			ID:         did2,
+			Controller: []string{did1},
+			VerificationMethod: []didcli.VerificationMethod{
+				map[string]any{
+					"id":                 keyID2,
+					"type":               "Ed25519VerificationKey2020",
+					"controller":         did2,
+					"publicKeyMultibase": publicKeyMultibase2,
+				},
+			},
+			Authentication: []string{keyID2},
+		}
+
+		signInputs2 := []didcli.SignInput{
+			{
+				VerificationMethodID: keyID1,
+				PrivKey:              privateKey1,
+			},
+			{
+				VerificationMethodID: keyID2,
+				PrivKey:              privateKey2,
+			},
+		}
+
+		versionID2 := uuid.NewString()
+
+		res2, err := cli.CreateDidDoc(tmpDir, payload2, signInputs2, versionID2, testdata.BASE_ACCOUNT_1, helpers.GenerateFees(feeParams.CreateDid.String()))
+		Expect(err).To(BeNil())
+		Expect(res2.Code).To(BeEquivalentTo(0))
+
+		AddReportEntry("Integration", fmt.Sprintf("%sPositive: %s", cli.Green, "can query diddoc with controller being another diddoc (Ed25519VerificationKey2020)"))
+		// Query the DID Doc
+		resp, err := cli.QueryDidDoc(did2)
+		Expect(err).To(BeNil())
+
+		didDoc := resp.Value.DidDoc
+		Expect(didDoc.Id).To(BeEquivalentTo(did2))
+		Expect(didDoc.Controller).To(HaveLen(1))
+		Expect(didDoc.Controller[0]).To(BeEquivalentTo(did1))
+		Expect(didDoc.Authentication).To(HaveLen(1))
+		Expect(didDoc.Authentication[0]).To(BeEquivalentTo(keyID2))
+		Expect(didDoc.VerificationMethod).To(HaveLen(1))
+		Expect(didDoc.VerificationMethod[0].Id).To(BeEquivalentTo(keyID2))
+		Expect(didDoc.VerificationMethod[0].VerificationMethodType).To(BeEquivalentTo("Ed25519VerificationKey2020"))
+		Expect(didDoc.VerificationMethod[0].Controller).To(BeEquivalentTo(did2))
+		Expect(didDoc.VerificationMethod[0].VerificationMaterial).To(BeEquivalentTo(publicKeyMultibase2))
 	})
 })
