e0bf7f059ad3a4b97b8e0653d1edee52bcdb3fda
PrathyushaLakkireddy <prathyusha@vitwit.com>
2025-04-21 14:46:17 +0530

    fix autocli

--- x/resource/autocli.go
+++ x/resource/autocli.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/version"
 )
+a
 
 // AutoCLIOptions implements the autocli.HasAutoCLIConfig interface.
 func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
@@ -17,31 +18,31 @@ func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 			RpcCommandOptions: []*autocliv1.RpcCommandOptions{
 				{
 					RpcMethod: "Resource",
-					Use:       "resource [collection-id] [id]",
-					Short:     "Fetch data/payload for a specific resource (with metadata)",
-					Example:   fmt.Sprintf("%s query resource resource c82f2b02-bdab-4dd7-b833-3e143745d612 93f2573c-eca9-4098-96cb-a1ec676a29ed", version.AppName),
+					Use:       "specific-resource [collection-id] [resource-id]",
+					Short:     "Query a specific resource",
+					Example:   fmt.Sprintf("%s query resource pecific-resource c82f2b02-bdab-4dd7-b833-3e143745d612 wGHEXrZvJxR8vw5P3UWH1j", version.AppName),
 					PositionalArgs: []*autocliv1.PositionalArgDescriptor{
 						{ProtoField: "collection_id"},
-						{ProtoField: "id"},
+						{ProtoField: "resource-id"},
+					},
+				},
+				{
+					RpcMethod: "CollectionMetadata",
+					Use:       "collection-metadata [collection-id]",
+					Short:     "Query metadata for an entire Collection",
+					Example:   fmt.Sprintf("%s query resource collection-metadata c82f2b02-bdab-4dd7-b833-3e143745d612 wGHEXrZvJxR8vw5P3UWH1j", version.AppName),
+					PositionalArgs: []*autocliv1.PositionalArgDescriptor{
+						{ProtoField: "collection_id"},
 					},
 				},
 				{
 					RpcMethod: "ResourceMetadata",
-					Use:       "resource-metadata [collection-id] [id]",
-					Short:     "Fetch only metadata for a specific resource",
-					Example:   fmt.Sprintf("%s query resource resource-metadata c82f2b02-bdab-4dd7-b833-3e143745d612 93f2573c-eca9-4098-96cb-a1ec676a29ed", version.AppName),
+					Use:       "metadata [collection-id] [resource-id]",
+					Short:     "Query metadata for a specific resource",
+					Example:   fmt.Sprintf("%s query resource metadata c82f2b02-bdab-4dd7-b833-3e143745d612 wGHEXrZvJxR8vw5P3UWH1j", version.AppName),
 					PositionalArgs: []*autocliv1.PositionalArgDescriptor{
 						{ProtoField: "collection_id"},
-						{ProtoField: "id"},
-					},
-				},
-				{
-					RpcMethod: "CollectionResources",
-					Use:       "collection-resources [collection-id]",
-					Short:     "Fetch metadata for all resources in a collection",
-					Example:   fmt.Sprintf("%s query resource collection-resources c82f2b02-bdab-4dd7-b833-3e143745d612", version.AppName),
-					PositionalArgs: []*autocliv1.PositionalArgDescriptor{
-						{ProtoField: "collection_id"},
+						{ProtoField: "resource-id"},
 					},
 				},
 			},
@@ -51,46 +52,10 @@ func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 			Service: resourcev2.Msg_ServiceDesc.ServiceName,
 			RpcCommandOptions: []*autocliv1.RpcCommandOptions{
 				{
-					RpcMethod: "CreateResource",
-					Use:       "create-resource",
-					Short:     "Create a new resource",
-					Long:      fmt.Sprintf("Create a new resource with the specified parameters. Resources are associated with DID document collections and have various metadata fields. Example:\n%s tx resource create-resource --collection-id=c82f2b02-bdab-4dd7-b833-3e143745d612 --id=93f2573c-eca9-4098-96cb-a1ec676a29ed --name=\"My Resource\" --resource-type=\"AnonCredsSchema\" --from mykey", version.AppName),
-					FlagOptions: map[string]*autocliv1.FlagOptions{
-						"payload.collection_id": {
-							Name:  "collection-id",
-							Usage: "Identifier of the DID Document collection the resource belongs to",
-						},
-						"payload.id": {
-							Name:  "id",
-							Usage: "Unique id of the resource (UUID format)",
-						},
-						"payload.name": {
-							Name:  "name",
-							Usage: "Human-readable name of the resource",
-						},
-						"payload.version": {
-							Name:  "version",
-							Usage: "Version of the resource (e.g., 1.0.0)",
-						},
-						"payload.resource_type": {
-							Name:  "resource-type",
-							Usage: "Type of the resource (e.g., AnonCredsSchema, StatusList2021)",
-						},
-						"payload.data": {
-							Name:  "data",
-							Usage: "Base64-encoded data representing the actual content to store",
-						},
-					},
-				},
-				{
 					RpcMethod: "UpdateParams",
-					Use:       "update-params",
-					Short:     "Update the module parameters",
-					Long:      "Update the resource module parameters. Can only be executed by the module authority (governance).",
-					Skip:      true, // Skip because this is authority gated
+					Skip:      true, // skipped because authority gated
 				},
 			},
-			EnhanceCustomCommand: true,
 		},
 	}
 }
