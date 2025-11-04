2643a01983f9e74922ead9a384a0f9f10ecafa2a
Tasos Derisiotis <tasosderiziotis@gmail.com>
2025-11-04 20:38:33 +0200

    Raise oracle jitter tolerance (covering CHEQ:USDC 1:1 ratio)

--- tests/integration/helpers/const.go
+++ tests/integration/helpers/const.go
@@ -2,7 +2,7 @@ package helpers
 
 const (
 	// OracleJitterTolerance is the tolerance for oracle price fluctuations
-	OracleJitterTolerance = int64(5e3) // 5000 ncheq
+	OracleJitterTolerance = int64(5e8) // 500_000_000 ncheq
 	// BalanceJitterTolerance is the tolerance for account balance fluctuations; this is higher than OracleJitterTolerance to account for multiple txs or ICQ twap updates (generally higher slippage - ICQ is less predictable and takes precedence, if moving averages not yet computed)
 	BalanceJitterTolerance = int64(4e8) // 400_000_000 ncheq
 )
